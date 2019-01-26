a0794e68174468b4b982e5f4af36807d20f59367
Tsumari <lucasflicky@hotmail.com>
2019-01-26 08:36:15 -0200

    preliminary achievment checker

---
+++ core/subroutines/achievementsCheck.js
@@ -0,0 +1,12 @@
+const gear = require("../utilities/Gearbox");
+const DB = require("../database/db_ops");
+const fs = require('fs')
+const locale = require(appRoot+'/utils/i18node');
+const _EVT = require("../archetypes/Events"); 
+
+exports.run = (msg) => {
+
+    
+
+
+}
