bac35b8c3922ee360b68c912b2eaa1ad5f90dc04
Tsumari <lucasflicky@hotmail.com>
2019-01-26 08:20:22 -0200

    mod command

---
+++ core/commands/_botOwner/selfrestart.js
@@ -0,0 +1,14 @@
+
+const init = async function (){
+    process.exit()
+}
+
+module.exports={
+    init
+    ,pub:true
+    ,cmd:'selfrestart'
+    ,perms:3
+    ,cat:'dev'
+    ,botPerms:['attachFiles','embedLinks']
+    ,aliases:[]
+}
