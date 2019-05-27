d5255a0b251e9018625d2dde1481ddafa8d5be77
Tsumari <lucasflicky@hotmail.com>
2019-05-27 11:53:03 -0300

    rng galleries

---
+++ core/structures/Galleries.js
@@ -0,0 +1,50 @@
+const axios = require('axios' )
+const {randomize} = require('../utilities/Gearbox')
+
+module.exports = {
+
+    randomOne: async (gallery)=>{
+        URL = paths.CDN+"/random/"+gallery
+        const response = await axios.get(URL, {
+            headers: { 'Accept': '*' },
+            responseType: 'arraybuffer'
+        });         
+        return response.data;       
+    },
+    randomOneIndexed: async (gallery,url=false)=>{
+        URL = paths.CDN+"/random/"+gallery
+        const pre_res = await axios.get(URL+"/size", {
+            headers: { 'Accept': 'json' },
+            responseType: 'json'
+        });         
+        let rand = randomize(0,pre_res.data-1)
+      
+        if(url){
+            return  URL+"/"+rand;
+        }else{            
+            const response = await axios.get(URL+"/"+rand, {
+                headers: { 'Accept': '*' },
+                responseType: 'arraybuffer'
+            });   
+            return Promise.resolve({file: response.data, index: rand+1});
+        }
+    },
+    indexedOne: async (gallery,index=0)=>{
+        URL = paths.CDN+"/random/"+gallery+"/"+index
+        const response = await axios.get(URL, {
+            headers: { 'Accept': '*' },
+            responseType: 'arraybuffer'
+        });         
+        return response.data;       
+    },
+    filteredOne: async (gallery,filter)=>{
+        URL = paths.CDN+"/random/"+gallery+"/filter/"+filter+"?json=1"
+        const response = await axios.get(URL, {
+            headers: { 'Accept': 'json' },
+            responseType: 'json'
+        });         
+        return response.data;       
+    },
+
+
+}
