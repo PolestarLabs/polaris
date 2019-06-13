5a8b07325448eb7f32b0f1bed98a94ad5ced07df
flicksie <lucasflicky@hotmail.com>
2019-06-13 13:46:39 -0300

    rss command

---
+++ core/commands/utility/rss.js
@@ -0,0 +1,101 @@
+const DB = require('../../database/db_ops');
+const gear = require('../../utilities/Gearbox');
+const YesNo = require('../../structures/YesNo');
+const RSS = require('rss-parser');
+const parser = new RSS();
+
+
+const init = async function (msg){
+    
+    let P={lngs:msg.lang,prefix:msg.prefix}
+    if(gear.autoHelper(["noargs",$t('helpkey',P)],{cmd:this.cmd,msg,opt:this.cat}))return;
+    
+    let feedData = await DB.feed.get({server: msg.guild.id});
+
+    // +RSS add (LINK) 
+    if(msg.args[0]=== "add"){
+        let str = msg.args[1];
+        let destination = msg.channelMentions[0]
+        let feed = await parser.parseURL(str).catch(e=>false);
+        if(!feed) return msg.channel.send("Invalid RSS link");
+        channel = destination || feedData.defaultChannel;
+        
+        if(!channel) return msg.channel.send("No channel pointed and no default channel set");
+        
+        let payload = {type: "rss", url: str, last: feed.items[0], channel: channel};
+
+        if(feedData && feedData.feeds.find(fdd=> fdd.url == str)){
+            await DB.feed.set({server:msg.guild.id,'feeds.url':str},{'feeds.$.channel':channel });
+            return msg.channel.send("This link is already present in your feed list. Updating Channel");
+        }
+                
+        await DB.feed.set({server:msg.guild.id},{$addToSet:{feeds:payload}});
+        let embed = await feedEmbed(feed.items[0],feed);
+        
+        msg.channel.send(gear.emoji("yep"),"Feed Saved - Submitting last post from it to <#"+channel+">");
+        return  msg.guild.channels.find(chn=>chn.id===channel).send( {embed} );        
+        
+    }
+    // +RSS remove (LINK || index) 
+    if(msg.args[0]=== "remove"){
+        let target = msg.args[1];
+        if (!target) return msg.channel.send("State an ID or URL");
+        let toDelete = feedData.feeds[target] || feedData.feeds.find(f=>f.url == target || f.url.includes(target) )
+        let embed = new gear.Embed;
+        embed.description = `
+                URL: \`${toDelete.url}\`
+                Channel: <#${toDelete.channel}>
+                `;
+
+        let confirm = await msg.channel.send({content:"Confirm delete?",embed});
+        YesNo.run(confirm,msg,async (cc)=>{           
+            await DB.feed.set({server:msg.guild.id},{$pull:{feeds:delet}});
+            return msg.channel.send("Deleted");
+        })
+    
+    }
+    if(msg.args[0]=== "list"){       
+        
+        msg.channel.send(`
+        ${feedData.feeds.map((x,i)=>i+" "+x.url+" @ "+`<#${x.channel}>`).join('\n')}        
+        `)
+    }
+    // +RSS defaultchannel (#channel) 
+    if(msg.args[0]=== "channel"){
+        feedData
+    }
+ 
+}
+
+
+
+async function feedEmbed(item,data){
+    let embed = new gear.Embed;
+    embed.color("#ff8a42") 
+    embed.title  = item.title
+    embed.url    = item.url || item.link || item.guid
+    embed.author(item.author || item.creator)
+    embed.timestamp(item.isoDate)
+    embed.thumbnail((data.image||{url:'https://cdn.pixabay.com/photo/2017/06/25/14/38/rss-2440955_960_720.png'}).url || '')
+    embed.description     = (item.contentSnippet || item.content || "" ).split('\n')[0]
+    embed.footer(data.title )
+    let ogs = require('open-graph-scraper');
+    let results = await ogs({ 'url': item.guid  }).catch(e=>{return false});
+    let img_link = results ? results.data.ogImage.url: null;  
+    if(img_link) embed.image = {url:img_link};
+    return embed;
+  }
+
+
+
+  
+module.exports={
+    init
+    ,embedGenerator: feedEmbed
+    ,pub:true
+    ,cmd:'rss'
+    ,perms:3
+    ,cat:'util'
+    ,botPerms:['embedLinks','manageMessages','manageChannels']
+    ,aliases:['feed']
+}
