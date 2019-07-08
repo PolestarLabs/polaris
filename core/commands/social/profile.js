7213ad0eec367ee2f9abacfef05c8fa1787f6fce
Tsumari <lucasflicky@hotmail.com>
2019-07-08 10:16:11 -0300

    standalone user profile class

--- core/commands/social/profile.js
+++ core/commands/social/profile.js
@@ -1,7 +1,7 @@
+const UserProfileModel = require('../../archetypes/UserProfileModel');
 const gear = require('../../utilities/Gearbox.js');
 const Picto = require('../../utilities/Picto.js');
 const DB = require('../../database/db_ops');
-const moment = require("moment");
 
 const XYZ ={
   global_roundel: {X: 680,  Y: 2},
@@ -90,97 +90,6 @@ const TEXT={
   }       
 };
 
-class UserProfileModel{
-  constructor(userData,discordMember){
-
-    // Discord Data
-    const {Member} = require('eris');
-    if(!discordMember) discordMember = POLLUX.users.get(userData.id||userData);
-
-    if(userData && userData.constructor.modelName !== "UserDB") discordMember = userData;
-    if(typeof discordMember === 'string') discordMember = POLLUX.users.get(discordMember);
-    const notMember = discordMember && discordMember.constructor != Member;
-
-    this.ID = discordMember.id;
-    this.server = notMember ? null : discordMember.guild.id;
-    this.localName = notMember ? discordMember.tag : discordMember.nick || discordMember.user.username;
-    this.avatar = notMember ? discordMember.avatarURL : discordMember.user.avatarURL;
-    this.bot = discordMember.bot;
-    
-    // Pollux User Data
-    if(!userData || !userData.modules) {
-      userData = {modules:{}};
-      this.PARTIAL = true;
-    }
-
-    this.favColor     = /^#[0-9,A-F,a-f]{6}$/.test(userData.modules.favcolor) ? userData.modules.favcolor : "#dd5383";
-    this.tagline      = userData.modules.tagline  || ""; 
-    this.background   = this.bot ? "IlyEEDBj0GLLlFl8n6boPLSkADNuBwke" : userData.modules.bgID || "5zhr3HWlQB4OmyCBFyHbFuoIhxrZY6l6"; 
-    this.personalText = userData.modules.persotext|| "";
-    this.exp          = userData.modules.exp      || 0;
-    this.level        = userData.modules.level    || 0;
-    this.percent      = XPercent(this.exp,this.level) || 0;
-    this.sticker      = userData.modules.sticker  || null;
-    this.flair        = userData.modules.flairTop || 'default';
-    this.rubines      = userData.modules.rubines  || 0;
-    this.medals       = userData.modules.medals   || [];
-    this.marriage     = userData.featuredMarriage || null;
-    this.commend     = userData.modules.commend  || 0;
-    this.countryFlag  = (userData.personal||{}).country || null;
-    this.profileFrame = userData.switches && userData.switches.profileFrame === true ? userData.donator : null;
-    
-    if(this.medals.length>0){
-      let valid_medals = this.medals.filter(mdl=>mdl&&mdl!="0").map(v=> this.medals.indexOf(v) );
-      let arrange = valid_medals.length <= 4 ? valid_medals.length : 9; 
-      this.medalsArrangement = {style:arrange,valid:valid_medals}
-    }      
-  }
-
-  get globalRank (){
-     return DB.users
-        .find({"modules.exp": {$gt: this.exp},blacklisted: {$exists: false}}).countDocuments();
-  }
-
-  get localData (){
-    return new Promise(async resolve=>{
-      if (!this.server){
-        this.thx = "---"
-        this.localRank = "---"
-        return resolve(false);
-      }
-    
-      let svRankData = await DB.localranks.get({user:this.ID,server:this.server});
-      this.thx = svRankData.thx || 0;
-      this.localRank = await DB.localranks
-        .find({server:svRankData.server,exp:{$gt:svRankData.exp}}).countDocuments();
-      return resolve(true);
-    })
-  }
-
-  get wifeData (){
-    return new Promise(async resolve=>{
-      if (this.wife) return resolve(this.wife);
-      if (!this.marriage) return resolve(null);
-      const marriage = await DB.relationships.findOne({type:'marriage', _id: this.marriage });
-      if (!marriage) return resolve(null);
-      const wifeID = marriage.users.find(usr=>usr!=this.ID);
-      if (!wifeID) return resolve(null);
-      let discordWife = POLLUX.users.get(wifeID) || (await DB.users.get(wifeID)).meta || {username: "Unknown", avatar: POLLUX.users.get(userID).defaultAvatarURL };
-
-      this.wife = {
-        ring: marriage.ring,
-        initiative: marriage.initiative === this.ID,
-        lovepoints: marriage.lovepoints||0,
-        since:  moment.utc(marriage.since).fromNow(true),
-        wifeName: discordWife.username,
-        wifeAvatar: (discordWife.avatarURL || discordWife.avatar).replace("size=512","size=64")
-      }
-      resolve(this.wife);
-    });
-  }
-
-}
-
 
 init = async (msg)=>{
 
@@ -225,7 +134,7 @@ init = async (msg)=>{
 
  
 
-  const USERPROFILE = new UserProfileModel(Target_Database||msg.args[0],msg.guild.member(Target));
+  const USERPROFILE = new UserProfileModel(Target_Database||msg.args[0],(msg.guild?msg.guild.member(Target):Target));
   //console.log(USERPROFILE)
   await Promise.all([
     await USERPROFILE.wifeData,
@@ -541,9 +450,7 @@ try{
   });
 
     //=========================================
-    //=========================================
     ///             HONORIFICS
-    //=========================================
     //=========================================
 
     
@@ -649,12 +556,4 @@ async function FINALIZE(msg,canvas){
     name: "profile.png"
   })
 
-}
-function XPercent(X, Lv, f = 0.0427899) {
-  let toNEXT = Math.trunc(Math.pow((Lv + 1) / f, 2));
-  let toTHIS = Math.trunc(Math.pow(Lv / f, 2));
-  let PACE = toNEXT - toTHIS;
-  let PROGRESS = X - toTHIS
-  let percent = PROGRESS / PACE;
-  return percent;
 }
