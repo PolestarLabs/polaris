0def9a72fe535c08f53d4d243a567b79a9b6abf5
Tsumari <lucasflicky@hotmail.com>
2019-07-17 02:54:40 -0300

    odds

--- GlobalNumbers.js
+++ GlobalNumbers.js
@@ -6,6 +6,26 @@ module.exports={
     },
     medalPrices: {
         UR:2850,SR:1875,R:1223,U:830,C:500
+    },
+    LootRates: {
+        rarity    : { C:1000, U:600, R:300, SR:150, UR:100, XR:0},
+        gems      : { C:250, U:500, R:850, SR:1100, UR:1320, XR:1880 },
+        itemType  : {
+            //JADES
+               JDE:  500
+            //RUBINES
+              ,RBN:  300
+            //MEDALS
+              ,MDL:  300
+            //BACKGROUNDS
+              ,BKG:  250
+            //BOOSTERS
+              ,BPK:  150
+            //ITEMS
+              ,ITM:  0
+            //SAPPHIRE
+              ,SPH:  1  
+            }
     }
 
 }
