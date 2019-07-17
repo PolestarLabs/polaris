73c03f7ad83a085ec29a046f8d6f469ae8209b1d
Tsumari <lucasflicky@hotmail.com>
2019-07-17 02:57:41 -0300

    hard reload

--- core/commands/_botStaff/reload.js
+++ core/commands/_botStaff/reload.js
@@ -1,6 +1,21 @@
-const init = async function (msg){
- 
+const init = async function (msg,args){
     POLLUX.registerCommands(true);
+    if(args[0] == "hard"){
+        Object.keys(require.cache).forEach((R)=> {
+            if(R.includes('node_modules'))  return;
+            if(R.includes('bot/pollux.js')) return;
+            if(R.includes('core/database')) return;
+            if(R.includes('Microserver'))   return;
+
+            delete require.cache[R]
+            try{
+                require(R)                
+            }catch(e){
+                console.log(R.red)
+                console.error(e)
+            }
+        });    
+    }
     msg.addReaction(_emoji("yep").reaction);
 
 }
