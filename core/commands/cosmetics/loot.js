40964611bf7792c950cb74a95cb5394afd870e5f
Tsumari <lucasflicky@hotmail.com>
2019-07-17 02:53:08 -0300

    loot pre-port (legacy)

--- core/commands/cosmetics/loot.js
+++ core/commands/cosmetics/loot.js
@@ -51,7 +51,7 @@ const  init = async function (msg,options={}) {
   
  delete require.cache[require.resolve("../../archetypes/Lootbox.js")];
   const LOOT = require("../../archetypes/Lootbox.js");
-  let USERDATA = await DB.users.get({id:msg.author.id});
+  let USERDATA = await DB.users.findOne({id:msg.author.id});
 
   
   let thisRoll = (options||{thisroll:0}).thisRoll || 0;
@@ -81,13 +81,13 @@ const  init = async function (msg,options={}) {
       rerolls = rerolls === 0 ? 0 : !rerolls? 3 : rerolls;
 
       let event = options.event     || false;
-  let boxaher = options.boxaher || 'lootbox_C_O';
+  let boxaher = options.boxaher || `lootbox_${options.rarity||"C"}_O`;
 
   let box_rarity = options.rarity || 'C';
     
   let stake = Math.round((USERDATA.modules.bgInventory.length||100) + (USERDATA.modules.bgInventory.length||100) + (USERDATA.modules.inventory.length||100) /1.5)  
   stake = stake < 80 ? 80 : stake;
-  console.log({stake})
+
 
   let factors = ["C", "U", "R", "SR", "UR"].indexOf(box_rarity) || 0;
   let rerollCost = ((thisRoll||0)+1)*Math.ceil(factors*1.5+1)*(stake+80);
@@ -153,18 +153,23 @@ dispatcher.on('end', () => {
     }
     
   }catch(e){} */
-    
-  await gear.wait(2);
-  sttup.delete().catch();
+
 
     
   const boxparams = await DB.items.findOne({id:boxaher});
-  const BOX = await LOOT.Lootbox(boxparams);
+  const BOXE = new LOOT.Lootbox(boxparams.rarity,boxparams);
+  const BOX = await BOXE.legacyfy;
+
+      
+  gear.wait(2).then(x=>{
+    sttup.delete().catch();
+  });
+
 
   //==>
-console.log('a')
-let LootData = await drawBox(BOX,message);
-console.log('b')
+
+let LootData = await drawBox(BOX,message,USERDATA);
+
     let LootMeta = LootData.LOOTS;
 
  // let lootpic=await msg.channel.send('box')
@@ -228,7 +233,8 @@ ${ej("retweet")} ${$t("loot.rerollRemain",P)} **${rerolls}**
       await gear.wait(2);
       q.delete().catch();
       message.author.looting = false;
-      return this.init(message,{issuer,thisRoll:thisRoll+1,rerolls:rerolls-1,event,boxaher,rarity:boxaher.split('_')[1]});
+     init(message,{issuer,thisRoll:thisRoll+1,rerolls:rerolls-1,event,boxaher,rarity:boxaher.split('_')[1]});
+     
     }else{
       await keeppit(LootMeta,boxaher,msg,USERDATA,rerollCostPrevious,thisRoll);
     };
@@ -240,7 +246,7 @@ ${ej("retweet")} ${$t("loot.rerollRemain",P)} **${rerolls}**
 
 
 function lineup(currentItem){
-   return box[currentItem.rarity] + box[currentItem.type] +(typeof currentItem.name=='number'?currentItem.name: '"'+(currentItem.type=='STICKERE'?'['+currentItem.SSS.split(' ').slice(0,2).join(' ').toUpperCase()+']':'')+currentItem.name+'"');
+  return box[currentItem.rarity] + box[currentItem.type] +(typeof currentItem.name=='number'?currentItem.name: '"'+(currentItem.type=='STICKERE'?'['+currentItem.SSS.split(' ').slice(0,2).join(' ').toUpperCase()+']':'')+currentItem.name+'"');
 };
 async function composeItem(currentItem,base,UDATA){
   let img,ITEM={}, nmo,eventide,qtd;
@@ -249,7 +255,7 @@ async function composeItem(currentItem,base,UDATA){
   switch(currentItem.type){
     case "RUBINES":
     case "JADES":
-      img =await Picto.getCanvas(paths.BUILD + "LOOT/" +(currentItem.emblem) + ".png");
+      img =await Picto.getCanvas(paths.CDN+"/build/LOOT/" +(currentItem.emblem) + ".png");
       qtd = await Picto.tag(base, "x" + gear.miliarize(currentItem.name,true), "34px 'Corporate Logo Rounded'", "#a0a0a0");
       
       break;
@@ -271,8 +277,8 @@ async function composeItem(currentItem,base,UDATA){
 
   };
 
-  let rarity = await Picto.getCanvas(appRoot+"/../v7/dash/public/images/tiers/new/" + currentItem.rarity + ".png")
-  let rarityShine = await Picto.getCanvas(paths.BUILD + "LOOT/shine" + currentItem.rarity + ".png")
+  let rarity = await Picto.getCanvas(paths.CDN+"/images/tiers/new/" + currentItem.rarity + ".png")
+  let rarityShine = await Picto.getCanvas(paths.CDN+"/build/LOOT/shine" + currentItem.rarity + ".png")
 
   let event = await Picto.tag(base, " ", "26px 'Corporate Logo Rounded'", "#d82d2d");
   let topname = await Picto.tag(base, ((ITEM||currentItem).displayType||currentItem.type).toUpperCase(), "30px 'Corporate Logo Rounded'", "#d0d0d0");
@@ -298,8 +304,7 @@ async function composeItem(currentItem,base,UDATA){
 
   return item;
 };
-async function drawBox(BOX,message){
-  let UDATA = await DB.users.get({id:message.author.id});
+async function drawBox(BOX,message,UDATA){
   try{
   const canvas = new Canvas.createCanvas(800, 600);
   const base = canvas.getContext('2d');
@@ -308,24 +313,29 @@ async function drawBox(BOX,message){
 
     let sq = 70
     let shift = (50-sq)/2
-    const B = await Picto.getCanvas(paths.BUILD + "LOOT/mainframe.png");
+    const B = await Picto.getCanvas(paths.CDN+"/build/LOOT/mainframe.png");
 
   for (let i = 0; i < Max; ++i) {
     try{
       let LOOTIE = await composeItem(BOX[i],base,UDATA);
 
        base.drawImage(LOOTIE.RS, 74 + i*230+shift, 45);
+       console.log({LOOTIE})
        base.drawImage(LOOTIE.I, 32 + i*230,32);  //DRAW ITEM
        base.drawImage(LOOTIE.R, 152 + i*230+shift, 305, sq,sq);  //DRAW RARITY
        base.drawImage(LOOTIE.N.item, 175 + i*230-(LOOTIE.N.width/2), 44);  //DRAW NAME
-       base.drawImage(LOOTIE.EV.item, 175 + i*230-(LOOTIE.EV.width/2), 44);  //DRAW EVENT
+       try{
+
+         base.drawImage(LOOTIE.EV.item, 175 + i*230-(LOOTIE.EV.width/2), 44);  //DRAW EVENT
+        }catch(e){
+
+        }
         
       if (LOOTIE.Q) await base.drawImage(LOOTIE.Q.item, 197 + i*230+shift, 250);  //DRAW QUANT
-
       LOOTS.push(LOOTIE.meta);
 
     }catch(err){
-      console.log(err)
+      console.error(err)
     };
   }
   await base.drawImage(B, 0, 0);
@@ -339,7 +349,7 @@ async function drawBox(BOX,message){
 async function renderBG(bname,UDATA,Rar) {
   
 
-    let BGFILE = paths.BUILD + "backdrops/"+bname+".png";
+    let BGFILE = paths.CDN+"/backdrops/"+bname+".png";
     const itemCanvas = {};
     itemCanvas.displayName = '"'+bname+'"'
     itemCanvas.canvas = new Canvas.createCanvas(285,350);
@@ -348,8 +358,8 @@ async function renderBG(bname,UDATA,Rar) {
 
     const ctx = itemCanvas.canvas.getContext('2d');
     let I = await Picto.getCanvas(BGFILE);
-    let J = await Picto.getCanvas(paths.BUILD+"LOOT/BG.png");
-    let NW = await Picto.getCanvas(paths.BUILD+"new.png");
+    let J = await Picto.getCanvas(paths.CDN+"/build/LOOT/BG.png");
+    let NW = await Picto.getCanvas(paths.CDN+"/build/new.png");
     ctx.rotate(0.16);
     ctx.drawImage(I, 36, 80,260,140);
     ctx.rotate(-0.16);
@@ -380,7 +390,7 @@ ctx.fillStyle=grd;
 ctx.fillRect(0,220,285,50);
   ctx.font = "900 18px 'Whitney HTF'";
   let amt = exRate[rar]
-let RBNm = await Picto.getCanvas("http://pollux.fun/images/cosmoshard.png");
+let RBNm = await Picto.getCanvas(paths.CDN+"/build/items/cosmo.png");
 let TX = await Picto.tag(ctx, "DUPLICATE", "600 26px 'Corporate Logo Rounded'", "#ffffff");  
 let TX1 = await Picto.tag(ctx, "DUPLICATE", "600 26px 'Corporate Logo Rounded'", "rgba(0, 0, 0, .7)");  
 let TX2 = await Picto.tag(ctx, `+${amt} Cosmo Fragments`, "400 18px 'Corporate Logo Rounded'", "#ffffff");  
@@ -397,7 +407,7 @@ ctx.drawImage(RBNm,142-TX2.width/2-10,247,20,20);
 
 async function renderMedal(medal,UDATA,Rar) {  
 
-    let MEDALFILE = paths.MEDALS + medal.item + ".png";
+    let MEDALFILE = paths.CDN+"/medals/" + medal.item + ".png";
     const itemCanvas = {};
     itemCanvas.displayName = '"'+medal.name+'"'
     itemCanvas.canvas = new Canvas.createCanvas(285,350);
@@ -406,14 +416,13 @@ async function renderMedal(medal,UDATA,Rar) {
 
     const ctx = itemCanvas.canvas.getContext('2d');
     let I = await Picto.getCanvas(MEDALFILE);
-    let J = await Picto.getCanvas(paths.BUILD+"LOOT/MEDAL.png");
-    let NW = await Picto.getCanvas(paths.BUILD+"new.png");
+    let J = await Picto.getCanvas(paths.CDN+"/build/LOOT/MEDAL.png");
+    let NW = await Picto.getCanvas(paths.CDN+"/build/new.png");
     ctx.drawImage(I, 94, 128,100,100);
     ctx.drawImage(J, 26, 26);
    
       
      if(!UDATA.modules.medalInventory.includes(medal.item)){
-       console.error("doop")
          
     ctx.drawImage(NW, 150, 90, 64,64);
        }else{
@@ -426,7 +435,7 @@ await makegrad(ctx,Rar);
 };
 async function renderSticker(stk) {
 
-    let STIKFILE = paths.BUILD+"stickers/" + stk.item + ".png"
+    let STIKFILE = paths.CDN+"/stickers/" + stk.item + ".png"
     const itemCanvas = {};
     //itemCanvas.displayName = '"['+stk.SSS.split(' ').slice(0,2).join(' ').toUpperCase()+']'+stk.name+'"' 
     itemCanvas.canvas = new Canvas.createCanvas(285,350);
@@ -435,7 +444,7 @@ async function renderSticker(stk) {
 
     const ctx = itemCanvas.canvas.getContext('2d');
     let I = await Picto.getCanvas(STIKFILE);
-    let J = await Picto.getCanvas(paths.BUILD+"LOOT/STAMP.png");
+    let J = await Picto.getCanvas(paths.CDN+"/build/LOOT/STAMP.png");
     ctx.drawImage(I, 94, 128,100,100);
     ctx.drawImage(J, 26, 26);
 
@@ -443,7 +452,7 @@ async function renderSticker(stk) {
 };
 
 async function renderBooster(stk) {
-    let STIKFILE = paths.BUILD+"items/" + stk.SSS + ".png"
+    let STIKFILE = paths.CDN+"/build/items/" + stk.item.split('_')[0] + ".png"
     const itemCanvas = {};
     //itemCanvas.displayName = '"['+stk.SSS.split(' ').slice(0,2).join(' ').toUpperCase()+']'+stk.name+'"' 
     itemCanvas.canvas = new Canvas.createCanvas(285,350);
@@ -452,7 +461,8 @@ async function renderBooster(stk) {
 
     const ctx = itemCanvas.canvas.getContext('2d');
     let I = await Picto.getCanvas(STIKFILE);
-    let J = await Picto.getCanvas(paths.BUILD+"LOOT/STAMP.png");
+
+    let J = await Picto.getCanvas(paths.CDN+"/build/LOOT/STAMP.png");
     ctx.drawImage(I, 54, 98,200,200);
     ctx.drawImage(J, 26, 26);
 
@@ -472,7 +482,7 @@ function invent_merge(item,message,UDATA){
   switch(iMETA.type){
     case "RUBINES":
       amt=Number(parseInt(iMETA.name));
-      USERDATA.addItem(message.author.id,amt,"lootbox","RBN","+").then(ok=>{
+      ECO.receive(message.author.id,amt,"lootbox","RBN","+").then(ok=>{
         P.X = ej("rubine")+"**"+iMETA.name +"**"
         return resolve($t("loot.addedRubines",P));
       }).catch(e=> console.warn("ERROR"));
@@ -538,7 +548,7 @@ function invent_merge(item,message,UDATA){
 
       let m=iMETA.rawName;
 
-        DB.users.set(message.author.id,{$push:{'modules.inventory':m}}).then(ok=>{
+      UDATA.addItem(m).then(ok=>{
           P.itm= iMETA.name
           return resolve(ej("no1")+$t("loot.addedSticker",P));
         }).catch(e=> resolve("ERROR"))
@@ -577,7 +587,8 @@ async function keeppit(LOOTS,boxaher,message,UDATA,rerollCost,thisRoll) {
     if(thisRoll>0){
       ECO.pay(message.author.id,rerollCost,"lootbox_rerolls","RBN");
     }
-      await DB.items.consume(message.author,boxaher);
+
+      await UDATA.removeItem(boxaher).then(console.log);
 };
 
  module.exports = {
