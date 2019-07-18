66e49617f4a43e2534e1361403b3a54b939ffb31
Tsumari <lucasflicky@hotmail.com>
2019-07-17 21:50:55 -0300

    reaction box open

--- core/commands/inventory/lootbox.js
+++ core/commands/inventory/lootbox.js
@@ -15,17 +15,29 @@ const init = async function (msg,args,userID){
         description: Inventory.map(i=> `${_emoji(i.rarity)} ${_emoji(i.emoji||i.emoji_alt)} **${i.name}** × ${i.count} \`${msg.prefix||args[1]}open box ${i.rarity}\`` ).join('\n')
     }
 
+
     return {content: `${_emoji('LOOTBOX')} Browsing **Lootbox** Inventory` , embed } 
  
 }
 
 
-const open = async function (msg,args){
+const open = async function (msg,args,userID){
 
-    const userInventory = new INVENTORY(msg.author.id,"box");
+    console.log(args)
+    try{
+
+        if(userID && (args[1]||{}).id != userID) return "Only the owner can see inside";
+    }catch(e){
+        console.log(e)
+    }
+    console.log("papa")
+    
+    const userInventory = new INVENTORY(userID||msg.author.id,"box");
     const Inventory     = await userInventory.listItems();
-
+    console.log("tutu")
+    
     if(!Inventory.find(bx=>bx.rarity == args[0])) return "No such box";
+    
 
     (require("../cosmetics/loot.js")).init(msg,{rarity:args[0]})
 
@@ -51,4 +63,18 @@ module.exports={
             }
         }
     ]
+    ,reactionButtons:[
+        {
+            emoji: _emoji("UR").reaction,
+            type: "edit",
+            response: async (msg,args,uid)=>{
+                console.log('wa')
+                msg.author.id = uid;
+                await open(msg,["UR"]);
+                console.log('wu')
+                return "ok"
+            },
+        }
+    ]
+
 }
