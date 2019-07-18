552447b0b6a37587dd71b7e3ad0b63a8ea948dc8
Tsumari <lucasflicky@hotmail.com>
2019-07-17 23:10:04 -0300

    reaction chain command

--- core/commands/inventory/lootbox.js
+++ core/commands/inventory/lootbox.js
@@ -1,7 +1,7 @@
-const {Embed} = require('../../utilities/Gearbox');
 const INVENTORY = require('../../archetypes/Inventory');
 
-
+const INVOKERS   = new Map();
+const INV_STATUS = new Map();
 
 
 const init = async function (msg,args,userID){
@@ -15,35 +15,46 @@ const init = async function (msg,args,userID){
         description: Inventory.map(i=> `${_emoji(i.rarity)} ${_emoji(i.emoji||i.emoji_alt)} **${i.name}** × ${i.count} \`${msg.prefix||args[1]}open box ${i.rarity}\`` ).join('\n')
     }
 
+    args[0] = msg
+    args[1] = Inventory.map(i=>i.rarity)
+    INV_STATUS.set(userID || msg.author.id, args[1] );
+      
+    let response =  {content: `${_emoji('LOOTBOX')} ${$t('responses.inventory.browsingBox',{lngs:msg.lang})}` , embed };
 
-    return {content: `${_emoji('LOOTBOX')} Browsing **Lootbox** Inventory` , embed } 
- 
+    if(userID) return response;
+
+    let res = await msg.channel.send(response);
+    INVOKERS.set(userID || msg.author.id, res.id );
+    return res;
+     
 }
 
 
 const open = async function (msg,args,userID){
-
-    console.log(args)
-    try{
-
-        if(userID && (args[1]||{}).id != userID) return "Only the owner can see inside";
-    }catch(e){
-        console.log(e)
-    }
-    console.log("papa")
     
+    INVOKERS.delete(userID||msg.author.id)
+    INV_STATUS.delete(userID||msg.author.id)
+    
+    if(userID && msg.author.id != userID) return "Only the owner can see inside";
+
     const userInventory = new INVENTORY(userID||msg.author.id,"box");
-    const Inventory     = await userInventory.listItems();
-    console.log("tutu")
-    
-    if(!Inventory.find(bx=>bx.rarity == args[0])) return "No such box";
-    
+    const Inventory     = await userInventory.listItems();    
+     
+    if(!Inventory.find(bx=>bx.rarity == args[0])) return $t('responses.inventory.noSuchBox',{lngs:msg.lang});  
 
-    (require("../cosmetics/loot.js")).init(msg,{rarity:args[0]})
-
- 
+    require("../cosmetics/loot.js").init(msg,{rarity:args[0]}).catch(console.error) 
 }
 
+const reactionOption = (rar) => {    
+    return {        
+        emoji: _emoji(rar).reaction,
+        type: "cancel",
+        response: (msg,args,uid)=>{
+            return open(args[0],[rar,args[1]],uid);            
+        },
+        filter:(msg,emj,uid)=> INVOKERS.get(uid) == msg.id && INV_STATUS.get(uid).includes(emj.substr(0,2).replace('_',''))
+    }
+}
 
 module.exports={
     init
@@ -63,18 +74,7 @@ module.exports={
             }
         }
     ]
-    ,reactionButtons:[
-        {
-            emoji: _emoji("UR").reaction,
-            type: "edit",
-            response: async (msg,args,uid)=>{
-                console.log('wa')
-                msg.author.id = uid;
-                await open(msg,["UR"]);
-                console.log('wu')
-                return "ok"
-            },
-        }
-    ]
+    ,reactionButtons: ["C","U","R","SR","UR"].map(reactionOption)
 
 }
+
