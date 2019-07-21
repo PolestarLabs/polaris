b5bcef59c1f0bb77387ede68d20f80a492fc7dec
Tsumari <lucasflicky@hotmail.com>
2019-07-21 15:46:49 -0300

    loot security

--- core/commands/inventory/lootbox.js
+++ core/commands/inventory/lootbox.js
@@ -4,11 +4,9 @@ const INVOKERS   = new Map();
 const INV_STATUS = new Map();
 const LOOTING    = new Map();
 
-
 const init = async function (msg,args,userID){
     
     if(userID && (args[10]||{}).id != userID) return "Only the owner can see inside";
-    const P = {lngs:msg.lang};
 
     const userInventory = new INVENTORY(userID||msg.author.id,"box");
     const Inventory     = await userInventory.listItems( args[10] );
@@ -17,18 +15,17 @@ const init = async function (msg,args,userID){
     embed.description = 
         Inventory.length > 0 
             ? Inventory.map(i=> `${_emoji(i.rarity)} ${_emoji(i.emoji||i.emoji_alt)} **${i.name}** × ${i.count} \`${msg.prefix||args[1]}open box ${i.rarity}\`` ).join('\n')
-            :  `*${rand$t('responses.inventory.emptyJokes',P)}*`
+            :  `*${rand$t('responses.inventory.emptyJokes',{lngs:msg.lang})}*`
     
 
     args[0] = msg
     args[1] = Inventory.map(i=>i.rarity)
     INV_STATUS.set(userID || msg.author.id, args[1] );
    
-      const LOOTING    = new Map();
-    let response =  {content: `${_emoji('LOOTBOX')} ${$t('responses.inventory.browsingBox',P)}` , embed }; 
+
+    let response =  {content: `${_emoji('LOOTBOX')} ${$t('responses.inventory.browsingBox',{lngs:msg.lang})}` , embed }; 
 
     if(userID) return response;
-
     let res = await msg.channel.send(response);
     INVOKERS.set(userID || msg.author.id, res.id );
     return res;
@@ -47,8 +44,7 @@ const open = async function (msg,args,userID){
     const Inventory     = await userInventory.listItems();    
      
     LOOTING.set(userID || msg.author.id, true );
-    if(!Inventory.find(bx=>bx.rarity == args[0])) return $t('responses.inventory.noSuchBox',P);  
-    console.log(LOOTING)
+    if(!Inventory.find(bx=>bx.rarity == args[0])) return $t('responses.inventory.noSuchBox',{lngs:msg.lang});  
     require("../cosmetics/loot.js").init(msg,{rarity:args[0]}).catch(console.error).then(done=> LOOTING.delete(userID || msg.author.id, true) );
 }
 
@@ -59,7 +55,7 @@ const reactionOption = (rar) => {
         response: (msg,args,uid)=>{
             return open(args[0],[rar,args[1]],uid);            
         },
-        filter:(msg,emj,uid)=> INVOKERS.get(uid) == msg.id && INV_STATUS.get(uid).includes(emj.substr(0,2).replace('_','') )
+        filter:(msg,emj,uid)=> INVOKERS.get(uid) == msg.id && INV_STATUS.get(uid).includes(emj.substr(0,2).replace('_','')) && !LOOTING.get(uid)
     }
 }
 
