fc7f4130a7f29b590cc1183115944add7d82fc8f
Tsumari <lucasflicky@hotmail.com>
2019-08-03 23:39:01 -0300

    bad return

--- core/commands/_botOwner/istranslator.js
+++ core/commands/_botOwner/istranslator.js
@@ -1,6 +1,6 @@
 const init = async function (msg,args){
     const Target = PLX.getTarget(msg,1,false)
-    if(!Target) return msg.addReaction(_emoji('nope').reaction);
+    if(!Target) {msg.addReaction(_emoji('nope').reaction);return}
     await DB.users.set(Target.id,{$set:{'switches.role':'translator','switches.translator':args[0].toUpperCase()}});
     msg.addReaction(_emoji('yep').reaction);
 }
