71956c80b7f92c41fe4dc371ec74f9a1737e11fa
Flicky <lucasflicky@hotmail.com>
2019-08-30 17:39:57 -0300

    canvas test command

---
+++ core/commands/_botOwner/cnvs.js
@@ -0,0 +1,23 @@
+const Picto =  require('../../utilities/Picto');
+
+const init = async function (msg,args){
+
+    const Canvas = Picto.new(800,600);
+    const ctx    = Canvas.getContext('2d');
+
+    
+
+
+    msg.channel.send( "." ,file(Canvas.toBuffer(),"x.png"));
+
+
+}
+module.exports={
+    init
+    ,pub:false
+    ,cmd:'asd'
+    ,perms:3
+    ,cat:'_botOwner'
+    ,botPerms:['attachFiles','embedLinks']
+    ,aliases:[]
+}
