d1a163cf9a6d64bac045bf71f653b83d5ec06758
Tsumari <lucasflicky@hotmail.com>
2019-11-29 05:27:41 +0000

    change sendMessage to send for standardization

--- core/commands/games/russianroulette.js
+++ core/commands/games/russianroulette.js
@@ -4,11 +4,11 @@ const economy = require(appRoot + '/core/archetypes/Economy.js');
 
 const init = async function (msg, args) {
   if (args[0] === 'multiplayer') {
-    await msg.channel.sendMessage(`Ok, multiplayer mode.\nTo join the match, just use \`join <how many rubines you are using>\`.\n**The match starts in __20 seconds__.**`);
-    const players = await startPlayerCollector(await msg.channel.sendMessage('**Total of rubines in the pool:** 0 rubines\n**Players**\n---'));
-    if (players.length === 1) return msg.channel.sendMessage('If only 1 person is going to play, you should use singleplayer mode.')
-    if (players.length === 0) return msg.channel.sendMessage('No one joined. I\'m not playing this alone.')
-    await msg.channel.sendMessage(`**Time's up!** Let's get started.\nPlayers: \`\`\`${players.map(a => a.name).join(', ')}\`\`\``);
+    await msg.channel.send(`Ok, multiplayer mode.\nTo join the match, just use \`join <how many rubines you are using>\`.\n**The match starts in __20 seconds__.**`);
+    const players = await startPlayerCollector(await msg.channel.send('**Total of rubines in the pool:** 0 rubines\n**Players**\n---'));
+    if (players.length === 1) return msg.channel.send('If only 1 person is going to play, you should use singleplayer mode.')
+    if (players.length === 0) return msg.channel.send('No one joined. I\'m not playing this alone.')
+    await msg.channel.send(`**Time's up!** Let's get started.\nPlayers: \`\`\`${players.map(a => a.name).join(', ')}\`\`\``);
     return startMultiplayerLoop(msg, shuffle(players))
   }
 
@@ -20,7 +20,7 @@ const init = async function (msg, args) {
 
   const game = new RussianRoulette(msg, parseInt(args[0]))
 
-  await msg.channel.sendMessage(`Russian Roulette? You probably already know the rules, so let\'s get started.\nIf you survive this one, you\'re going to receive **${game.nextValue} rubines**.\nUse \`shoot\` to proceed (if you get shot, you'll lose your money).`)
+  await msg.channel.send(`Russian Roulette? You probably already know the rules, so let\'s get started.\nIf you survive this one, you\'re going to receive **${game.nextValue} rubines**.\nUse \`shoot\` to proceed (if you get shot, you'll lose your money).`)
   startGameCollector(game, msg)
 };
 
@@ -28,14 +28,14 @@ const startGameCollector = async (msg) => {
   const response = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, { time: 30000, maxMatches: 1 });
   if (!response[0]) return msg.reply('you haven\'t said your action in 30 seconds! Stopping the game.')
   const result = await game.handleInput(response[0].content)
-  if (result.invalidInput) return msg.channel.sendMessage('You **have** to say shoot or stop. Game stopped, and I\'m not returning your money back.')
+  if (result.invalidInput) return msg.channel.send('You **have** to say shoot or stop. Game stopped, and I\'m not returning your money back.')
   if (result.stopped) {
     const better = parseInt(msg.args[0]) > result.currentValue
     if (better) economy.pay(msg.author.id, result.currentValue, 'RUSSIANROULETTE')
-    return msg.channel.sendMessage(better ? `You're a quitter!\n I added **${result.currentValue} rubines** to your account. Sigh.` : 'You\'re a quitter!\nI haven\'t changed anything because you didn\'t even played.')
+    return msg.channel.send(better ? `You're a quitter!\n I added **${result.currentValue} rubines** to your account. Sigh.` : 'You\'re a quitter!\nI haven\'t changed anything because you didn\'t even played.')
   }
 
-  const message = await msg.channel.sendMessage('Let\'s see if you\'re going to die now...')
+  const message = await msg.channel.send('Let\'s see if you\'re going to die now...')
   if (result.lost) {
     return message.edit(`BOOM! Someone got shot...\nYou lost your money. RIP.`)
   } else if (result.won) {
@@ -70,7 +70,7 @@ const startMultiplayerLoop = async (msg, players) => {
   while (!finished) {
     const game = new RussianRoulette(null, 0)
     let content = `**Round ${round}**\n`
-    const message = await msg.channel.sendMessage(content);
+    const message = await msg.channel.send(content);
     let _p = 0
     while (_p < players.length) {
       const player = players[_p]
@@ -95,11 +95,11 @@ const startMultiplayerLoop = async (msg, players) => {
     }
     if (players.length === 1) {
       economy.receive(players[0].id, value, 'RUSSIANROULETTE')
-      return message.channel.sendMessage(`Game over, you all! ${players[0].name} won.`)
+      return message.channel.send(`Game over, you all! ${players[0].name} won.`)
       finished = true
     }
     _p = 0
-    await message.channel.sendMessage('End of the round! **Results:**\n'+ (lost ? `${lost.name} was the loser. RIP.` : 'No one died this time...') +'\nStarting the next round.');
+    await message.channel.send('End of the round! **Results:**\n'+ (lost ? `${lost.name} was the loser. RIP.` : 'No one died this time...') +'\nStarting the next round.');
     lost = null
     round++
   }
