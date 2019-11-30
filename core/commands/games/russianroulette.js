b702de1e154bfcf05ef733219bb763fb48b7a9a6
Tsumari <flicky@pollux.gg>
2019-11-30 15:32:08 -0300

    review & fixes

--- core/commands/games/russianroulette.js
+++ core/commands/games/russianroulette.js
@@ -2,9 +2,6 @@ const RussianRoulette = require(appRoot + '/core/archetypes/RussianRoulette.js')
 const ECO = require(appRoot + '/core/archetypes/Economy.js');
 
 
-
-
-
 const init = async function (msg, args) {
 
 	if (args[0] === 'multiplayer') {
@@ -18,9 +15,9 @@ const init = async function (msg, args) {
 
 	if ( !parseInt(args[0]) ) return msg.reply('you have to give me a number of how much rubines you are going to ~~waste~~ use, or you can use `multiplayer` to create a multiplayer game.');
 
-	const urf = await ECO.checkFunds(msg.author.id, parseInt(args[0]))
+	const urf = await ECO.checkFunds(msg.author.id, parseInt(args[0]));
 	if (!urf) return msg.reply('you don\'t have all this money to waste with russian roulette.');
-	ECO.pay(msg.author.id, parseInt(args[0]))
+	await ECO.pay(msg.author.id, parseInt(args[0]));
 
 	const game = new RussianRoulette(msg, parseInt(args[0]))
 
@@ -29,33 +26,38 @@ const init = async function (msg, args) {
 };
 
 const startGameCollector = async (msg) => {
+
 	const response = await msg.channel.awaitMessages(m => m.author.id === msg.author.id, {
 		time: 30e3,
 		maxMatches: 1
 	});
+
 	if (!response[0]) return msg.reply('you haven\'t said your action in 30 seconds! Stopping the game.');
-	const result = await game.handleInput(response[0].content)
+
+	const result = await game.handleInput(response[0].content);
 	if (result.invalidInput) return msg.channel.send('You **have** to say shoot or stop. Game stopped, and I\'m not returning your money back.');
+	
 	if (result.stopped) {
 		const better = parseInt(msg.args[0]) > result.currentValue
-		if (better) ECO.pay(msg.author.id, result.currentValue, 'RUSSIANROULETTE')
+		if (better) await ECO.pay(msg.author.id, result.currentValue, 'RUSSIANROULETTE');
 		return msg.channel.send(better ? `You're a quitter!\n I added **${result.currentValue} rubines** to your account. Sigh.` : 'You\'re a quitter!\nI haven\'t changed anything because you didn\'t even played.');
 	}
 
-	const message = await msg.channel.send('Let\'s see if you\'re going to die now...')
+	const message = await msg.channel.send('Let\'s see if you\'re going to die now...');
 	if (result.lost) {
 		return message.edit(`BOOM! Someone got shot...\nYou lost your money. RIP.`);
 	} else if (result.won) {
-		ECO.receive(msg.author.id, parseInt(msg.args[0]) * 1.5, 'RUSSIANROULETTE')
+		await ECO.receive(msg.author.id, parseInt(msg.args[0]) * 1.5, 'RUSSIANROULETTE');
 		return message.edit(`**no bullet noise**\nYou came out alive of the game...\nI added **${game.maxValue}** rubines to your account.`);
 	}
 
-	await message.edit(`*\*no bullet noise\**\nNo bullet this time (${result.rounds} rounds remaining)...\nYou currently have **${result.currentValue} rubines.**\nUse \`shoot\` to test your luck one more time (if you don't get shot, I'm going to add more money to your current amount)\nUse \`stop\` to stop here and get your money.`)
+	await message.edit(`*\*no bullet noise\**\nNo bullet this time (${result.rounds} rounds remaining)...\nYou currently have **${result.currentValue} rubines.**\nUse \`shoot\` to test your luck one more time (if you don't get shot, I'm going to add more money to your current amount)\nUse \`stop\` to stop here and get your money.`);
 	startCollector(game, msg)
 }
 
 const startPlayerCollector = async (msg) => {
 	const verifiedPlayers = []
+
 	const filter = async (m) => {
 		return m.content.startsWith('join ') &&
 			!verifiedPlayers.filter(a => a.id === m.author.id)[0] &&
@@ -66,26 +68,32 @@ const startPlayerCollector = async (msg) => {
 				id: m.author.id,
 				name: m.author.username,
 				money: parseInt(m.content.split(' ')[1])
-			}) &&
+			}) &&			
 			msg.edit(`**Total of rubines in the pool**: ${verifiedPlayers.map(a => a.money).reduce((a, b) => a + b)} rubines\n**Players**\n${verifiedPlayers.map(a => `- **${a.name}** - ${a.money} rubines\n`)}`)
 	}
+
 	const collector = await msg.channel.awaitMessages(filter, {
 		time: 20e3,
 		maxMatches: 5
 	});
+
 	return verifiedPlayers;
 }
 
 const startMultiplayerLoop = async (msg, players) => {
+
 	const value = players.map(a => a.money).reduce((a, b) => a + b)
 	let finished = false
 	let round = 1
 	let lost = false
+
 	while (!finished) {
+
 		const game = new RussianRoulette(null, 0)
 		let content = `**Round ${round}**\n`
 		const message = await msg.channel.send(content);
 		let _p = 0
+
 		while (_p < players.length) {
 			const player = players[_p]
 			content += `${player.name}'s turn.... `
@@ -96,27 +104,34 @@ const startMultiplayerLoop = async (msg, players) => {
 				await ECO.pay(player.id, player.money, 'RUSSIANROULETTE');
 				lost = player
 			}
+
 			await wait(3);
-			if (lost) content += `BOOM! ${player.name} is dead.`
-			else content += '*no bullet noise*\n'
+
+			if (lost) content += `BOOM! ${player.name} is dead.`;
+			else content += '*no bullet noise*\n';
+
 			await message.edit(content);
-			if (lost) {
-				_p = players.length + 1
-			} else { // Stop the forEach loop, the bullet is now gone
-				_p++
-			}
+
+			if (lost) _p = players.length + 1;
+			else _p++; 	// Stop the forEach loop, the bullet is now gone
+			
 			await wait(3);
 		}
+
 		if (players.length === 1) {
-			ECO.receive(players[0].id, value, 'RUSSIANROULETTE')
-			return message.channel.send(`Game over, you all! ${players[0].name} won.`);
+			await ECO.receive(players[0].id, value, 'RUSSIANROULETTE');
+			message.channel.send(`Game over, you all! ${players[0].name} won.`)
 			finished = true
+			return
 		}
+
 		_p = 0
+
 		await message.channel.send('End of the round! **Results:**\n' + (lost ? `${lost.name} was the loser. RIP.` : 'No one died this time...') + '\nStarting the next round.');
 		lost = null
 		round++
 	}
+
 }
 
 module.exports = {
