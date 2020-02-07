ad29bbc861a63e775e46200948ca5f00fe131676
Tsumari <flicky@pollux.gg>
2020-02-07 11:18:02 -0300

    discoin reimplementation

--- core/archetypes/Discoin.js
+++ core/archetypes/Discoin.js
@@ -1,68 +1,30 @@
-const request = require("request");
-const rescodes = [200, 400, 401, 403];
+const request 	= require("request");
+const rescodes 	= [200, 201, 400, 401, 403];
+const ROOT 		= "https://discoin.zws.im"
 
 module.exports = class Discoin {
 	constructor (token){
 		this.token = token;
-	}
-	request (user, amount, exchangeTo) {
-		return new Promise((resolve, reject) => {
-			request.post({url: "http://discoin.sidetrip.xyz/transaction", headers: {"Authorization": this.token}, json: {"user": user, "amount": amount, "exchangeTo": exchangeTo}}, function (error, response, body) {
-				if (error || rescodes.indexOf(response.statusCode) === -1) {
-					return reject(`[${response.statusCode}] :: API failure - Contact MacDue`);
-				}
-				if (response.statusCode !== 200) {
-					return reject(body);
-				}
-				return resolve(body);
+		this.rest = (METHOD, route, payload, query ) => {
+			if(typeof payload === 'string') query = payload, payload = null;
+			return new Promise((resolve,reject)=>{
+				console.log(this.token);
+				(request[METHOD])({url: `${ROOT}/${route}${query?"?s="+encodeURIComponent(query):""}`, headers: {"Authorization": "Bearer " + this.token}, json: payload }, (err, res, body) => {
+					if (err || rescodes.indexOf(res.statusCode) === -1)		
+						return reject(`[${res.statusCode}] :: API failure`);
+					if ( ![200,201].includes(res.statusCode) )
+						return reject({body,request: `${METHOD.toUpperCase()}: /${route}`,payload});			
+					return resolve(body);
+				})
 			});
-		});
-	}
-
-	fetch () {
-		return new Promise((resolve, reject) => {
-			request({url: "http://discoin.sidetrip.xyz/transactions", headers: {"Authorization": this.token}}, function (error, response, body) {
-				if (error || response.statusCode !== 200) {
-					return reject(`[${response.statusCode}] :: API failure - Contact MacDue`);
-				}
-				return resolve(body);
-			});
-		});
-	}
-
-	reverse (receipt) {
-		return new Promise((resolve, reject) => {
-			request.post({url: "http://discoin.sidetrip.xyz/transaction/reverse", headers: {"Authorization": this.token}, json: {"receipt": receipt}}, function (error, response, body) {
-				if (error || response.statusCode !== 200) {
-					return reject(`[${response.statusCode}] :: API failure - Contact MacDue`);
-				}
-				return resolve(body);
-			});
-		});
-	}
-
-	info (receipt) {
-		return new Promise((resolve, reject) => {
-			request({url: "http://discoin.sidetrip.xyz/transaction/" + receipt, headers: {"Authorization": this.token}}, function (error, response, body) {
-				if (error || response.statusCode !== 200) {
-					return reject(`[${response.statusCode}] :: API failure - Contact MacDue`);
-				}
-				if (response.statusCode !== 200) {
-					return reject(body);
-				}
-				return resolve(body);
-			});
-		});
-	}
-
-	rates () {
-		return new Promise((resolve, reject) => {
-			request({url: "http://discoin.sidetrip.xyz/rates.json"}, function (error, response, body) {
-				if (error || response.statusCode !== 200) {
-					return reject("API failure - Contact MacDue");
-				}
-				return resolve(body);
-			});
-		});
+		};
+	}	
+	rates	() 				{return this.rest("get",   `currencies` ) } 
+	fetch	() 				{return this.rest("get",   "transactions",`{"to.id": "RBN", "handled": false}`) } 
+	create 	(user,amt,toId) {return this.rest("post",  "transactions",{user, amount: Number(amt), toId} ) } 
+	process	(receipt) 		{return this.rest("patch", `transactions/${receipt}`, {handled: true} ) } 
+	info	(receipt) 		{return this.rest("get",   `transactions/${receipt}` ) } 
+	reject	(receipt) 		{
+		return this.rest("patch", `transactions/${receipt}`, {handled: true} ).then(res=> this.rest("post",  "transactions",{user: res.user, amount: Number(res.amount), toId: res.from.id} ))
 	}
 }
---
+++ core/commands/economy/exchange.js
@@ -0,0 +1,51 @@
+const Discoin = require('../../archetypes/Discoin');
+const ECO = require('../../archetypes/Economy');
+const CFG = require('../../../config.json');
+const DCN = new Discoin(CFG.dicoin);
+
+const init = async function (msg){
+
+    const Rates = JSON.parse(await DCN.rates());
+    
+    let empty = {name:'\u200b',value:'\u200b',inline:true}
+
+    const embed = {fields:[]};
+console.log({Rates})
+    let RBN = Rates.find(r=>r.id === "RBN")
+
+    embed.description = _emoji('RBN')+"**1 Rubine `RBN` =**"
+
+    Rates.filter(r=>r.id!="RBN").forEach(curr=>{
+        
+        embed.fields.push({name: curr.name, value: `\`${curr.id}\` ${Math.round(RBN.value/curr.value)||(RBN.value/curr.value).toFixed(2)}` , inline: true})
+    }) 
+    while(embed.fields.length % 3) embed.fields.push(empty);
+
+    embed.fields.push({
+        name:"\u200b",
+        value: "```js\n"+
+`${msg.prefix}exg 100 XYZ\n`+"```*example usage*",
+        inline: true
+    })
+    embed.fields.push(empty);
+    embed.fields.push({
+        name:"Tax information:",
+        value: "Exchange Tax: **15%**\nIncome Tax: **12%**\nTransfer Tax: **5%**",
+        inline: true
+    })
+    //embed.thumbnail = {url:"https://cdn.discordapp.com/attachments/488142034776096772/674882674287968266/piechart.gif"}
+    embed.thumbnail = {url:"https://cdn.discordapp.com/attachments/488142034776096772/674882599956643840/abacus.gif"}
+    embed.color = 0xff3355
+    return {embed};
+
+}
+
+module.exports={
+    init
+    ,pub:true
+    ,cmd:'exchange'
+    ,perms:3
+    ,cat:'economy'
+    ,botPerms:['attachFiles','embedLinks']
+    ,aliases:['discoin','convert','exg']
+}
