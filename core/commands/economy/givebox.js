97e9299d455aa43e7c93e9f48d0c326d40f4c42b
Tsumari <flicky@pollux.gg>
2020-02-28 10:26:49 -0300

    use CDN

--- core/commands/economy/givebox.js
+++ core/commands/economy/givebox.js
@@ -81,7 +81,7 @@ const init = async function(msg, args) {
       embed.thumbnail = {};
       embed.image = {
         url:
-          "https://beta.pollux.gg/build/TRANSFER_BOX_timeout_1.gif"
+        paths.CDN+"/build/TRANSFER_BOX_timeout_1.gif"
       };
       embed.footer.text = "🕑";
       prompt.edit({ embed });
@@ -93,7 +93,7 @@ const init = async function(msg, args) {
       embed.thumbnail = {};
       embed.image = {
         url:
-          "https://beta.pollux.gg/build/TRANSFER_BOX_nope_4.gif"
+        paths.CDN+"/build/TRANSFER_BOX_nope_4.gif"
       };
       embed.footer.text = "❌";
       prompt.edit({ embed });
@@ -155,7 +155,7 @@ const init = async function(msg, args) {
           embed.color = 0x2deb88;
           embed.image = {
             url:
-              "https://beta.pollux.gg/build/TRANSFER_BOX_1.gif"
+            paths.CDN+"/build/TRANSFER_BOX_1.gif"
           };
           prompt.edit({ embed });
           return true;
@@ -174,7 +174,7 @@ const init = async function(msg, args) {
             `;
         embed.image = {
           url:
-            "https://beta.pollux.gg/build/TRANSFER_BOX_nope_4.gif"
+          paths.CDN+"/build/TRANSFER_BOX_nope_4.gif"
         };
         prompt.edit({ embed });
         return false;
