25c60f08b6dfef8a62222b7689d79acc2fe9953a
Bsian <chharry321@gmail.com>
2020-04-19 02:33:42 +0100

    Remove superagent

--- core/utilities/BooruGetter.js
+++ core/utilities/BooruGetter.js
@@ -1,37 +1,31 @@
 "use strict";
 
-const sa = require("superagent");
+const axios = require('axios');
 const parseString = require('xml2js').parseString;
 
 exports.get = function (limit, pid, tags) {
     return new Promise((resolve, reject) => {
         var url = `https://safebooru.org/index.php?page=dapi&s=post&q=index&limit=${limit}&pid=${pid}&tags=${tags}`;
-        sa.post(url).end(function (err, res) {
-            if (err) return reject(err);
-            resolve(res);
-        });
+        axios.post(url).then(res => resolve(res), err => reject(err));
     });
 };
 
 exports.getLewd = function (limit, pid, tags) {
     return new Promise((resolve, reject) => {
         var url = `http://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=${limit}&pid=${pid}&tags=${tags}`;
-        sa.post(url).end(function (err, res) {
-            if (err) return reject(err);
-            resolve(res);
-        });
+        axios.post(url).then(res => resolve(res), err => reject(err));
     }); 
 };
 
 exports.getRandom = function (tags) {
     return new Promise((resolve, reject) => {
-        exports.get(1, 0, tags).then(function (data) {
-            let cleanedString = data.text.replace("\ufeff", "");
+        exports.get(1, 0, tags).then(function ({data}) {
+            let cleanedString = data.replace("\ufeff", "");
             parseString(cleanedString, function (err, result) {
                 if (err) return reject(err);
                 let randomPid = Math.floor(Math.random() * result.posts.$.count);
-                exports.get(1, randomPid, tags).then(function (data) {
-                    let cleanedString = data.text.replace("\ufeff", "");
+                exports.get(1, randomPid, tags).then(function ({data}) {
+                    let cleanedString = data.replace("\ufeff", "");
                     parseString(cleanedString, function (err, result) {
                         if (err) return reject(err);
                         try {
@@ -49,13 +43,13 @@ exports.getRandom = function (tags) {
 
 exports.getRandomLewd = function (tags) {
     return new Promise((resolve, reject) => {
-        exports.getLewd(1, 0, tags).then(function (data) {
-            let cleanedString = data.text.replace("\ufeff", "");
+        exports.getLewd(1, 0, tags).then(function ({data}) {
+            let cleanedString = data.replace("\ufeff", "");
             parseString(cleanedString, function (err, result) {                
                 if (err) return reject(err);
                 let randomPid = Math.floor(Math.random() * result.posts.$.count / (result.posts.$.count>20001?20000:1));
-                exports.getLewd(1, randomPid, tags).then(function (data) {
-                    let cleanedString = data.text.replace("\ufeff", "");
+                exports.getLewd(1, randomPid, tags).then(function ({data}) {
+                    let cleanedString = data.replace("\ufeff", "");
                     parseString(cleanedString, function (err, result) {
                         if (err) return reject(err);
                         try {
