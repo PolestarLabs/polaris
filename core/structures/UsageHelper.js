852fdde623857659169dccebbe20034b37a6887e
Mitchell <apps+gitlab@mitchells.work>
2020-05-29 15:20:15 +0000

    UsageHelper - remove exchange code

--- core/structures/UsageHelper.js
+++ core/structures/UsageHelper.js
@@ -26,7 +26,7 @@ exports.run = function run(cmd, m, third, extras) {
   } catch (e) {
     emb.setColor("#eee")
   }
- 
+
   emb.footer(`${v.mod}: ${v.name} | ${third.toUpperCase()} > ${cmd}`, "https://img.icons8.com/"+ICONSET+"/250/puzzle.png");
   emb.author($t("help.commUsage", {
     lngs: m.lang,
@@ -45,32 +45,19 @@ exports.run = function run(cmd, m, third, extras) {
     prefix: m.prefix,
     command: cmd,
     prepend: i18n.exists("commands:usage." + cmd)?'':"`{{prefix}}{{command}}` -",
-    websiteRoot: paths.CDN, 
+    websiteRoot: paths.CDN,
     squad: "- PurpleCat\n - Shamisu\n - Pollyanna\n - Kurono\n - Yuki\n - Celeste"
   }), false);
-  
+
   extras.aliases?emb.field("Aliases:",extras.aliases.map(y=>`\`${y}\``).join(' '),false):false;
 console.log({extras})
   if(extras.related){
-    emb.field("**" + $t(["dict.seeAlso","See Also"], {lngs: m.lang}) + "**", 
+    emb.field("**" + $t(["dict.seeAlso","See Also"], {lngs: m.lang}) + "**",
       extras.related.map(ex=> `\n\u200b • \`${m.prefix}${ex}\``).join('')
     , false)
   }
 
-  
 
-  if (cmd == "exchange") {
-    
-    let litzka = "\u200b"
-    
-    for (i in coinbase) {
-      if (i != "DISCOIN" && i != "RBN") litzka += "  `" + i + "`  " + coinbase[i].icon + `  **${coinbase[i].bot}**  ${coinbase[i].name}` + "\n"
-    };
-
-    emb.field("\u200b", litzka, false)
-
-  }
-  
   if (third === "lang") {
     emb.field("**" + $t("usage.speakAvailable", {
       lngs: m.lang
