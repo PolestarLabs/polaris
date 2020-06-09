81e60fcf756c668e08496ad1bf2a4510eb21ce89
Mitchell3514 <10900096+Mitchell3514@users.noreply.github.com>
2020-06-09 16:37:14 +0200

    feat(Discoin): allow custom fetching filter

--- core/archetypes/Discoin.js
+++ core/archetypes/Discoin.js
@@ -1,6 +1,6 @@
 const request 	= require("request");
 const rescodes 	= [200, 201, 400, 401, 403];
-const ROOT 		= "https://discoin.zws.im"
+const ROOT 		= "https://discoin.zws.im";
 
 module.exports = class Discoin {
 	constructor (token){
@@ -9,27 +9,27 @@ module.exports = class Discoin {
 			if(typeof payload === 'string') query = payload, payload = null;
 			return new Promise((resolve,reject)=>{
 				(request[METHOD])({url: `${ROOT}/${route}${query?"?s="+encodeURIComponent(query):""}`, headers: {"Authorization": "Bearer " + this.token}, json: payload }, (err, res, body) => {
-					if (err || rescodes.indexOf(res.statusCode) === -1)		
+					if (err || rescodes.indexOf(res.statusCode) === -1)
 						return reject(`[${res.statusCode}] :: API failure`);
 					if ( ![200,201].includes(res.statusCode) )
-						return reject({body,request: `${METHOD.toUpperCase()}: /${route}`,payload});			
+						return reject({body,request: `${METHOD.toUpperCase()}: /${route}`,payload});
 					return resolve(body);
-				})
+				});
 			});
 		};
 		this.currencies = () => new Promise((resolve,reject)=>{
 			request.get({url:"https://pollux.gg/api/discoin/currencies"}, (err,res,body)=>{
 				if ( res.statusCode === 200 ) resolve(JSON.parse(body));
 				else reject(res.statusCode);
-			})
-		})
-	}	
-	rates	() 				{return this.rest("get",   `currencies` ) } 
-	fetch	() 				{return this.rest("get",   "transactions",`{"to.id": "RBN", "handled": false}`) } 
-	create 	(user,amt,toId) {return this.rest("post",  "transactions",{user, amount: Number(amt), toId} ) } 
-	process	(receipt) 		{return this.rest("patch", `transactions/${receipt}`, {handled: true} ) } 
-	info	(receipt) 		{return this.rest("get",   `transactions/${receipt}` ) } 
-	reject	(receipt) 		{
-		return this.rest("patch", `transactions/${receipt}`, {handled: true} ).then(res=> this.rest("post",  "transactions",{user: res.user, amount: Number(res.amount), toId: res.from.id} ))
+			});
+		});
 	}
-}
+	rates	() 				{return this.rest("get",   `currencies` ); }
+	fetch	(filter) 				{return this.rest("get", "transactions", filter ? filter : `{"to.id": "RBN", "handled": false}`); } 
+	create 	(user,amt,toId) {return this.rest("post",  "transactions",{user, amount: Number(amt), toId} ); }
+	process	(receipt) 		{return this.rest("patch", `transactions/${receipt}`, {handled: true} ); }
+	info	(receipt) 		{return this.rest("get",   `transactions/${receipt}` );}
+	reject	(receipt) 		{
+		return this.rest("patch", `transactions/${receipt}`, {handled: true} ).then(res=> this.rest("post",  "transactions",{user: res.user, amount: Number(res.amount), toId: res.from.id} ));
+	}
+};
