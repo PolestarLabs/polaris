4a1816bf600c9805fa6faac6b9022cc26e566656
Tsumari <lucasflicky@hotmail.com>
2020-06-14 17:33:51 -0300

    [FIX] profiled bots param

--- core/commands/social/profile.js
+++ core/commands/social/profile.js
@@ -97,7 +97,7 @@ const TEXT={
 
 const {performance} = require('perf_hooks');
 
-init = async (msg)=>{
+init = async (msg,args)=>{
   msg.runtime_internal = performance.now();
   
   let startimer = Date.now();
@@ -138,7 +138,7 @@ init = async (msg)=>{
     return;
   }  
   // NORMAL PROFILE -->
-  const Target = await PLX.getTarget(msg,0,false,true,{force:true});
+  const Target = await PLX.getTarget(args[0]);
   console.log({Target})
   let Target_Database = await DB.users.get({id:Target.id});
   
@@ -162,6 +162,7 @@ try{
   //                            Gather Images
   //=========================================
 
+    const PFLD = Target_Database?.profiled;
     let img={};
     img.defaultAvi   = Picto.getCanvas( "https://cdn.discordapp.com/embed/avatars/0.png" );
     img.mainframe    = Picto.getCanvas(paths.CDN + "/build/profile/"        + (Target.bot ? PFLD ? "mainframe_botpart" : "mainframe_bot" : "mainframe-nex") + ".png");
