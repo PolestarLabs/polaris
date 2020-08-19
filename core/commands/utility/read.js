37d700897d9a5f9792811e3ef5780e3e58fcfaab
Tsumari <lucasflicky@hotmail.com>
2020-08-19 15:08:13 -0300

    [critical fix] fix IP leak vuln

--- core/commands/utility/read.js
+++ core/commands/utility/read.js
@@ -5,9 +5,11 @@ const Vision = require("@google-cloud/vision/");
 const cmd = "read";
 const init = async function (message, cmdPiece = false) {
   return new Promise(async (resolve) => {
-    i2b(message.args[0], async (err, img) => {
-      if (err) {
-        const nwurl = await PLX.getChannelImg(message);
+    let url = "https://proxy.pollux.workers.dev/?pollux_url="+encodeURIComponent(message.args[0]);
+    i2b(url, async (err, img) => {
+      if (err) { 
+        let nwurl = await PLX.getChannelImg(message);
+        if(nwurl.includes("cdn.discordapp")) nwurl = decodeURIComponent(nwurl.replace('https://proxy.pollux.workers.dev/?pollux_url=',''));
         if (!nwurl) return message.channel.send("`INVALID IMAGE URL`");
         return i2b(nwurl, (err, b64) => resolve(vere(b64.base64, message, cmdPiece)));
       }
