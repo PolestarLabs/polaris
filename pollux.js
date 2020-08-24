ef073185686ca47d92b92cb848940fd7a3439e3e
Tsumari <lucasflicky@hotmail.com>
2020-08-24 01:24:01 -0300

    receive voice states

--- pollux.js
+++ pollux.js
@@ -52,7 +52,7 @@ global.PLX = new Eris.CommandClient(cfg.token, {
   defaultImageSize: 512,
   restMode: true,
   defaultImageFormat: "png",
-  intents: 5639,
+  intents: 5767,
   disableEvents: {
     TYPING_START: true,
     TYPING_STOP: true,
