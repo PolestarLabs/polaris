10635086f8941dc5fc01704801d6019c05c05bd9
Bsian <chharry321@gmail.com>
2020-09-28 12:01:43 +0100

    fix(clear): show error if failed

--- core/commands/moderation/clear.js
+++ core/commands/moderation/clear.js
@@ -1,6 +1,6 @@
 const init = async function (msg) {
   const P = { lngs: msg.lang, prefix: msg.prefix };
-  if (PLX.autoHelper([$t("helpkey", P)], { cmd: this.cmd, msg, opt: this.cat })) return;
+  if (PLX.autoHelper([$t("helpkey", P)], { cmd: this.cmd, msg, opt: this.cat })) return null;
 
   const ServerDATA = await DB.servers.get(msg.guild.id);
   const modPass = PLX.modPass(msg.member, "manageMessages", ServerDATA, msg.channel);
@@ -11,10 +11,10 @@ const init = async function (msg) {
   const bucket = (await msg.channel.getMessages(AMT, msg.id)).map((m) => m.id);
 
   msg.channel.send("Deleting messages...").then((m) => m.delete());
-  msg.channel.deleteMessages(bucket).then((x) => {
+  return msg.channel.deleteMessages(bucket).then((x) => {
     console.log(x);
     msg.channel.send(`${_emoji("yep")} Deleted **${AMT}** messages`);
-  }).catch((e) => null);
+  });
 };
 module.exports = {
   init,
