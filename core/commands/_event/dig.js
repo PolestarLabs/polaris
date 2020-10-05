9b8943f4b3acaf88738fbf7123e38a1cc5ab4d0e
Tsumari <lucasflicky@hotmail.com>
2020-10-05 03:15:31 -0300

    initial port

--- core/commands/_event/dig.js
+++ core/commands/_event/dig.js
@@ -1,151 +1,214 @@
-const gear = require("../../gearbox.js");
-const paths = require("../../paths.json");
+
+/*
+
+                         __    _                                   
+                    _wr""        "-q__                             
+                 _dP                 9m_     
+               _#P                     9#_                         
+              d#@                       9#m                        
+             d##                         ###                       
+            J###                         ###L                      
+            {###K                       J###K                      
+            ]####K      ___aaa___      J####F                      
+        __gmM######_  w#P""   ""9#m  _d#####Mmw__                  
+     _g##############mZ_         __g##############m_               
+   _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
+  a###""          ,Z"#####@" '######"\g          ""M##m            
+ J#@"             0L  "*##     ##@"  J#              *#K           
+ #"               `#    "_gmwgm_~    dF               `#_          
+7F                 "#_   ]#####F   _dK                 JE          
+]                    *m__ ##### __g@"                   F          
+                       "PJ#####LP"                                 
+ `                       0######_                      '           
+                       _0########_                                   
+     .               _d#####^#####m__              ,              
+      "*w_________am#####P"   ~9#####mw_________w*"                  
+          ""9@#####@M""           ""P@#####@M""                    
+
+
+ VANILLA CODE AHEAD   -   VANILLA CODE AHEAD   -   VANILLA CODE AHEAD   -   VANILLA CODE AHEAD   -   VANILLA CODE AHEAD   -   VANILLA CODE AHEAD 
+
+######################################################################
+######################################################################
+####                                                              ####
+####             THIS IS DIRECT PORT OF OLD CODE                  ####
+####                                                              ####
+####     AND SHOULD _NOT_ BE USED WITH THE NEW CODEBASE           ####
+####                                                              ####
+######################################################################
+####                                                              ####
+####                THINGS TO BE PROPERLY PORTED:                 ####
+####                                                              ####
+####     -- Proper use of async/await                             ####
+####     -- Database: INVENTORY here is handled the old way       ####
+####     -- Timed Command: does not use TimedCommand module       ####
+####                                                              ####
+####                                                              ####
+######################################################################
+######################################################################
+*/
+
 const moment = require("moment");
-const rq = require('request');
-//const locale = require('../../../utils/multilang_b');
-//const mm = locale.getT();
 
-const EV = require('./clockwork/halloween.js');
+const EventData = require("../../archetypes/Events.js");
+const EV = EventData.event_details;
 
-const cmd = 'dig';
 
-const init = async function (message) {
 
-  const P = {
-    lngs: message.lang
-  }
-  let lang = message.lang[0]
-  if (lang == 'cz') lang = 'cs';
-  moment.locale(lang);
+const init = async function (msg) {
+  const P = { lngs: msg.lang };
+  moment.locale(msg.lang[0]);
+
+  const Author = msg.author;
+
+  const USERDATA = await DB.users.findOne({ id: Author.id });
 
   const v = {
-    last: "<:shovel:504412718871085077> "+mm('events:halloween18.dig.lastTomb', P),
-    next: "<:grave:465902357465661442> " +mm('events:halloween18.dig.moreTombs', P),
-  }
+    last:
+      "<:shovel:504412718871085077> " +
+      $t("events:halloween18.dig.lastTomb", P),
+    next:
+      "<:grave:465902357465661442> " +
+      $t("events:halloween18.dig.moreTombs", P),
+  };
 
-
-
-  const Author = message.author
-
-  if (Author.dailing === true) return message.channel.send("There's already a dig request going on!");
+  //if (Author.dailing === true) return message.channel.send("There's already a dig request going on!");
 
   //const STREAK_EXPIRE = 1.296e+8*2
-  const DAY = 2.16e+7  
 
+  const DAY = 2.16e7;
   const now = Date.now();
-  const USERDATA = await gear.userDB.findOne({
-    id: Author.id
-  });
 
-  const eventData = await EV.userData(Author);
   const userDaily = USERDATA.eventDaily || 1;
   const dailyAvailable = now - userDaily >= DAY;
 
+  const embed = {};
+  embed.color = 0xd83668;
 
-
-  const embed = new gear.RichEmbed;
-  embed.setColor("#d83668");
-
-
-  if (message.content.endsWith('info')) {
-    let embe2 = new gear.RichEmbed;
-    embe2.setColor('#e35555')
-    embe2.setDescription(
- "**"+v.last+"** "+moment.utc(userDaily).fromNow()+"\n"+
-v.next+" "+ moment.utc(userDaily).add((DAY/1000/60/60),'hours').fromNow() 
-  )
-    return message.channel.send({
-      embed: embe2
-    });
+  if (msg.content.endsWith("info")) {
+    let infoEmbed = {};
+    infoEmbed.setColor("#e35555");
+    infoEmbed.setDescription(
+      "**" +v.last +"** " +
+        moment.utc(userDaily).fromNow() +"\n" +
+        v.next + " " +
+        moment
+          .utc(userDaily)
+          .add(DAY / 1000 / 60 / 60, "hours")
+          .fromNow()
+    );
+    return msg.channel.send({embed: infoEmbed});
   }
 
-  if(!USERDATA.modules.inventory.includes("shovel")&&!USERDATA.modules.inventory.includes("excavator")){
-    
-    P.SHOVEL_LINK = `**[<:shovel:504412718871085077> ${mm("events:halloween18.dig.shovel",P)}](https://pollux.amarok.kr/crafting#shovel "Pollux Website: Crafting Guide")**`
-    embed.setDescription(":no_entry_sign: "+mm("events:halloween18.dig.needShovel",P))
-    
-    return message.reply({embed});
+  if (
+    !USERDATA.modules.inventory.includes("shovel") &&
+    !USERDATA.modules.inventory.includes("excavator")
+  ) {
+    P.SHOVEL_LINK = `**[<:shovel:504412718871085077> ${$t(
+      "events:halloween18.dig.shovel",
+      P
+    )}](https://pollux.amarok.kr/crafting#shovel "Pollux Website: Crafting Guide")**`;
+    embed.setDescription(
+      ":no_entry_sign: " + $t("events:halloween18.dig.needShovel", P)
+    );
+
+    return msg.reply({ embed });
   }
-  
-  
-  
 
   if (!dailyAvailable && Author.id != "88120564400553984z") {
-
     let r = userDaily + DAY;
-    P.Xtime = moment.utc(r).fromNow(true)
-    let dailyNope = mm('events:halloween18.dig.cooldown', P);
-    message.reply(gear.emoji('nope') + dailyNope);
-    let embe2 = new gear.RichEmbed;
-    embe2.setColor('#e35555');
-    embe2.setDescription(`
-${gear.emoji('time')   } **${v.last}** ${ moment.utc(userDaily).fromNow()}
-    `);
+    P.Xtime = moment.utc(r).fromNow(true);
+    let dailyNope = $t("events:halloween18.dig.cooldown", P);
+    msg.reply(_emoji("nope") + dailyNope);
+    let availabilityEmbed = {};
+    availabilityEmbed.color = 0xe35555;
+    availabilityEmbed.description = `
+${_emoji("time")} **${v.last}** ${moment.utc(userDaily).fromNow()}
+    `;
 
-     message.channel.send({
-      embed: embe2
-    });
+    msg.channel.send({embed: availabilityEmbed});
     shovelocalypse();
     return;
+  }
 
-  };
+  await DB.users.set(Author.id, { $set: {eventDaily: now}  });
 
-  await gear.userDB.set(Author.id, {
-    $set: {
-      "eventDaily": now
+  if (USERDATA.modules.inventory.includes("excavator")) {
+    await DB.users
+      .updateOne(
+        { id: Author.id },
+        {
+          $inc: {
+            eventDaily: +((DAY / 6) * 4),
+            "eventData.halloween18.caskets": 2,
+          },
+        }
+      )
+      .lean()
+      .exec();
+    const gotCasket =
+      "You excavated 2 Caskets! Next Excavation will be available in 10 hours!";
+    msg.channel.send(gotCasket);
+  } else {
+    await DB.users
+      .updateOne(
+        { id: Author.id },
+        { $inc: { "eventData.halloween18.caskets": 1 } }
+      )
+      .lean()
+      .exec();
+    const gotCasket =
+      "<:shovel:504412718871085077> " +
+      $t("events:halloween18.caskets.gotCasket", P);
+    msg.channel.send(gotCasket);
+  }
+
+  shovelocalypse();
+
+  async function shovelocalypse() {
+    if (USERDATA.modules.inventory.includes("excavator")) {
+      if (randomize(0, 100) == 13) {
+        await DB.users
+          .updateOne(
+            { id: Author.id, "modules.inventory": "excavator" },
+            { $set: { "modules.inventory.$": "broken_excavator" } }
+          )
+          .lean()
+          .exec();
+        msg.channel.send(
+          "Something went wrong... Your excavator poofed! :( "
+        );
+      }
+
+      return;
     }
-  });
-  
-  if(USERDATA.modules.inventory.includes("excavator")){
-   await gear.userDB.updateOne({id:Author.id},{$inc:{"eventDaily":+(DAY/6*4),"eventData.halloween18.caskets":2}}).lean().exec();
-  const gotCasket = "You excavated 2 Caskets! Next Excavation will be available in 10 hours!"
-  message.channel.send(gotCasket);
-  
-  }else{
-   await gear.userDB.updateOne({id:Author.id},{$inc:{"eventData.halloween18.caskets":1}}).lean().exec();
-  const gotCasket = "<:shovel:504412718871085077> "+ mm("events:halloween18.caskets.gotCasket",P)
-  message.channel.send(gotCasket);
-    
+
+    if (randomize(0, 20) == 4) {
+      await DB.users
+        .updateOne(
+          { id: Author.id, "modules.inventory": "shovel" },
+          { $set: { "modules.inventory.$": null } }
+        )
+        .lean()
+        .exec();
+      await DB.users
+        .updateOne({ id: Author.id }, { $pull: { "modules.inventory": null } })
+        .lean()
+        .exec();
+      msg.channel.send(
+        rand$t("responses.verbose.interjections.ohmy_negative", mm, P) +
+          $t("events:halloween18.dig.shovelbroke", P)
+      );
+    }
   }
-  
-  
-shovelocalypse();
-
-
-
-
-async function shovelocalypse(){
-  
-  if(USERDATA.modules.inventory.includes("excavator")){
-    
-     
-  if( gear.randomize(0,100)==13){    
-    await gear.userDB.updateOne({id:Author.id,"modules.inventory":"excavator"},{$set:{"modules.inventory.$":"broken_excavator"}}).lean().exec();
-    message.channel.send("Something went wrong... Your excavator poofed! :( ");
-  }
-    
-    return;
-  };
-  
-  if( gear.randomize(0,20)==4){    
-    await gear.userDB.updateOne({id:Author.id,"modules.inventory":"shovel"},{$set:{"modules.inventory.$":null}}).lean().exec();
-    await gear.userDB.updateOne({id:Author.id},{$pull:{"modules.inventory":null}}).lean().exec();
-    message.channel.send( gear.mmrand('responses.verbose.interjections.ohmy_negative', mm, P)+ mm("events:halloween18.dig.shovelbroke",P));
-  }
-
-}
-
-
-
-}
-
-module.exports = {
-  pub: true,
-  cmd: cmd,
-  perms: 3,
-  botperms: ["EMBED_LINKS", "SEND_MESSAGES", "ATTACH_FILES"],
-  init: init,
-  cat: 'event',
-  exp: 15,
-  cool: 1000
 };
+
+ 
+module.exports={
+  init
+  ,pub:true
+  ,cmd:'dig'
+  ,cat:'_event'
+  ,botPerms:['attachFiles','embedLinks']
+  ,aliases:[]
+}
