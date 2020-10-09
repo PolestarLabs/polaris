ff561df3201c5c784853838a5e482a7e5b7fba1f
Tsumari <lucasflicky@hotmail.com>
2020-10-09 07:26:07 -0300

    split feeds outside main file

--- core/subroutines/cronjobs.js
+++ core/subroutines/cronjobs.js
@@ -1,17 +1,7 @@
-const axios             = require("axios");
+
 const moment            = require("moment");
-const RSS               = require("rss-parser");
+
 const { CronJob }       = require("cron");
-
-const cfg               = require(`${appRoot}/config.json`);
-const RSSembedGenerator = require("../commands/utility/rss.js").embedGenerator;
-
-const parser = new RSS();
-const tubeParser = new RSS({
-  customFields: {
-    item: [["media:group", "media"]],
-  },
-});
 
 const userDB = DB.users;
 
@@ -137,100 +127,11 @@ exports.run = async function run() {
   const FIFTEENminute = new CronJob("*/1 * * * *", async () => {
     PLX.updateBlacklists(DB);
 
-    (async () => {
-      DB.feed.find({ server: { $in: PLX.guilds.map((g) => g.id) } }).lean().exec().then((serverFeeds) => {
-        serverFeeds.forEach(async (feed) => {
-          const serverData = await DB.servers.get(feed.server);
+    delete require.cache[require.resolve('./feeds')];
+    const feeds = require('./feeds');
 
-          if (feed.type === "rss") {
-            const data = await parser.parseURL(feed.url).timeout(950).catch(() => null);
-            if (!data) return;
-            data.items = data.items.filter((x) => x.link.startsWith("http"));
-            if (feed.last.guid !== data.items[0]?.guid) {
-              const embed = await RSSembedGenerator(data.items[0], data);
-              await DB.feed.updateOne(
-                { server: feed.server, url: feed.url },
-                { $set: { last: data.items[0], thumb: embed.thumbnail.url } },
-              ).catch(() => null);
-
-              PLX.getChannel(feed.channel).send({ embed });
-            }
-          }
-
-          if (feed.type === "twitch") {
-            const thisFeed = feed;
-
-            const response = await axios.get(
-              `https://api.twitch.tv/helix/streams?user_login=${thisFeed.url}`,
-              { headers: { "User-Agent": "Pollux@Polaris.beta-0.1", "Client-ID": cfg.twitch } },
-            ).timeout(3000).catch(() => null);
-
-            if (!response) return;
-            const StreamData = response.data?.data[0];
-
-            if (!StreamData) return;
-            if (
-              !(
-                thisFeed.last.type === StreamData.type
-                && thisFeed.last.title === StreamData.title
-                && thisFeed.last.started_at === StreamData.started_at
-              )
-            ) {
-              const res = await axios.get(
-                `https://api.twitch.tv/helix/users?login=${thisFeed.url}`,
-                { headers: { "User-Agent": "Pollux@Polaris.beta-0.1", "Client-ID": cfg.twitch } },
-              ).timeout(3000).catch(() => null);
-              if (!res) return;
-              const streamer = res.data[0] || res.data.data[0];
-              const embed = new Embed();
-              embed.thumbnail(streamer.profile_image_url);
-              embed.author(StreamData.title);
-              embed.image(StreamData.thumbnail_url.replace("{width}", "400").replace("{height}", "240"));
-              embed.timestamp(StreamData.started_at);
-              embed.color("#6441A4");
-              const P = { lngs: [serverData.modules.LANGUAGE || "en", "dev"], streamer: streamer.display_name };
-              const ping = thisFeed.pings || thisFeed.pings || "";
-
-              await DB.feed.updateOne(
-                { server: thisFeed.server, url: thisFeed.url },
-                { $set: { last: StreamData, thumb: streamer.profile_image_url } },
-              ).catch(console.error);
-
-              PLX.getChannel(thisFeed.channel).send({
-                content: `${ping}${$t("interface.feed.newTwitchStatus", P)} <https://twitch.tv/${streamer.login}>`,
-                embed,
-              });
-            }
-          }
-
-          if (feed.type === "youtube") {
-            const thisFeed = feed;
-            const { ytEmbedCreate } = require("../commands/utility/ytalert.js");
-            const data = await tubeParser.parseURL(
-              `https://www.youtube.com/feeds/videos.xml?channel_id=${thisFeed.url}`,
-            ).timeout(3120).catch(() => null);
-
-            if (thisFeed.last.link !== data?.items[0]?.link) {
-              const embed = await ytEmbedCreate(data.items[0], data);
-              const P = { lngs: [serverData.modules.LANGUAGE || "en", "dev"] };
-              P.tuber = data.items[0].author;
-              const LastVideoLink = `
-              ${$t("interface.feed.newYoutube", P)}
-              ${data.items[0].link}`;
-              data.items[0].media = null;
-              await DB.feed.updateOne(
-                { server: thisFeed.server, url: thisFeed.url },
-                { $set: { last: data.items[0], thumb: embed.thumbnail.url } },
-              ).catch(console.error);
-              const ping = thisFeed.pings || thisFeed.pings || "";
-              PLX.getChannel(thisFeed.channel).send(
-                { content: ping + LastVideoLink },
-              ).then((m) => m.channel.send({ embed })).catch(() => null);
-            }
-          }
-        });
-      });
-    })();
+    feeds.check();
+   
   });
 
   const ONEminute = new CronJob("*/1 * * * *", async () => {
