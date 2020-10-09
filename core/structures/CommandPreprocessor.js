bc22f9d2b2cbbcf6bca2f6ac873314bcf9e23dc1
Bsian <chharry321@gmail.com>
2020-10-10 00:21:53 +0100

    fix(CommandLoader): Filter unique permissions

--- core/structures/CommandPreprocessor.js
+++ core/structures/CommandPreprocessor.js
@@ -139,7 +139,8 @@ const registerOne = (folder, _cmd) => {
     PLX.commands[CMD.label].related = commandFile.related;
     PLX.commands[CMD.label].helpImage = commandFile.helpImage;
     PLX.commands[CMD.label].module = folder;
-    PLX.commands[CMD.label].botPerms = ["attachFiles", "embedLinks", "externalEmojis"].concat(commandFile.botPerms || []);
+    PLX.commands[CMD.label].botPerms = ["attachFiles", "embedLinks", "externalEmojis"]
+      .concat(commandFile.botPerms || []).filter((v, i, a) => a.indexOf(v) === i);
     if (commandFile.subs) {
       commandFile.subs.forEach((sub) => {
         delete require.cache[require.resolve(`${CMD_FOLDER}/${folder}/${_cmd}/${sub}`)];
