36d36646b26e7c1e67de76eb7e38d32c81382852
Bsian <chharry321@gmail.com>
2020-10-10 16:13:37 +0100

    fix(choose): First input parsing

--- core/commands/utility/choose.js
+++ core/commands/utility/choose.js
@@ -1,8 +1,8 @@
 const init = async function (msg) {
   const P = { lngs: msg.lang, prefix: msg.prefix };
 
+  const target = Number.isNaN(msg.args[0]) ? 1 : parseInt(msg.args.shift());
   let trueArgs = msg.args.join(" ").split(/, ?/g);
-  const target = Number.isNaN(trueArgs[0]) ? 1 : parseInt(msg.args.shift());
 
   const rolefind = (x) => msg.guild.roles.find((rl) => msg.args.slice(x).join(" ").toLowerCase() === rl.name.toLowerCase());
   if (
