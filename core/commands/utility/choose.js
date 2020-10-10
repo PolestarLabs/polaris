4e2231df9012479553382a681e4a9c67630ae4e1
Bsian <chharry321@gmail.com>
2020-10-10 16:19:32 +0100

    fix(choose): Number detection logic

--- core/commands/utility/choose.js
+++ core/commands/utility/choose.js
@@ -1,7 +1,7 @@
 const init = async function (msg) {
   const P = { lngs: msg.lang, prefix: msg.prefix };
 
-  const target = Number.isNaN(msg.args[0]) ? 1 : parseInt(msg.args.shift());
+  const target = Number.isNaN(Number(msg.args[0])) ? 1 : parseInt(msg.args.shift());
   let trueArgs = msg.args.join(" ").split(/, ?/g);
 
   const rolefind = (x) => msg.guild.roles.find((rl) => msg.args.slice(x).join(" ").toLowerCase() === rl.name.toLowerCase());
