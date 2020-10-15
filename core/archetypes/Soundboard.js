cc9f78629d3d95a55adb2d1ecec515043b913815
Tsumari <lucasflicky@hotmail.com>
2020-10-14 21:33:33 -0300

    SB: random functionalities

--- core/archetypes/Soundboard.js
+++ core/archetypes/Soundboard.js
@@ -9,14 +9,22 @@ const play = async function (msg,SOUND,options) {
     PLX.joinVoiceChannel(msg.member.voiceState.channelID)
         .catch((err) => msg.channel.send("Please join a voice channel first!") )
         .then((voiceChannel) => {
-        if (voiceChannel.playing) voiceChannel.stopPlaying();
+            voiceChannel.dontLeave = false;
+            if (voiceChannel.playing) {
+                voiceChannel.dontLeave = true;
+                voiceChannel.stopPlaying();
+            };
         
-        voiceChannel.play(SOUND);        
+        voiceChannel.play(SOUND);
         if (playingMessage) msg.channel.send( playingMessage );  
 
-        voiceChannel.once("end", () => {
-            if (exitMessage) msg.channel.send( exitMessage );
-            PLX.leaveVoiceChannel(msg.member.voiceState.channelID)
+        voiceChannel.on("end", () => {
+            if(!voiceChannel.dontLeave){
+                if (exitMessage) msg.channel.send( exitMessage );
+                PLX.leaveVoiceChannel(msg.member.voiceState.channelID)
+            }else{
+
+            }
         });
     });
 
