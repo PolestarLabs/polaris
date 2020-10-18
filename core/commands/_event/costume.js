6cb07aa1c79346f9cd8d0800a9b1e4e071e9a506
Tsumari <lucasflicky@hotmail.com>
2020-10-17 23:18:08 -0300

    costume display 2020 gfx

--- core/commands/_event/costume.js
+++ core/commands/_event/costume.js
@@ -91,8 +91,6 @@ const init = async function (msg){
     }
   }  
 
-
-
   if (msg.args[0] == "unequip"){
     if ( Number(msg.args[1]) <= eventData.inventory.length ){
 
@@ -128,10 +126,10 @@ const init = async function (msg){
   
   if (msg.args[0] == "set" || msg.args[1] == "gender"){
     
-    let resolve = msg.args[2]
+    let resolve = msg.args[2] || msg.args[1]
     if (!resolve) return msg.reply("Missing parameter");
     if(["girl","female","gal","woman"].includes(resolve.toLowerCase()))
-          await DB.users.set(Author.id,{$set: {"eventData.halloween18.gender": "fem"}});
+          await DB.users.set(Author.id,{$set: {"eventData.halloween18.gender": "girl"}});
     else if (["boy","male","guy","man"].includes(resolve.toLowerCase()))
           await DB.users.set(Author.id,{$set: {"eventData.halloween18.gender": "boy"}});
     else
@@ -143,8 +141,7 @@ const init = async function (msg){
     }
   
 
-  
-let base = eventData.gender || "fem";
+let base = eventData.gender || "girl";
 //let skin = [message.args[0]||false,message.args[1]||false,message.args[2]||false]
 let equipH = (eventData.inventory||[]).find(x=>x.id == eventData.head)||{};
 let equipB = (eventData.inventory||[]).find(x=>x.id == eventData.body)||{};
@@ -167,14 +164,15 @@ let totalSpook =  equippeds.length > 0 ? setBonus + equippeds.reduce((a,b)=>a+b)
 
  
 if(Target.id == "271394014358405121"){
-  equipH = {rarity:"XR",name:"Fancy Lace Headpiece" , id:"---"}
-  equipB = {rarity:"XR",name:"Cute Maid Dress" , id:"---"}
-  equipF = {rarity:"XR",name:"Techy Robot Legs" , id:"---"}
+  equipH = {rarity:"XR",name:"Fancy Lace Headpiece" , id:"---", aspectBonus: 40, spook: 100}
+  equipB = {rarity:"XR",name:"Cute Maid Dress" , id:"---", aspectBonus: 40, spook: 100}
+  equipF = {rarity:"XR",name:"Techy Robot Legs" , id:"---", aspectBonus: 40, spook: 100}
   totalSpook = 1000
 }
 
 
 let spookPowa = $t("events:halloween18.keywords.spookPowa",P)
+/*
 embed.fields = [
   { name: "**`🎩 Head  \u200b`**\u200b*`"+`[${equipH.spook||0}](+${equipH.aspectBonus||0})\`*`,
     value: equipH.id?_emoji(equipH.rarity)+equipH.name:"---",
@@ -196,10 +194,13 @@ embed.fields = [
     inline: true
   }
 ]
+*/
 embed.author = {name: Target.tag+"'s Outfit", avatar_url: Target.avatarURL };
+embed.description = `\`${msg.prefix}costume equip [#]\` Equip from Wardrobe
+\`${msg.prefix}costume unequip [head,body,feet]\` Remove one item
+\`${msg.prefix}costume set [boy,girl]\` Change character`
 
-
-const canvas = Picto.new(400, 200);
+const canvas = Picto.new(850, 600);
 const ctx = canvas.getContext('2d');  
 ///================= 
 
@@ -210,43 +211,173 @@ let skin = [
   equipF.costume
 ]
 
-let top =  skin[0]? `${base}_${skin[0]}_head.png` : "none.png";
-let mid =  skin[1]? `${base}_${skin[1]}_body.png` : "none.png";
-let bot =  skin[2]? `${base}_${skin[2]}_legs.png` : "none.png";
+let top =  skin[0]? `${base}_${skin[0]}_head` : "none";
+let mid =  skin[1]? `${base}_${skin[1]}_body` : "none";
+let bot =  skin[2]? `${base}_${skin[2]}_legs` : "none";
 
 let _base;
 
 if(Target.id=="271394014358405121"){
-  _base = await Picto.getCanvas(oldBuildPath+'event/halloween18/bodies/polluxboo.png');
-   }else{
-  _base = await Picto.getCanvas(oldBuildPath+'event/halloween18/bodies/'+base+"_base.png");     
+  _base = await Picto.getCanvas(paths.Build+'events/halloween20/bodies/poly.png');
+   }else{ 
+  _base = await Picto.getCanvas(paths.Build+'/events/halloween20/bodies/'+base+'_base.png');
 }
 
-const _top =  await Picto.getCanvas(oldBuildPath+'event/halloween18/bodies/' +top );
-const _mid =  await Picto.getCanvas(oldBuildPath+'event/halloween18/bodies/' +mid );
-const _bot =  await Picto.getCanvas(oldBuildPath+'event/halloween18/bodies/' +bot );    
+//TEST
+_frame = await Picto.getCanvas(paths.Build+'/events/halloween20/frame.png');
+_bar = await Picto.getCanvas(paths.Build+'/events/halloween20/rainbow_bar.png');
+
+
+
+const _top =  await Picto.getCanvas(paths.Build+`events/halloween20/bodies/${top}.png` );
+const _mid =  await Picto.getCanvas(paths.Build+`events/halloween20/bodies/${mid}.png` );
+const _bot =  await Picto.getCanvas(paths.Build+`events/halloween20/bodies/${bot}.png` );    
 const _cask =  await Picto.getCanvas(oldBuildPath+'event/halloween18/casket_cls.png' );    
 //const _candy =  await Picto.getCanvas(oldBuildPath+'event/halloween18/casket_cls.png' );    
 const _candy =  await Picto.getCanvas("https://cdn.discordapp.com/emojis/366437119658557440.png" );
 
 let rarCols = {
   XR : "#ff3664",
-  UR : "#b76af0",
-  SR : "#eb90b3",
-  R : "#96bfe8",
-  U : "#efcb86",
+  UR : "#Efa947",
+  SR : "#ba6bff",
+  R : "#3b90ff",
+  U : "#70d473",
   C : "#beb9cc",
 }
-
-const _power =   Picto.tag(ctx,totalSpook,'900 24px "Whitney HTF"',"#CCD");  
-const _tspook =   Picto.tag(ctx,spookPowa,'600 10px "Whitney HTF SC"',"#CCD");  
-const _hat =   Picto.tag(ctx,equipH.name||"---",'400 12px "Corporate Logo Rounded"',rarCols[equipH.rarity]);    
-const _bod =   Picto.tag(ctx,equipB.name||"---",'400 12px "Corporate Logo Rounded"',rarCols[equipB.rarity]);    
-const _leg =   Picto.tag(ctx,equipF.name||"---",'400 12px "Corporate Logo Rounded"',rarCols[equipF.rarity]);    
-const _txcaskr =  Picto.tag(ctx,eventData.caskets||"0",'900 16px "Whitney HTF"',"#CCD");  
-const _txcandy =  Picto.tag(ctx,eventData.candy||"0",'900 16px "Whitney HTF"',"#CCD");  
+ 
 
 
+const BAR_MAX = 223;
+const MAX_SPOOK = 100;
+const MAX_ASPECT = 40;
+const _d = {};
+   _d.H_spook_w     =((equipH.spook||0) / (MAX_SPOOK)) * BAR_MAX;
+   _d.H_aspect_w    = ((equipH.aspectBonus||0) / (MAX_ASPECT) * BAR_MAX);
+
+   _d.B_spook_w     =((equipB.spook||0) / (MAX_SPOOK)) * BAR_MAX;
+   _d.B_aspect_w    = ((equipB.aspectBonus||0) / (MAX_ASPECT) * BAR_MAX);
+
+   _d.F_spook_w     =((equipF.spook||0) / (MAX_SPOOK)) * BAR_MAX;
+   _d.F_aspect_w    = ((equipF.aspectBonus||0) / (MAX_ASPECT) * BAR_MAX);
+
+let rainbow = ctx.createLinearGradient(520, 0, 743, 0 );
+rainbow.addColorStop(0, "#9da0b8");
+rainbow.addColorStop(.3, "#3dfcad");
+rainbow.addColorStop(.4, "#458bDf");
+rainbow.addColorStop(.6, "#a742ff");
+rainbow.addColorStop(.8, "#ffaa42");
+rainbow.addColorStop(1, "#ff304f");
+
+ 
+ctx.fillStyle = rainbow;
+ 
+
+ function drawBars(item, x,y){
+  let widthA = _d[item+"_aspect_w"];    
+  let widthS = _d[item+"_spook_w"];
+  
+  ctx.globalCompositeOperation = 'destination-over'
+  ctx.fillRect(x,y,widthS,25);
+  ctx.fillRect(x,y+50,widthA,25);
+  ctx.globalCompositeOperation = 'source-over'
+  Picto.setAndDraw( ctx,
+    Picto.tag(ctx,eval('equip'+item+'.spook||0'),'600 36px "Panton"',"#DDF")
+    , x-10,y,52,'right')
+  
+  Picto.setAndDraw( ctx,
+    Picto.tag(ctx,eval('equip'+item+'.aspectBonus||0'),'600 36px "Panton"',"#DDF")
+    , x-10,y+38,52,'right')
+  
+ } 
+
+ctx.drawImage(_frame,0,0);
+
+const [x,y,w,h] = [-10,70-85,426,511];
+
+ctx.drawImage(_base,x,y,w,h);
+ctx.drawImage(_bot, x,y,w,h);
+if(equipH.costume !== 'duck') ctx.drawImage(_top, x,y,w,h);  //=====================================//
+ctx.drawImage(_mid, x,y,w,h);                                //   DUCK HAS ISSUES WITH HEAD ORDER   //
+if(equipH.costume === 'duck') ctx.drawImage(_top, x,y,w,h);  //=====================================//
+
+
+drawBars('H',520,115)
+drawBars('B',520,260)
+drawBars('F',520,400)
+
+
+
+
+const _power =   Picto.tag(ctx,totalSpook,'900 36px "Panton Black"',"#CCD");  
+const _tspook =   Picto.tag(ctx,spookPowa,'600 24px "Panton"',"#CCD");  
+
+// DRAW EQUIP
+Picto.popOutTxt(ctx,equipH.name||"---",  
+411,
+75,
+'600 26px "Panton"',rarCols[equipH.rarity],
+320,{style:'#15161b',line:8});
+
+Picto.popOutTxt(ctx,equipB.name||"---",  
+411,
+75+144,
+'600 26px "Panton"',rarCols[equipB.rarity],
+320,{style:'#15161b',line:8});
+
+Picto.popOutTxt(ctx,equipF.name||"---",  
+411,
+70+144*2,
+'600 26px "Panton"',rarCols[equipF.rarity],
+320,{style:'#15161b',line:8});
+
+
+//TEXT
+
+Picto.popOutTxt(ctx,(eventData.caskets||"0").toString().padStart(2,'\u2002'),
+426,523,
+'600 32px "Panton"',"#FFF",
+320,{style:'#15161b',line:8});
+
+
+Picto.popOutTxt(ctx,(eventData.candy||"0").toString().padStart(4,'\u2002'),
+620,519,
+'600 32px "Panton"',"#FFF",
+320,{style:'#15161b',line:8});
+
+////
+ctx.save()
+ctx.rotate(.20534)
+Picto.popOutTxt(ctx,(totalSpook||"0").toString().padStart(3,'\u2002'),  
+180,406,
+'600 80px "Panton Black"',"#FFF",
+320,{style:'#EE6522',line:10},0);
+ctx.restore()
+ctx.save()
+ctx.rotate(-.084)
+Picto.popOutTxt(ctx, spookPowa+"!",  
+50,540,
+'600 32px "Panton Black"',"#FFF",
+320,{style:'#fc5a03',line:8},0);
+ctx.restore()
+
+//RARS
+const [rarH,rarB,rarF] = await Promise.all([
+  equipH.rarity? Picto.getCanvas(paths.CDN+`/build/tiers/mobage/${equipH.rarity}.png`):"",
+  equipB.rarity? Picto.getCanvas(paths.CDN+`/build/tiers/mobage/${equipB.rarity}.png`):"",
+  equipF.rarity? Picto.getCanvas(paths.CDN+`/build/tiers/mobage/${equipF.rarity}.png`):"",
+]);
+
+if (equipH.rarity) ctx.drawImage(rarH, 390-25, 85-25,50,50) ;
+if (equipB.rarity) ctx.drawImage(rarB, 390-25, 85-25+143,50,50) ;
+if (equipF.rarity) ctx.drawImage(rarF, 390-25, 85-25+143+143,50,50) ;
+
+ctx.translate(0,-40)
+
+//drawImage(_base,-10,70);
+
+
+
+/*
 ctx.fillStyle = "#282a31"
 ctx.fillRect(0,0,400,200)
 ctx.fillStyle = "rgba(55, 60, 70, 0.49)"
@@ -259,10 +390,7 @@ x=55
 y=10
 w=125
 h=150
-ctx.drawImage(_base,x,y,w,h)
-ctx.drawImage(_bot, x,y,w,h)
-ctx.drawImage(_mid, x,y,w,h)
-ctx.drawImage(_top, x,y,w,h)
+
 let ww
 let MW = 180
 ctx.drawImage(_tspook.item, _power.width + 40,165)
@@ -278,7 +406,7 @@ ctx.drawImage(_cask, 200,135, 45,45);
 ctx.drawImage(_txcaskr.item,26+  230,150);
 ctx.drawImage(_candy, 300,143, 28,28);
 ctx.drawImage(_txcandy.item,45+ 290,150);
-
+*/
 
 let attc = "attachment://costume.png"
 
