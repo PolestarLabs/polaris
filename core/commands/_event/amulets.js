0774c11832bad8f2a9854f97ab3e8d2fab88b74f
Tsumari <lucasflicky@hotmail.com>
2020-10-25 19:56:58 -0300

    amulet: finish

--- core/commands/_event/amulets.js
+++ core/commands/_event/amulets.js
@@ -4,38 +4,51 @@ const EV = EventData.event_details;
 
 const init = async function (msg){
 
-    const P = { lngs:[msg.lang], prefix: msg.prefix };
+    const P = { lngs:msg.lang, prefix: msg.prefix };
     const eventData = await EV.userData(msg.author);
 
     let userData = await DB.users.getFull(msg.author.id);
 
     let hasTwo = userData.hasItem('ancient_amulet',2); // two?
+    P.emj = "<:amulet:767214978972254239>"
+    P.emj2 = "☯"
 
     if (hasTwo){
-        //exchange x for jiang set blabla;
 
-        const promptEmbed = {
-            description: $t('events:halloween20.amuletExchangePrompt',P)
+        let promptEmbed = {
+            description: $t('events:halloween20.amuletExchangePrompt',P),
+            thumbnail: {url: "https://cdn.discordapp.com/emojis/767214978972254239.png"}
         }
-        const prompt = await msg.channel.send( {embed:promptEmbed} ); // temp
+        const prompt = await msg.channel.send( {embed:promptEmbed} );
 
 
+        await YesNo(prompt,msg,async (_,m)=>{
+            console.log(m)
+            promptEmbed = m.embeds[0];
+            promptEmbed.description = $t("events:halloween18.caskets.tooManyGar",P);
+            if( eventData.inventory?.length > 11) return prompt.edit( {embed: promptEmbed} );
 
-        await YesNo(prompt,msg,()=>{
-
-            if( eventData.inventory?.length > 11) return message.reply(canOnly20);
-            //Author,typ,rar,cos,name,asp,aug
-            let phab = EV.phabricate(Author,null,null,'jiangshi', 'Jiangshi',null,20 );
-            DB.users.set(Author.id,{$push:{"eventData.halloween20.inventory":phab}});
+            let phab = EV.phabricate(msg.author,null,null,'jiangshi', 'Jiangshi',null,20);
             
+            await Promise.all([
+                userData.removeItem('ancient_amulet',2),
+                DB.users.set(msg.author.id,{$push:{"eventData.halloween20.inventory":phab}})
+            ]);
+            
+            P.rar   = _emoji(phab.rarity);
+            P.item  = phab.name + "`"+(phab.spook)+"`";
+            promptEmbed.description = $t('events:halloween20.amuletExchangeSuccess',P);
+            await prompt.edit({embed:promptEmbed})
+
         });
+             
         
 
 
 
 
 
-        return $t('events:hallowen20.amuletExchangeSuccess',P);
+       
     }else{
         return $t('events:hallowen20.amuletCantAfford',P);
     }
