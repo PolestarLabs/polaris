9207fc6dbc364d8145b87d03e614c245d568e364
Mitchell <contact@mitchells.work>
2020-11-08 19:31:22 +0100

    keep old stuff to be added back later

--- core/commands/fun/eurobeat.js
+++ core/commands/fun/eurobeat.js
@@ -1,4 +1,14 @@
 // const gear = require('../../utilities/Gearbox');
+/**
+  _   _ ______ ______ _____   _____  __          ______  _____  _  __
+ | \ | |  ____|  ____|  __ \ / ____| \ \        / / __ \|  __ \| |/ /
+ |  \| | |__  | |__  | |  | | (___    \ \  /\  / / |  | | |__) | ' / 
+ | . ` |  __| |  __| | |  | |\___ \    \ \/  \/ /| |  | |  _  /|  <  
+ | |\  | |____| |____| |__| |____) |    \  /\  / | |__| | | \ \| . \ 
+ |_| \_|______|______|_____/|_____/      \/  \/   \____/|_|  \_\_|\_\
+  
+ The removedStuff needs replacement.
+*/
 const init = async (message) => {
   const stuff = [
     "https://youtu.be/3NRCDOAO8JY",
@@ -12,7 +22,16 @@ const init = async (message) => {
     "https://youtu.be/SRbhLtjOiRc",
   ];
 
-  message.channel.send(stuff.join("\n"));
+  const removedStuff = [
+    "https://youtu.be/hF7QX0hfv8o",
+    "https://youtu.be/Wz4dENwCN3E",
+    "https://youtu.be/SivLaZ2GyrU",
+    "https://youtu.be/WDvW1r85DcY",
+    "https://youtu.be/_vTwDqsqvj4",
+    "https://youtu.be/sk1Q-BFgnPM",
+    "https://youtu.be/w_4RDmaO15E",
+    "https://youtu.be/fS0fU0r_yvo",
+  ]
 
   const stuff2 = [
     "You want some eurobeat? You get some eurobeat!",
@@ -32,20 +51,23 @@ const init = async (message) => {
     "I could say I need your *Manifold Love* right now :3c",
     "Lemme show the best music from the *Future Land*.",
     "And please, *don't stop the music*, teehee.",
-    "Are you a *speedy boy* or not?",
-    "Listen to this every time you want to *remember me*.",
-    "Did you know my favorite WW2 plane was the Supermarine *Spitfire*? ",
-    "I feel like i'm gonna be *forever young*~",
-    "Wao wao~",
     "And dance to the *heartbeat*!",
     "How about a classic one?",
     "*Do you like.... my car**",
     "*I've just been in this place before*.",
     "Let's set this *night on fire*",
+  ];
+
+  const removedStuff3 = [
+    "Are you a *speedy boy* or not?",
+    "Listen to this every time you want to *remember me*.",
+    "Did you know my favorite WW2 plane was the Supermarine *Spitfire*? ",
+    "I feel like i'm gonna be *forever young*~",
+    "Wao wao~",
     "Play this and turn the volume to *the top*!",
     "You can listen this every day, but it is *never enough*",
     "Welcome to the *House of Fire*, please don't get yourself burnt.",
-  ];
+  ]
 
   const rand = randomize(0, stuff.length - 1);
   const rand2 = randomize(0, stuff2.length - 1);
