46dc871f22d6f27e61e2dd2cd2b0c42064ab6481
Mitchell <contact@mitchells.work>
2020-11-08 14:57:37 +0100

    feat(weather): first commit, arch & iso list

---
+++ core/commands/utility/weather.js
@@ -0,0 +1,43 @@
+const Weather = require("../../archetypes/Weather");
+
+const init = async (msg, args) => {
+    if (!args.length) msg.reply("Where though?");
+    const weather = new Weather(args.join(" "));
+    weather.on("error", code => msg.reply("Something went wrong..."));
+    weather.on("done", () => {
+      const inspect = require("util").inspect;
+      const now = inspect(weather.now);
+      const week = inspect([weather.week[0], weather.week[2], "and more..."], {depth: 1});
+      const loc = inspect(weather.location);
+      msg.channel.send({ 
+        embed: {
+          title: "Weather properties",
+          color: 0x9dd9f2,
+          fields: [
+            {
+              name: "location",
+              value: `\`\`\`js\n${loc}\`\`\``,
+            },
+            {
+              name: "now",
+              value: `\`\`\`js\n${now}\`\`\``,
+            },
+            {
+              name: "week",
+              value: `\`\`\`js\n${week}\`\`\``,
+            },
+          ]
+        }
+      });
+    });
+}
+
+module.exports = {
+  init,
+  pub: true,
+  cmd: "weather",
+  perms: 3,
+  cat: "utility",
+  botPerms: ["attachFiles", "embedLinks"],
+  aliases: [],
+};
