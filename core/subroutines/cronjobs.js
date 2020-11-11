0954bf6ae52ccf2ed6151667c5eb18047282c2fe
Tsumari <lucasflicky@hotmail.com>
2020-11-11 13:23:02 -0300

    new: detach cronjobs into sidecar

--- core/subroutines/cronjobs.js
+++ core/subroutines/cronjobs.js
@@ -3,80 +3,6 @@ const moment            = require("moment");
 
 const { CronJob }       = require("cron");
 
-const userDB = DB.users;
-
-/*
-const Discoin = require("../archetypes/Discoin.js");
-
-const discoin = new Discoin(cfg.discoin);
-const coinbase = require("../../resources/lists/discoin.json");
-// const gear = g
-const { receive } = require("../archetypes/Economy.js");
-
-async function resolveExchange(exchange_itm, bot) {
-  const usr = `${exchange_itm.user}`;
-  const ts = Date(exchange_itm.timestamp * 1000);
-  const src = exchange_itm.source;
-  const amt = Number(Math.floor(exchange_itm.amount));
-  const inv = exchange_itm.receipt;
-  const taxes = 0; // Math.ceil(amt*0.1837)
-  const coinfee = 0; // Math.floor(amt*(coinbase[src]||{rbnRate:0.005}).rbnRate)
-  const newAmt = Math.floor(amt - taxes - coinfee);
-
-  if (newAmt < 1) {
-    discoin.reverse(inv);
-    return bot.users.fetch(usr)
-      .then((u) => u.send(":warning: Transaction Reversed :: Amount of Rubines below Zero")
-        .catch((e) => console.warn(`User ${u.id} cannot receive DMs`)));
-  }
-
-  userDB.findOne({ id: usr }, { id: 1 }).then(async (USDATA) => {
-    if (!USDATA) {
-      discoin.reverse(inv);
-      bot.users.fetch(usr)
-        .then((u) => u.send("Transaction Reversed :: Not in Pollux Database")
-          .catch((e) => console.warn(`User ${u.id} cannot receive DMs`)))
-        .catch((e) => console.error(e));
-      return;
-    }
-    receive(usr, newAmt, "discoin", "RBN", "+", `DISCOIN_${src}`).then((ok) => {
-      function aN(inc, ref = amt) {
-        const len = ref.toString().length;
-        const len2 = inc.toString().length;
-        let spaces = "";
-        for (i = 0; i < len - len2; i++) {
-          spaces += " ";
-        }
-        return spaces + inc;
-      }
-      bot.users.fetch(usr).then((u) => {
-        u.getDMChannel().then((dmChan) => {
-          dmChan.send(`
-\`${src}\` ${coinbase[src].icon}:currency_exchange: ${_emoji("rubine")} \`RBN\`
-**Exchange Processed!**
-
-Inbound  : ${_emoji("rubine")} × **${amt}**
-Fees         : ${_emoji("rubine")} × **${taxes + coinfee}**
-\`\`\`diff
-+Inbound Amount   :  ${aN(amt)}
--Transaction Fee  :  ${aN(taxes)}
--Exg. Tax for ${src} :  ${aN(coinfee)}
----------------------------
- Net Income       :  ${aN(newAmt)}
-\`\`\`
-Received **${newAmt}** **RBN**(*Pollux Rubines*) converted from **${src}**(*${`${coinbase[src].bot} ${coinbase[src].name}`}*)!
----
-*Transaction Receipt:*
-\`${ts}\`
-\`\`\`${inv}\`\`\`
-
-`).catch((e) => console.warn("[DISCOIN] User can't recveive DMs"));
-        }).catch((e) => console.log(e, "\n\nERROR ON FETCH"));
-      });
-    });
-  });
-}
-*/
 
 //= =====================================================================================
 //= =====================================================================================
@@ -92,22 +18,6 @@ exports.run = async function run() {
   /* EVERY MIDNIGHT */
   //= =====================================================================================
 
-    userDB.updateMany(
-      { "limits.slots": { $gt: 10 } },
-      { $set: { "limits.slots": 0 } },
-    ).then(() => console.log("Daily Limit Reset for Slots "));
-    userDB.updateMany(
-      { "limits.blackjack": { $gt: 10 } },
-      { $set: { "limits.blackjack": 0 } },
-    ).then(() => console.log("Daily Limit Reset for Blackjack "));
-    userDB.updateMany(
-      { "limits.receive": { $gt: 2 } },
-      { $set: { "limits.receive": 0 } },
-    ).then(() => console.log("Daily Limit Reset for Receive "));
-    userDB.updateMany(
-      { "limits.give": { $gt: 2 } },
-      { $set: { "limits.give": 0 } },
-    ).then(() => console.log("Daily Limit Reset for Give "));
   }, null, true);
 
   const FIVEminute = new CronJob("*/5  * * * *", async () => {
@@ -126,14 +36,8 @@ exports.run = async function run() {
     } catch(e) { PLX.guilds.get("277391723322408960").channels.get("599352130486403079").send("<@124989722668957700> bug at cronjobs.js:124 still exists") }
   });
 
-  const FIFTEENminute = new CronJob("*/1 * * * *", async () => {
+  const FIFTEENminute = new CronJob("*/15 * * * *", async () => {
     PLX.updateBlacklists(DB);
-
-    delete require.cache[require.resolve('./feeds')];
-    const feeds = require('./feeds');
-
-    feeds.check();
-   
   });
 
   const ONEminute = new CronJob("*/1 * * * *", async () => {
@@ -145,115 +49,6 @@ exports.run = async function run() {
     /* EVERY 1 MINUTE */
     //= =====================================================================================
 
-    DB.temproles.find({ expires: { $lte: Date.now() } }).lean().exec()
-      .then((temps) => {
-        temps.forEach((tprl) => {
-          DB.servers.get(tprl.server.id).then(async (/* svData */) => {
-            const logSERVER = PLX.guilds.get(tprl.server);
-            const logUSER = await PLX.resolveUser(tprl.user);
-            if (!logSERVER || !logUSER) return;
-            const logMEMBER = logSERVER.member(logUSER);
-            if (!logMEMBER) return;
-            await DB.temproles.expire({ U: tprl.user, S: tprl.server });
-            await logMEMBER.removeRole(tprl.role, "Temporary Role").catch(() => "Die Silently");
-
-            /*
-            if (svData.dDATA || svData.logging) {
-              return;
-              // delete require.cache[require.resolve('./modules/dev/logs_infra.js')]
-
-              const log = require("./modules/dev/logs_infra.js");
-              log.init({
-                bot,
-                server: logSERVER,
-                member: logMEMBER,
-                user: logUSER,
-                logtype: "usrUnmute",
-              });
-            }
-            */
-          });
-        });
-      });
-
-    /* Manage Reminders */ //= ===============================
-    DB.feed.find({ expires: { $lte: Date.now() } }).lean().exec()
-      .then((reminders) => {
-        reminders.forEach(async (rem) => {
-          try {
-            // url = userID
-            const destChannel = PLX.getChannel(rem.channel) || (await PLX.getDMChannel(rem.url));
-            await DB.feed.deleteOne({ _id: rem._id });
-            await destChannel.createMessage({
-              content: (rem.channel === "dm" ? "" : `<@${rem.url}>`),
-              embed:
-        {
-          title: "<:alarm:446901834305634304> REMINDER:",
-          description: rem.name,
-          timestamp: new Date(),
-          color: 0xcc2233,
-          thumbnail: { url: "https://visualpharm.com/assets/601/Stopwatch-595b40b65ba036ed117d167a.svg" },
-        },
-            });
-          } catch (e) {
-            await DB.feed.deleteOne({ _id: rem._id });
-            console.error("REMOVED FAULTY REMINDER");
-            console.error(e);
-          }
-        });
-      });
-
-    /* Manage Mutes */ //= ===============================
-    DB.mutes.find({ expires: { $lte: Date.now() } })
-      .then((mutes) => {
-        [...new Set(mutes.map((m) => m.server))].forEach((muteSv) => {
-          DB.servers.get(muteSv).then((svData) => {
-            mutes.filter((mut) => mut.server === muteSv).forEach(async (mtu) => {
-              // DB.mutes.expire(Date.now());
-              const logSERVER = PLX.guilds.get(mtu.server);
-              if (svData.modules.MUTEROLE) {
-                await logSERVER.removeMemberRole(mtu.user, svData.modules.MUTEROLE, "Mute Expired")
-                  .then(() => mtu.delete())
-                  .catch(() => mtu.delete());
-              } else {
-                return mtu.delete();
-              }
-              // activity logs stuff (LEGACY CODE)
-              return undefined;
-              /*
-              const logUSER = await PLX.resolveUser(mtu.user);
-              if (!logSERVER || !logUSER) return;
-              const logMEMBER = logSERVER.member(logUSER);
-              if (!logMEMBER) return;
-
-              if (svData.dDATA || svData.logging) {
-                return;
-                // delete require.cache[require.resolve('./modules/dev/logs_infra.js')]
-                const log = require("./modules/dev/logs_infra.js");
-                log.init({
-                  bot,
-                  server: logSERVER,
-                  member: logMEMBER,
-                  user: logUSER,
-                  logtype: "usrUnmute",
-                });
-              }
-              */
-            });
-          });
-        });
-      });
-
-    /* Exchange Currency */ //= ===============================
-  /*
-  discoin.fetch().then(async trades => {
-    trades = JSON.parse(trades)
-    if (!trades.length || trades.length === 0) return;
-    await wait(Number(process.env.SHARD) * 2);
-    Promise.all(trades.map(td => resolveExchange(td, bot)));
-  });
-*/
-  // ---CRON END----///-----////------/////
   }, null, true);
 
   MIDNIGHT.start();
---
+++ sidecar.js
@@ -0,0 +1,199 @@
+
+const {Client} = require('eris')
+const cfg = require('./config.json')
+const moment = require("moment")
+const {CronJob} = require('cron');
+require('colors')
+
+Promise = require('bluebird');
+
+console.log("Sidecar Started".blue);
+
+global.PLX = new Client("Bot "+cfg.token,{restMode:true, intents: 0});
+PLX.cluster = { id: "-1", name: "[SIDECAR]" };
+
+const WebhookDigester = require("./WebhookDigester.js");
+const hook = new WebhookDigester(PLX);
+
+
+const DBSchema = require('@polestar/database_schema');
+const dbConnectionData = {
+  hook:null,
+  url: cfg.dbURL,
+  options: {
+    useNewUrlParser: true,
+    keepAlive: 1,
+    connectTimeoutMS: 8000,
+    useUnifiedTopology: true,
+    poolSize: 100,
+  },
+};
+
+Gearbox = require("./core/utilities/Gearbox");
+
+Object.assign(global, Gearbox.Global);
+Object.assign(PLX, Gearbox.Client);
+
+global._emoji = (E,F) => new (require("./resources/lists/emoji.js")).PolluxEmoji(E,F);
+
+DBSchema(dbConnectionData).then(Connection => {
+    global.DB = Connection;
+    console.log("Discord connection start...");
+    PLX.connect().then(_=>hook.info("Sidecar instance running")).catch(console.error);    
+})
+
+
+ 
+//= =====================================================================================
+//= =====================================================================================
+//= =====================================================================================
+//= =====================================================================================
+//= =====================================================================================
+
+  console.log("• ".blue, "Loading CRON subroutines...");
+
+  const MIDNIGHT = new CronJob("0 0 * * *", () => {
+  //= =====================================================================================
+  /* EVERY MIDNIGHT */
+  //= =====================================================================================
+ 
+  }, null, true);
+
+  const FIVEminute = new CronJob("*/5  * * * *", async () => {
+ 
+  }, null, true);
+
+  
+  const FIFTEENminute = new CronJob("*/1 * * * *", async () => {
+    delete require.cache[require.resolve('./core/subroutines/feeds')];
+    const feeds = require('./core/subroutines/feeds');
+    feeds.check();   
+  });
+
+  const ONEminute = new CronJob("*/1 * * * *", async () => {
+    console.log(`
+   ${"[SIDECAR]".blue} - Uptime: ${moment(Date.now() - PLX.uptime).fromNow(true)}
+  `);
+
+    //= =====================================================================================
+    /* EVERY 1 MINUTE */
+    //= =====================================================================================
+
+    DB.temproles.find({ expires: { $lte: Date.now() } }).lean().exec()
+      .then((temps) => {
+        temps.forEach((tprl) => {
+          DB.servers.get(tprl.server).then(async (/* svData */) => {
+            const [logSERVER, logMEMBER] = await Promise.all([PLX.getRESTGuild(tprl.server), PLX.resolveMember(tprl.server,tprl.user)]).catch(async err=>{
+                await DB.temproles.remove({_id:tprl._id});
+            });
+            if (!logSERVER || !logMEMBER) return;
+            
+            await DB.temproles.expire({ U: tprl.user, S: tprl.server });
+            await PLX.removeGuildMemberRole(tprl.server,tprl.user,tprl.role, "Temporary Role").catch(() => "Die Silently");
+
+            /*
+            if (svData.dDATA || svData.logging) {
+              return;
+              // delete require.cache[require.resolve('./modules/dev/logs_infra.js')]
+
+              const log = require("./modules/dev/logs_infra.js");
+              log.init({
+                bot,
+                server: logSERVER,
+                member: logMEMBER,
+                user: logUSER,
+                logtype: "usrUnmute",
+              });
+            }
+            */
+          });
+        });
+      });
+
+    /* Manage Reminders */ //= ===============================
+    DB.feed.find({ expires: { $lte: Date.now() } }).lean().exec()
+      .then((reminders) => {
+        reminders.forEach(async (rem) => {
+            console.log(rem)
+          try {
+            // url = userID
+            const destChannel = await PLX.getRESTChannel(rem.channel) || (await PLX.getDMChannel(rem.url));
+            await DB.feed.deleteOne({ _id: rem._id });
+            await destChannel.createMessage({
+              content: (rem.channel === "dm" ? "" : `<@${rem.url}>`),
+              embed:
+        {
+          title: "<:alarm:446901834305634304> REMINDER:",
+          description: rem.name,
+          timestamp: new Date(),
+          color: 0xcc2233,
+          thumbnail: { url: "https://visualpharm.com/assets/601/Stopwatch-595b40b65ba036ed117d167a.svg" },
+        },
+            });
+          } catch (e) {
+            await DB.feed.deleteOne({ _id: rem._id });
+            console.error("REMOVED FAULTY REMINDER");
+            console.error(e);
+          }
+        });
+      });
+
+    /* Manage Mutes */ //= ===============================
+    DB.mutes.find({ expires: { $lte: Date.now() } })
+      .then((mutes) => {
+        [...new Set(mutes.map((m) => m.server))].forEach((muteSv) => {
+          DB.servers.get(muteSv).then((svData) => {
+            mutes.filter((mut) => mut.server === muteSv).forEach(async (mtu) => {
+              // DB.mutes.expire(Date.now());
+              const logSERVER = await PLX.getRESTGuild(mtu.server);
+              if (svData.modules.MUTEROLE) {
+                PLX.removeGuildMemberRole(svData.id,mtu.user, svData.modules.MUTEROLE, "Mute Expired").catch(() => "Die Silently")
+                  .then(() => mtu.delete())
+                  .catch(() => mtu.delete());
+              } else {
+                return mtu.delete();
+              }
+              // activity logs stuff (LEGACY CODE)
+              return undefined;
+              /*
+              const logUSER = await PLX.resolveUser(mtu.user);
+              if (!logSERVER || !logUSER) return;
+              const logMEMBER = logSERVER.member(logUSER);
+              if (!logMEMBER) return;
+
+              if (svData.dDATA || svData.logging) {
+                return;
+                // delete require.cache[require.resolve('./modules/dev/logs_infra.js')]
+                const log = require("./modules/dev/logs_infra.js");
+                log.init({
+                  bot,
+                  server: logSERVER,
+                  member: logMEMBER,
+                  user: logUSER,
+                  logtype: "usrUnmute",
+                });
+              }
+              */
+            });
+          });
+        });
+      });
+
+    /* Exchange Currency */ //= ===============================
+  /*
+  discoin.fetch().then(async trades => {
+    trades = JSON.parse(trades)
+    if (!trades.length || trades.length === 0) return;
+    await wait(Number(process.env.SHARD) * 2);
+    Promise.all(trades.map(td => resolveExchange(td, bot)));
+  });
+*/
+  // ---CRON END----///-----////------/////
+  }, null, true);
+
+  MIDNIGHT.start();
+  FIVEminute.start();
+  ONEminute.start();
+
+  FIFTEENminute.start();
+  console.log("• ".green, "CRONs ready");
