d8676fda1635a9a4d6f34a90bf7088d9fe9886e9
pedro <pedropivapedropiva1@outlook.com>
2020-12-08 20:19:07 -0300

    pushing linter fix before flicky conflicts me again

--- core/commands/games/hangmaid/index.js
+++ core/commands/games/hangmaid/index.js
@@ -5,12 +5,12 @@ const Hangmaid = require("../../../archetypes/Hangmaid.js");
 
 const init = async function (msg) {
   const game = new Hangmaid(msg, words);
-  game.start().then(async (data) => {
-    // await msg.channel.sendMessage(`\`${'_ '.repeat(data.word.length)}\`\nYour word is a type of **${data.theme}**.\nYou have 5 chances remaining.\nUsed letters: \`\`\`none\`\`\``);
-    const mainMessage = await msg.channel.send(`${paths.DASH}/generators/hangmaid?${ encodeURI(`g=${data.wordSpaced}&refresh=${Date.now()}&d=${data.difficulty}&h=${data.theme}`) }`);
-    game.registerMessage(mainMessage);
-    await startCollector(game, msg);
-  });
+  game.start()
+    .then(async (data) => {
+      const mainMessage = await msg.channel.send(`${paths.DASH}/generators/hangmaid?${encodeURI(`g=${data.wordSpaced}&refresh=${Date.now()}&d=${data.difficulty}&h=${data.theme}`)}`);
+      game.registerMessage(mainMessage);
+      await startCollector(game, msg);
+    });
 };
 
 const startCollector = async (game, msg) => {
@@ -28,18 +28,24 @@ const startCollector = async (game, msg) => {
     if (guess.length > 1) {
       // either player is talking or trying to guess
       return null;
-    } else me.delete();
+    } else {
+      me.delete();
+    }
 
-    // eslint-disable-next-line max-len
-    if (!game.isFullGuess(guess) && (game.wordBoard.includes(guess) || game.incorrectLetters.includes(guess))) return msg.channel.send("You already said that, honey~").then((mee) => setTimeout(() => mee.delete(), 1500));
+    if (!game.isFullGuess(guess) && (game.wordBoard.includes(guess) || game.incorrectLetters.includes(guess))) {
+      return msg.channel.send("You already said that, honey~")
+        .then((mee) => setTimeout(() => mee.delete(), 1500));
+    }
     const result = await game.handleInput(guess);
     if (!result) return;
     if (result.params.e) Collector.stop();
     await result.message.delete();
     const newMsg = await msg.channel.send(`${paths.DASH}/generators/hangmaid?${
-      encodeURI( `a=${result.params.a}&${result.params.e ? `e=${result.params.e}&` : ""}g=${result.params.g}&refresh=${Date.now()}&h=${result.params.h}` )
+      encodeURI(`a=${result.params.a}&${result.params.e ? `e=${result.params.e}&` : ""}g=${result.params.g}&refresh=${Date.now()}&h=${result.params.h}`)
     }`);
     await game.registerMessage(newMsg);
+    if (result.params.e && result.params.e === "won") return msg.channel.send("Congratulations! You guessed it!");
+    if (result.params.e && result.params.e === "lose") return msg.channel.send("Oh no... you guessed it wrong. Better luck next time~");
   });
 
   Collector.on("end", (coll, reason) => {
