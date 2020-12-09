993e1325c48ca1d41dfb59befc46af1a39fbc760
Tsumari <lucasflicky@hotmail.com>
2020-12-09 12:02:27 -0300

    retire product sans in favor of Quicksand

--- core/utilities/Picto.js
+++ core/utilities/Picto.js
@@ -59,7 +59,7 @@ module.exports = {
   },
 
   tag: function tag(ctx, text, font = "14px", color = "#b4b4b8", stroke) {
-    ctx.font = `${font}, "Product Sans", "DX아기사랑B", "Corporate Logo Rounded", sans-serif`;
+    ctx.font = `${font}, "Quicksand", "DX아기사랑B", "Corporate Logo Rounded", sans-serif`;
 
     text = text?.toString();
     const H = ctx.measureText(text).emHeightAscent;
@@ -88,7 +88,7 @@ module.exports = {
   },
 
   block: function block(ctx, text, font = "14px", color = "#b4b4b8", W = 300, H = 200, options = {}) {
-    ctx.font = `${font}, "Product Sans", "DX아기사랑B", "Corporate Logo Rounded", sans-serif`;
+    ctx.font = `${font}, "Quicksand", "DX아기사랑B", "Corporate Logo Rounded", sans-serif`;
 
     const item = Canvas.createCanvas(W, H);
     const c = item.getContext("2d");
