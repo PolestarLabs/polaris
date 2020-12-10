3ccb66e2335d23ce58d28ce1de6933cb9c50d7cf
Tsumari <lucasflicky@hotmail.com>
2020-12-09 22:55:44 -0300

    Hangmaid archetype: refactor

--- core/archetypes/Hangmaid.js
+++ core/archetypes/Hangmaid.js
@@ -1,24 +1,18 @@
+const games = new Map();
 const yesNo = require("../structures/YesNo");
 module.exports = class Hangmaid {
   constructor(message, words) {
-    const word = shuffle(words)[0];
-    this.word = word.word;
-    this.theme = word.theme;
-    this.level = word.level === 1 ? "Easy" : word.level === 2 ? "Medium" : "Hard";
+    const WORD = shuffle(words)[0];
+    this.word = WORD.word;
+    this.theme = WORD.theme;
+    this.level = WORD.level === 1 ? "Easy" : WORD.level === 2 ? "Medium" : "Hard";
     this.chances = 5;
     this.wordBoard = (new Array(this.word.length)).fill(" ");
     this.incorrectLetters = [];
-    this.ended = false;
-    this.originalMessage = undefined;
-  }
-
-  async start() {
-    return {
-      word: this.word,
-      theme: this.theme,
-      difficulty: this.level,
-      wordSpaced: this.wordBoard.join(""),
-    };
+    this.originalMessage = message;
+    this.end = false;
+    this.channel = message.channel.id
+    games.set(message.channel.id, message.id);
   }
 
   sanitize(word){
@@ -35,19 +29,12 @@ module.exports = class Hangmaid {
   }
 
   terminate(result) {
-    const params = {};
-    params.d = this.level;
-    params.h = this.theme;
-    params.e = result;
-    params.g = result === "win" ? this.word : this.wordBoard.join("");
-    params.a = this.incorrectLetters.join("");
-    return {
-      message: this.originalMessage,
-      params
-    };
+    this.end = result;
+    result === "win" ? this.word : this.wordBoard.join("");
+    return true;
   }
 
-  async handleFullGuess(guess, message, guessMessage) {    
+  async handleFullGuess(guess, message, guessMessage) {
     const m = await message.channel.send(`You're about to guess \`${guess}\`. Is that right?`);
     return (await yesNo(m, guessMessage));
   }
@@ -57,58 +44,38 @@ module.exports = class Hangmaid {
   }
 
   handleInput(guess) { // handleInput () => Object
-      
-    const params = {};
-    params.d = this.level;
-    params.h = this.theme;
 
-    if (this.isFullGuess(guess)) {
-      params.e = "win";
-      params.g = this.word;
-      params.a = this.incorrectLetters.join("");
-      return this.terminate("win");
-    } else {
-      if (guess.length > 1) {
-        params.e = "lose";
-        params.g = this.wordBoard.join("");
-        params.a = this.incorrectLetters.join("");
-        return this.terminate("lose");
-      }
-    }
-
-    if (this.ended) {
-      return {
-        message: this.originalMessage,
-        params
-      };
-    }
+    if (this.isFullGuess(guess)) return this.terminate("win");
+    else if (guess.length > 1) return this.terminate("lose");
 
     const wordArray = this.word.toUpperCase().split("");
 
     if (!wordArray.includes(guess)) {
       this.incorrectLetters.push(guess);
-      params.g = this.wordBoard.join("");
-      params.a = this.incorrectLetters.join("");
-      return {
-        message: this.originalMessage,
-        params
-      };
+      if (this.incorrectLetters.length > this.chances) return this.terminate("lose");
+      return false;
     }
-    console.log( "End of HANDLE INPUT".yellow)
-    console.log({wordBoard:this.wordBoard})
-    console.log(  (this.sanitize(this.wordBoard.join('')) +" == "+ this.sanitize(this.word)) )
-    console.log( "------------------".yellow)
 
     wordArray.forEach((wl, i) => wl === guess ? this.wordBoard[i] = guess : null);
     if (this.sanitize(this.wordBoard.join('')) === this.sanitize(this.word)) return this.terminate("win");
-
-
     
-    params.g = this.wordBoard.join("");
-    params.a = this.incorrectLetters.join("");
-    return {
-      message: this.originalMessage,
-      params
-    };
+    return false;
   }
+
+  get GUESSES(){ return this.end === "win" ? this.word : this.wordBoard.join("") }
+
+  get ATTEMPTS(){ return this.incorrectLetters.join("") }
+
+  get HINT(){ return this.theme }
+
+  get ENDGAME(){ return this.end }
+
+  static gameExists(channelID) {
+    return games.get(channelID);
+  }
+
+  finish(){
+    games.delete(this.channel);
+  }
+
 };
