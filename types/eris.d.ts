2032bc9387f2618ef035e4007019afc4859070b2
Bsian <chharry321@gmail.com>
2020-12-19 02:29:59 +0000

    Types

---
+++ types/eris.d.ts
@@ -0,0 +1,167 @@
+import { AxiosResponse } from "axios";
+import "eris";
+import { TextableChannel } from "eris";
+import { EventEmitter } from "events";
+
+interface EA_awaitMessagesOptions {
+  maxMatches: number;
+  time?: number;
+}
+
+class MessageCollector<T extends Textable = TextableChannel> extends EventEmitter {
+  filter: (m: Message<T>) => any;
+  channel: T;
+  options: EA_awaitMessagesOptions;
+  ended: boolean;
+  collected: Message<T>[];
+  constructor(channel: T, filter: (m: Message<T>) => any, options?: EA_awaitMessagesOptions);
+  verify(message: Message<T>): boolean;
+  stop(reason: string): void;
+  on(event: "message", cb: (message: Message<T>) => any): this;
+  on(event: "end", cb: (collected: Message<T>[], reason: string) => any): this;
+}
+
+class Microserver {
+  microtasks: {
+    updateServerCache(body: BaseData | "all", url: string, res: any): Promise<any>;
+    reloadServerCache(): Promise<void>;
+    updateChannels(body: BaseData | "all", url: string, res: any): Promise<any>;
+  } | null;
+  constructor(crossAuth: string);
+}
+
+declare module "eris" {
+  interface TextableChannel {
+    awaitMessages(filter: (m: Message) => any, options: EA_awaitMessagesOptions): Promise<Message[]>;
+    createCode(code: string, language: string): Promise<Message>;
+    sendCode(code: string, language: string): Promise<Message>;
+    createEmbed(embed: EmbedOptions): Promise<Message>;
+    sendEmbed(embed: EmbedOptions): Promise<Message>;
+    sendMessage(content: MessageContent  , file?: MessageFile | MessageFile[]): Promise<Message>;
+  }
+  interface PrivateChannel {
+    awaitMessages(filter: (m: Message<PrivateChannel>) => any, options: EA_awaitMessagesOptions): Promise<Message<PrivateChannel>[]>;
+    createCode(code: string, language: string): Promise<Message<PrivateChannel>>;
+    sendCode(code: string, language: string): Promise<Message<PrivateChannel>>;
+    createEmbed(embed: EmbedOptions): Promise<Message<PrivateChannel>>;
+    sendEmbed(embed: EmbedOptions): Promise<Message<PrivateChannel>>;
+    sendMessage(content: MessageContent  , file?: MessageFile | MessageFile[]): Promise<Message<PrivateChannel>>;
+  }
+  interface TextChannel {
+    awaitMessages(filter: (m: Message<TextChannel>) => any, options: EA_awaitMessagesOptions): Promise<Message<TextChannel>[]>;
+    createCode(code: string, language: string): Promise<Message<TextChannel>>;
+    sendCode(code: string, language: string): Promise<Message<TextChannel>>;
+    createEmbed(embed: EmbedOptions): Promise<Message<TextChannel>>;
+    sendEmbed(embed: EmbedOptions): Promise<Message<TextChannel>>;
+    sendMessage(content: MessageContent  , file?: MessageFile | MessageFile[]): Promise<Message<TextChannel>>;
+  }
+  interface NewsChannel {
+    awaitMessages(filter: (m: Message<NewsChannel>) => any, options: EA_awaitMessagesOptions): Promise<Message<NewsChannel>[]>;
+    createCode(code: string, language: string): Promise<Message<NewsChannel>>;
+    sendCode(code: string, language: string): Promise<Message<NewsChannel>>;
+    createEmbed(embed: EmbedOptions): Promise<Message<NewsChannel>>;
+    sendEmbed(embed: EmbedOptions): Promise<Message<NewsChannel>>;
+    sendMessage(content: MessageContent  , file?: MessageFile | MessageFile[]): Promise<Message<NewsChannel>>;
+  }
+
+  interface Client {
+    createCode(channelID: string, code: string, language: string): Promise<Message>;
+    createEmbed(channelID: string, embed: EmbedOptions): Promise<Message>;
+  }
+
+  class CodeBlock {
+    content?: string; lang?: string;
+    constructor(content?: string, lang?: string);
+    content(content: string): this;
+    language(lang: string): this;
+    toString(): string;
+  }
+
+  class Embed implements EmbedOptions {
+    fields: EmbedField[]; file?: unknown; // Eris file?
+    constructor(data?: EmbedOptions);
+    author(name: string, icon?: string, url?: string): this;
+    color(color: number): this;
+    description(desc: string): this;
+    field(name: string, value: string, inline?: boolean): this;
+    file(file: unknown): this;
+    footer(text: string, icon?: string): this;
+    image(url: string): this;
+    setColor(color: string): this;
+    timestamp(time?: Date): this;
+    title(title: string): this;
+    thumbnail(url: string): this;
+    url(url: string): this;
+  }
+
+  interface GuildChannel {
+    memberHasPermission(memberID: string, permission: string): boolean;
+  }
+
+  interface Member {
+    bannable: boolean;
+    createMessage(content: MessageContent, file?: MessageFile | MessageFile[]): Promise<Message<PrivateChannel>>;
+    effectiveName: string;
+    hasPermission(permission: string): boolean;
+    hasRole(roleID: Role | string): boolean;
+    highestRole: Role;
+    color: number;
+    kickable: boolean;
+    punishable(member2: Member): boolean;
+    roleObjects: Role[];
+    sendMessage(content: MessageContent, file?: MessageFile | MessageFile[]): Promise<Message<PrivateChannel>>;
+  }
+
+  interface Message<T extends Textable> {
+    guild: T extends GuildTextableChannel ? Guild : never;
+  }
+
+  interface Role {
+    addable: boolean;
+    higherThan(role2: Role): boolean
+  }
+
+  interface User {
+    createMessage(content: MessageContent, file?: MessageFile | MessageFile[]): Promise<Message<PrivateChannel>>;
+    displayAvatarURL: string;
+  }
+
+  interface Guild {
+    findMembers(query: string): Member[];
+    me: Member;
+    member(user: User | Member | BaseData | string): Member;
+  }
+
+  interface CommandClient {
+    resolveUser(user: User | Member | BaseData | string, options?: { enforceDB?: boolean }): Promise<User>;
+    resolveMember(guild: Guild | BaseData | string, user: User | Member | BaseData | string, options?: { enforceDB?: boolean; softMatch?: boolean; }): Promise<Member>;
+    getTarget(query: string, guild?: Guild | BaseData | string | null, strict?: boolean, member?: false): Promise<User>;
+    getTarget(query: string, guild: Guild | BaseData | string | null, strict?: boolean, member: true): Promise<User>;
+    getTargetLegacy(query: string | User | Member | BaseData, guild?: Guild | null, strict?: boolean, member?: false): Promise<User | null>;
+    getTargetLegacy(query: string | User | Member | BaseData, guild?: Guild | null, strict?: boolean, member: true): Promise<Member | null>;
+    getChannelImg(message: Message, nopool?: boolean): Promise<string | boolean>;
+    modPass(member: Member, extra?: string, sData?: boolea | object, channel?: Channel | null): boolean;
+    gamechange(gamein?: [string, number] | false, status?: Status): void;
+    getPreviousMessage(msg: Message, ID?: string): Promise<Message>;
+    autoHelper(trigger: string, options?: { message?: Message; msg?: Message; cmd: string; opt?: string; [s: string]: any }): boolean;
+    usage(cmd: string, m: Message, third?: string, sco?: { [s: string]: any }): void;
+    engine: typeof import("eris");
+    beta: boolean;
+    maintenance: boolean;
+    cluster: { id: number; name: string };
+    execQueue: any[]; // what's this
+    commandPool: {};
+    registerCommands(rel?: boolean): void;
+    registerOne(folder: string, _cmd: string): null;
+    blacklistedUsers: string[];
+    blacklistedServers: string[];
+    microserver: Microserver;
+    softKill(): void;
+    restarting: boolean;
+    hardKill(): void;
+    setAvatar(url: string): void;
+    bean(guild: string, user: string, delete_message_days?: number, reason?: string): Promise<AxiosResponse>;
+    unbean(guild: string, user: string, delete_message_days?: number, reason?: string): Promise<AxiosResponse>;
+    reply(msg: Message, content: MessageContent): Promise<AxiosResponse>;
+  }
+}
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,22 +1,113 @@
-import { CommandClient, Embed } from "eris";
+import { Bucket, Client, CommandClient, Embed, EmbedOptions } from "eris";
+import { i18n, TranslationFunction } from "i18next";
+import Bluebird from "bluebird";
+
+declare class PolluxEmoji extends String {
+  constructor(identifier: string, fallback?: PolluxEmoji);
+  reaction: string;
+  no_space: string;
+  name: string;
+  id: string;
+}
+
+enum bgPrices {
+  UR = 32520, SR = 15250, R = 8200, U = 3100, C = 1850,
+}
+enum medalPrices {
+  UR = 2850, SR = 1875, R = 1223, U = 830, C = 500,
+}
+enum rarity {
+  C = 1000, U = 400, R = 250, SR = 150, UR = 70, XR = 0,
+}
+enum gems {
+  C = 50, U = 100, R = 160, SR = 240, UR = 320, XR = 500,
+}
+enum itemType {
+  JDE = 400, RBN = 250, BPK = 225, MDL = 200, BKG = 180, ITM = 120, SPH = 5,
+}
+interface LootRates {
+  rarity: rarity; gems: gems; itemType: itemType;
+}
+
+interface GlobalNumbers {
+  DROPMAX: 1000;
+  sapphireModifier: 0.000794912559618442;
+  jadeModifier: 2250;
+  tokenModifier: 0.5;
+  bgPrices: bgPrices;
+  medalPrices: medalPrices;
+  LootRates: LootRates;
+}
+
+interface WHDOptions {
+  pings?: boolean | string;
+  once?: boolean;
+  hook?: { id: string; webhook: string; };
+  noRepeat?: boolean;
+  id?: string;
+}
+
+class WebhookDigester {
+  client: Client;
+  executed: string[];
+  constructor(client: Client);
+  execute(embed: EmbedOptions, options?: WHDOptions): void;
+  info(message: string, options: WHDOptions): void;
+  warn(message: string, options: WHDOptions): void;
+  error(message: string, options: WHDOptions): void;
+  ok(message: string, options: WHDOptions): void;
+}
+
+class AchievementsManager extends EventEmitter {
+  get(ach: string): Promise<object>;
+  give(user: any, ach: string): Promise<object>;
+  has(ach: string, user: any): Promise<boolean>;
+  progress(ach: string, user: any): Promise<{ current: any; goal: any; percent: number; }>;
+  check(userData: any, beau?: boolean, awardRightAway?: boolean): Promise<any>; // cba figuring this one out
+  on(event: "award", cb: (achievID: string, userID: string) => any): this;
+}
 
 declare global {
-    export const DB: any; // wtf is this
 
     // pollux.js
+    export interface Promise<T> extends Bluebird<T> {}
+    export const clusterNames: string[];
+    export const GNums: GlobalNumbers;
     export const appRoot: string;
     export const PLX: CommandClient;
-    export const paths: run;
+    export const MARKET_TOKEN: string;
+    export const hook: WebhookDigester;
+    export function _emoji(E: string, F?: PolluxEmoji): PolluxEmoji;
+    export const DB: any; // @polestar/database_schema
+    export function translateEngineStart(): void;
+    export const i18n: i18n;
+    export const $t: TranslationFunction;
+    export function rand$t(string: string, params?: {[s: string]: any}): string;
 
     // gearbox
     export const nope: string;
+    export function reload(): void;
     export const invisibar: string;
     export const Embed: Embed;
     export const RichEmbed: Embed;
+    export function weightedRand(wArr: number[]): number;
+    export function randomize(min: number, max: number, seed?: number): number;
+    export function wait(time: number): Promise<true>;
+    export function miliarize(numstring: number | string, strict?: boolean | "soft", char?: string): string;
+    export function shuffle<T>(array: T[]): T[];
+    export function capitalize(string: string): string;
+    export function objCount<T>(array: T[], what: T): number;
+    export function resolveFile(resource: Buffer | string | ReadableStream): Buffer;
+    export function file(file: string | number | Buffer | URL, name?: string): { finalFile: Buffer, name: string; };
 
-    export {
-      reload, weightedRand, randomize, wait, miliarize, shuffle, capitalize, objCount, resolveFile, file,
-    } from "../core/utilities/Gearbox/global";
+    // archetypes
+    export const Achievements: AchievementsManager;
+    export const userLimits: Map<string, Bucket>;
+    export const fakeFeed: null | { link: string; title: string; };
+
+    // utils
+    export const paths: run;
+
 }
 
 interface run {
