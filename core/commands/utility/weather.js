bf38dc5fac269e4315816693104f99e5a499f77b
Mitchell <contact@mitchells.work>
2020-12-26 18:02:17 +0100

    Promisify Weather

--- core/commands/utility/weather.js
+++ core/commands/utility/weather.js
@@ -7,40 +7,42 @@ const init = async (msg, args) => {
     far = true;
     args.splice(0, 1);
   }
-  const weather = new Weather(args.join(" "));
-  weather.on("error", (code) => msg.reply(`${code} - Something went wrong...`)); // return code != 200 TODO remove code
-  weather.on("done", () => { // got API response
-    if (!weather.found) {
-      return msg.channel.send("Location not found :(");
-    }
+  const weather = new Weather();
 
-    // WEATHER SHOWCASE
-    if (far) weather.setUnit("F");
-    const { inspect } = require("util");
-    const now = inspect(weather.now);
-    const week = inspect([weather.week[0], weather.week[2], "and more..."], { depth: 1 });
-    const loc = inspect(weather.location);
-    msg.channel.send({
-      embed: {
-        title: "Weather properties",
-        description: "Methods: weather.setUnit('F' | 'C')\nProperties: `found` boolean",
-        color: 0x9dd9f2,
-        fields: [
-          {
-            name: "weather.location",
-            value: `\`\`\`js\n${loc}\`\`\``,
-          },
-          {
-            name: "weather.now",
-            value: `\`\`\`js\n${now}\`\`\``,
-          },
-          {
-            name: "weather.week",
-            value: `\`\`\`js\n${week}\`\`\``,
-          },
-        ],
-      },
-    });
+  try {
+    await weather.initiate(args.join(" "));
+  } catch (code) { msg.reply(`${code} - Oops, something went wrong...`) };
+
+  if (!weather.found) {
+    return msg.channel.send("Location not found :(");
+  }
+
+  // WEATHER SHOWCASE
+  if (far) weather.setUnit("F");
+  const { inspect } = require("util");
+  const now = inspect(weather.now);
+  const week = inspect([weather.week[0], weather.week[2], "and more..."], { depth: 1 });
+  const loc = inspect(weather.location);
+  msg.channel.send({
+    embed: {
+      title: "Weather properties",
+      description: "Methods: weather.setUnit('F' | 'C')\nProperties: `found` boolean",
+      color: 0x9dd9f2,
+      fields: [
+        {
+          name: "weather.location",
+          value: `\`\`\`js\n${loc}\`\`\``,
+        },
+        {
+          name: "weather.now",
+          value: `\`\`\`js\n${now}\`\`\``,
+        },
+        {
+          name: "weather.week",
+          value: `\`\`\`js\n${week}\`\`\``,
+        },
+      ],
+    },
   });
 };
 
