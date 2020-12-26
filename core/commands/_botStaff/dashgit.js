850a1334f86c8974687a14440cb350d3da749256
Bsian <chharry321@gmail.com>
2020-12-26 17:21:33 +0000

    Resolve promise properly

--- core/commands/_botStaff/dashgit.js
+++ core/commands/_botStaff/dashgit.js
@@ -1,17 +1,13 @@
 const init = async (msg, args) => {
+  let description;
   exec(`git ${args.join(" ")}`, {
     cwd: "/home/pollux/polaris/dashboard",
-  }, (error, stdout, stderr) => {
-    const description =  `
-        ${
-  error
-    ? `${_emoji("nope")}**Oopsie Woopsie:** \`\`\`nginx\n${stderr.slice(0, 1900)}\`\`\``
-    : `${_emoji("yep")} \`${args.join(" ")}\` ${stdout.length ? "```nginx\n" : "```OK!"}${stdout.slice(0, 1900)}${"```"}`
-} 
-        `;
+  }).then(
+    (res) => (description = `${_emoji("yep")} \`${args.join(" ")}\` ${res.length ? "```nginx\n" : "```OK!"}${res.slice(0, 1900)}${"```"}`),
+    (rej) => (description = `${_emoji("nope")}**Oopsie Woopsie:** \`\`\`nginx\n${rej.message.slice(0, 1900)}\`\`\``),
+  );
 
-    msg.channel.send({ embed: { description } });
-  });
+  msg.channel.send({ embed: { description } });
 };
 
 module.exports = {
