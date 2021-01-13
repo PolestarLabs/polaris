e7767605b11ac6df06f640de30ec20122be9f75f
Tsumari <lucasflicky@hotmail.com>
2021-01-12 21:52:58 -0300

    unpromisify + ranking aggregation pipe

--- core/commands/social/top.js
+++ core/commands/social/top.js
@@ -7,75 +7,84 @@ const topGlobal = (m) => require("./leaderboards.js").init(m, ["global"]);
 const topCommend = async (m) => {
   const userData = await DB.users.get(m.author.id);
 
-
-  const [commendSort,commendedSort, myCommends] = await
+  
+  const myCommends = await DB.commends.parseFull(m.author.id);
+  const [commendSort,commendedSort, myRankIn,myRankOut] = await
   Promise.all([
-
     DB.commends.aggregate([{$group:{_id:"$from",total:{$sum:"$count"}}},{$sort:{total:-1}},{$limit:10}]),
     DB.commends.aggregate([{$group:{_id:"$to",total:{$sum:"$count"}}},{$sort:{total:-1}},{$limit:10}]),
-    DB.commends.parseFull(m.author.id)
-
+    (await DB.commends.aggregate([{$group:{_id:"$to",total:{$sum:"$count"}}},{$match:{total: {$gt:myCommends.totalIn } }},{$count: "COUNT" }]))[0].COUNT +1,
+    (await DB.commends.aggregate([{$group:{_id:"$from",total:{$sum:"$count"}}},{$match:{total: {$gt:myCommends.totalOut } }},{$count: "COUNT" }]))[0].COUNT +1,
   ]);
+
+  let neededUsers = [...new Set( [...commendSort.map(x=>x._id),...commendedSort.map(x=>x._id)] )];
+  const usersDiscordData = await Promise.all( neededUsers.map( PLX.resolveUser ) );
 
   //CommendRank.forEach((usr) => { usr.name = PLX.resolveUser(usr.id)?.username || usr.name; });
 
-
   const isUsr = (x) => x.id === m.author.id;
 
-  const listCommend = Promise.all(commendSort.slice(0, 3).map(
-    async (x) => {
-      let user = await PLX.resolveUser(x._id);
-      return `:reminder_ribbon: *\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
-        (isUsr(user) ? "**" : "")
-        + user.username.slice(0, 16) + ( user.username.length > 15 ? "..." : "")
-        + (isUsr(user) ? "**" : "")
-      }](${paths.DASH}/p/${x._id})*`
-    }
-  ));
+  console.log(usersDiscordData)
 
-  const listCommenders = Promise.all(commendedSort.slice(0, 3).map(
-    async (x) => {
-      let user = await PLX.resolveUser(x._id);
-      return `${_emoji("plxcoin")}*\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
-      (isUsr(user) ? "**" : "")
-                + user.username.slice(0, 16) + (user.username.length > 15 ? "..." : "")
-                + (isUsr(user) ? "**" : "")
-      }](${paths.DASH}/p/${x._id})*`
-    }
-  ));
+  const [listCommend,  listCommenders,  listCommend2,  listCommenders2] = [
 
-  const listCommend2 = Promise.all(commendSort.slice(3, 10).map(
-    async (x) => {
-      let user = await PLX.resolveUser(x._id);
-      return `:reminder_ribbon: *\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
-        (isUsr(user) ? "**" : "")
-                  + user.username.slice(0, 16) + (user.username.length > 15 ? "..." : "")
-                  + (isUsr(user) ? "**" : "")
-      }](${paths.DASH}/p/${x._id})*`
-    }
-  ));
+    (commendSort.slice(0, 3).map(
+        (x) => {
+          let user = usersDiscordData.find(u=>u.id===x._id);
+          return `:reminder_ribbon: *\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
+            (isUsr(user) ? "**" : "")
+            + user.username.slice(0, 16) + ( user.username.length > 15 ? "..." : "")
+            + (isUsr(user) ? "**" : "")
+          }](${paths.DASH}/p/${x._id})*`
+        }
+      )),
 
-  const listCommenders2 = Promise.all(commendedSort.slice(3, 10).map(
-    async  (x) => {
-      let user = await PLX.resolveUser(x._id);
-      return `${_emoji("plxcoin")}*\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
-        (isUsr(user) ? "**" : "")
-        + user.username.slice(0, 16) + (user.username.length > 15 ? "..." : "")
-        + (isUsr(user) ? "**" : "")
-      }](${paths.DASH}/p/${x._id})*`
-    }
-  ));
+    (commendedSort.slice(0, 3).map(
+        (x) => {
+          let user = usersDiscordData.find(u=>u.id===x._id);
+          return `${_emoji("plxcoin")}*\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
+          (isUsr(user) ? "**" : "")
+                    + user.username.slice(0, 16) + (user.username.length > 15 ? "..." : "")
+                    + (isUsr(user) ? "**" : "")
+          }](${paths.DASH}/p/${x._id})*`
+        }
+      )),
+
+    (commendSort.slice(3, 10).map(
+        (x) => {
+          let user = usersDiscordData.find(u=>u.id===x._id);
+          return `:reminder_ribbon: *\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
+            (isUsr(user) ? "**" : "")
+                      + user.username.slice(0, 16) + (user.username.length > 15 ? "..." : "")
+                      + (isUsr(user) ? "**" : "")
+          }](${paths.DASH}/p/${x._id})*`
+        }
+      )),
+
+    (commendedSort.slice(3, 10).map(
+        (x) => {
+          let user = usersDiscordData.find(u=>u.id===x._id);
+          return `${_emoji("plxcoin")}*\`\u200b${(x.total || 0).toString().padStart(3, "\u2003")} \`\u2003[${
+            (isUsr(user) ? "**" : "")
+            + user.username.slice(0, 16) + (user.username.length > 15 ? "..." : "")
+            + (isUsr(user) ? "**" : "")
+          }](${paths.DASH}/p/${x._id})*`
+        }
+      ))
+  ];
+
+  console.log(listCommend)
 
   const embed = {
     thumbnail: { url: "https://pollux.fun/build/rank.png" },
     color: 0x3b9ea5,
-    description: `**Your Score** \u2003 :reminder_ribbon: **#${'myRankIn'  }** (${ myCommends.totalIn }) \u2003 | \u2003  ${_emoji("plxcoin")}**#${'myRankOut' }** (${ myCommends.totalOut })`,
+    description: `**Your Score** \u2003 :reminder_ribbon: **#${myRankIn}** (${ myCommends.totalIn }) \u2003 | \u2003  ${_emoji("plxcoin")}**#${myRankOut}** (${ myCommends.totalOut })`,
     fields: [
-      { name: "Top Commended", value: `${(await listCommend).join("\n").slice(0, 1024)}\u200b`, inline: true },
-      { name: "Top Commenders", value: `${(await listCommenders).join("\n").slice(0, 1024)}\u200b`, inline: true },
+      { name: "Top Commended", value: `${(listCommend).join("\n").slice(0, 1024)}\u200b`, inline: true },
+      { name: "Top Commenders", value: `${(listCommenders).join("\n").slice(0, 1024)}\u200b`, inline: true },
       { name: "\u200b", value: "\u200b", inline: true },
-      { name: "\u200b", value: `${(await listCommend2).join("\n").slice(0, 1024)}\u200b`, inline: true },
-      { name: "\u200b", value: `${(await listCommenders2).join("\n").slice(0, 1024)}\u200b`, inline: true },
+      { name: "\u200b", value: `${(listCommend2).join("\n").slice(0, 1024)}\u200b`, inline: true },
+      { name: "\u200b", value: `${(listCommenders2).join("\n").slice(0, 1024)}\u200b`, inline: true },
       { name: "\u200b", value: "\u200b", inline: true },
     ],
   };
