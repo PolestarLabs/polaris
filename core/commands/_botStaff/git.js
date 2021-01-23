d6474277d9a170fa5d456f6ab8033dd140bf50d9
Mitchell <contact@mitchells.work>
2021-01-23 19:31:20 +0100

    make git more usable

--- core/commands/_botStaff/git.js
+++ core/commands/_botStaff/git.js
@@ -1,17 +1,18 @@
 const init = async (msg, args) => {
-  const regex = /^git[\w\d\s-"'./]+$/;
-
-  if (!regex.test(`git ${args.join(" ")}`)) return ` ${_emoji("nope")}`;
+  let fail = false;
 
   const description = await exec(`git ${args.join(" ")}`).then(
     (res) => `${_emoji("yep")} \`${args.join(" ")}\` ${res.length ? "```nginx\n" : "```OK!"}${res.slice(0, 1900)}${"```"}`,
-    (rej) => `${_emoji("nope")}**Oopsie Woopsie:** \`\`\`nginx\n${rej.message.slice(0, 1900)}\`\`\``,
+    (rej) => ((fail = true), `${_emoji("nope")}**Oopsie Woopsie:** \`\`\`nginx\n${rej.message.slice(0, 1900)}\`\`\``),
   );
 
   msg.channel.send({ embed: { description } });
-
-  return require("./reload").init(msg, ["hard"]);
+  if (!fail) reload();
 };
+
+function reload() {
+  return require("./reload").init(msg, ["hard"]);
+}
 
 module.exports = {
   init,
