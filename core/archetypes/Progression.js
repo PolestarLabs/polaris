36aaa10a0ed48d282268d13ca51a1710fdb09622
Tsumari <lucasflicky@hotmail.com>
2021-01-24 17:53:28 -0300

    DRAFT: general progression archetype

---
+++ core/archetypes/Progression.js
@@ -0,0 +1,26 @@
+const EventEmitter = require("events");
+
+class Progression extends EventEmitter {
+    constructor(){
+        super();
+        
+    }
+}
+
+
+
+isPartOfAchievement(param,value,options){
+    
+}
+
+
+module.exports = Progression;
+
+
+
+// command
+//send params: 
+    // Command Label
+    // Parameter ID
+    // Parameter value
+    // MSG
