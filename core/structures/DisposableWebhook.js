ccfef6d487902b0a295f30df7a7e945f32a0de2e
Bsian <chharry321@gmail.com>
2021-01-30 00:41:13 +0000

    Throw error on promise rejection

--- core/structures/DisposableWebhook.js
+++ core/structures/DisposableWebhook.js
@@ -2,11 +2,14 @@ const i2b64 = Promise.promisify((require("imageurl-base64")));
 
 class DisposableHook {
   constructor(msg, name, avatar, info) {
-    this.hook = i2b64(avatar).then((b64) => PLX.createChannelWebhook(
-      msg.channel.id,
-      { name, avatar: b64.dataUri },
-      (info.reason || info),
-    ).catch(() => { msg.channel.send("Cannot Create Webhooks :("); return null; }));
+    this.hook = i2b64(avatar).then(
+      (b64) => PLX.createChannelWebhook(
+        msg.channel.id,
+        { name, avatar: b64.dataUri },
+        (info.reason || info),
+      ).catch(() => { msg.channel.send("Cannot Create Webhooks :("); return null; }),
+      (e) => { throw e; },
+    );
 
     if (info.once) {
       this.hook.then((hk) => (hk ? PLX.executeWebhook(hk.id, hk.token, info.payload).then(() => this.destroy()) : null));
