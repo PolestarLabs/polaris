f13eac5b3f3942e5be51d31ec1b7699f365a8141
Mitchell <contact@mitchells.work>
2021-02-01 14:51:09 +0100

    one small edit

--- core/commands/_botStaff/eval.js
+++ core/commands/_botStaff/eval.js
@@ -88,7 +88,7 @@ const init = async (msg) => {
  * @returns {string} code with return
  */
 function addReturn(code) {
-  const lastSemiColon = code.match(/(;)(?!.*;.*[\w\n\r]+)/m);
+  const lastSemiColon = code.match(/(;)(?!.*;.*[\w]+)/m);
   if (lastSemiColon === null || lastSemiColon.index === undefined) return `return ${code}`;
   else {
     const codeArray = code.split("");
