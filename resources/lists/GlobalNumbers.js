390943cfd43de4eb8e2a974ec65c08f240f95375
Tsumari <lucasflicky@hotmail.com>
2021-02-11 12:48:14 +0000

    add lootbox 2.0s fibonacci to dev

--- resources/lists/GlobalNumbers.js
+++ resources/lists/GlobalNumbers.js
@@ -1,37 +1,51 @@
+const BASELINE = 128;
+const FIBONACCI = function(start,pos){
+  let res =start;
+  let pre =0;
+  while (pos--){
+    let t = res;
+    res += pre;
+    pre = t;
+  }
+  return res;
+}
+
+const mdP    = (S) => ~~(FIBONACCI(BASELINE,S))
+const bgP    = (S) => ~~( mdP(S) * FIBONACCI(BASELINE*2.5,1) * S / BASELINE );
+
 module.exports = {
 
   DROPMAX: 1000, // Maximum dice face for Lootbox Drops
-  sapphireModifier: 0.000794912559618442, // ÷1258
-  jadeModifier: 2250, // (legacy)
-  tokenModifier: 0.5,
+
+
   bgPrices: {
-    UR: 32520, SR: 15250, R: 8200, U: 3100, C: 1850,
+    UR: bgP(6), SR: bgP(5), R: bgP(4), U: bgP(3), C: bgP(2),
   },
   medalPrices: {
-    UR: 2850, SR: 1875, R: 1223, U: 830, C: 500,
+    UR: mdP(6), SR: mdP(5), R: mdP(4), U: mdP(3), C: mdP(2),
   },
   LootRates: {
     rarity: {
       C: 1000, U: 400, R: 250, SR: 150, UR: 70, XR: 0,
     },
     gems: {
-      C: 50, U: 100, R: 160, SR: 240, UR: 320, XR: 500,
+      C: 100, U: 200, R: 400, SR: 650, UR: 1000, XR: 2500,
     },
     itemType: {
       // JADES
       JDE: 400,
       // RUBINES
       RBN: 250,
-      // BOOSTERS
-      BPK: 225,
       // MEDALS
       MDL: 200,
       // BACKGROUNDS
-      BKG: 180,
+      BKG: 150,
+      // BOOSTERS
+      BPK: 200,
       // ITEMS
-      ITM: 120,
+      ITM: 100,
       // SAPPHIRE
-      SPH: 5,
+      SPH: 1,
     },
   },
 
