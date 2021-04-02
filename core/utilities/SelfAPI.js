10ac4672d42b7688edb952c88e015b01bb864fe2
Tsumari <lucasflicky@hotmail.com>
2021-04-02 20:00:00 -0300

    use dedicated api path

--- core/utilities/SelfAPI.js
+++ core/utilities/SelfAPI.js
@@ -1,13 +1,11 @@
 const axios = require("axios");
 const apiToken = require("../../config.json")["pollux-api-token"];
 
-const ROOT = paths.CDN;
-
 class SelfAPI {
   constructor(key) {
     this.key = key;
     this.request = axios.create({
-      baseURL: `${ROOT}/api/`,
+      baseURL: `${ paths.API }/`,
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${this.key}`,
