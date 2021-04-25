82bffe1a5a1bae162ab3406fc95f151dce92915a
Tsumari <lucasflicky@hotmail.com>
2021-04-25 19:47:36 -0300

    no bars at 0%

--- core/commands/pollux/errands/_meta.js
+++ core/commands/pollux/errands/_meta.js
@@ -5,12 +5,12 @@ function parseQuestItem(errandsData, errand, mini=false) {
     const thisErrand = errandsData.find(e => e.id === errand.id);
     const progress = ((Math.min(errand.progress, errand.target || 1)) / (errand.target || 1));
     if (mini){
-        return `${errand.completed ? _emoji('yep') : errand.progress ? _emoji('maybe') : _emoji('nope')} ${"`" + [...Array(10).keys()].map((b) => b > ~~(progress * 10) ? ' ' : '|').join('') + "`"} • **${thisErrand.INSTRUCTION || "UNK"}**`;
+        return `${errand.completed ? _emoji('yep') : errand.progress ? _emoji('maybe') : _emoji('nope')} ${"`" + [...Array(10).keys()].map((b) => b > ~~((progress * 10)-1) ? ' ' : '|').join('') + "`"} • **${thisErrand.INSTRUCTION || "UNK"}**`;
     }
     return ({
         inline: 0,
         name: `${errand.completed ? _emoji('yep') : errand.progress ? _emoji('maybe') : _emoji('nope')} **${thisErrand.INSTRUCTION || "UNK"}**`,
-        value: `${_emoji('__')} ${"`" + [...Array(10).keys()].map((b) => b > ~~(progress * 10) ? ' ' : '|').join('') + "`"} ${progress * 100}% \n${_emoji('__')} Rewards: ${[
+        value: `${_emoji('__')} ${"`" + [...Array(10).keys()].map((b) => b > ~~((progress * 10)-1) ? ' ' : '|').join('') + "`"} ${progress * 100}% \n${_emoji('__')} Rewards: ${[
                 (thisErrand.rewards?.exp ? `${_emoji('EXP')}**${thisErrand.rewards.exp}**  ` : ""),
                 (thisErrand.rewards?.RBN ? `${_emoji('RBN')}**${thisErrand.rewards.RBN}**  ` : ""),
                 (thisErrand.rewards?.SPH ? `${_emoji('SPH')}**${thisErrand.rewards.SPH}**  ` : "")
