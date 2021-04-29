31602800492aa024336cf164c83554da68ed9538
Tsumari <lucasflicky@hotmail.com>
2021-04-29 14:37:54 -0300

    tweak to animation module

--- core/structures/Animation.js
+++ core/structures/Animation.js
@@ -1,48 +1,57 @@
-const GIFEncoder = require("gif-encoder");
-const EventEmitter = require("events");
-const { createWriteStream } = require("fs");
+const GIFEncoder = require('gif-encoder');
+const EventEmitter = require('events');
+const {createWriteStream} = require('fs');
+const fs =  require('fs');
+const cacheDir = "/home/pollux/polaris/gifcache"
 
 class Animation extends EventEmitter {
-  constructor(options) {
-    super();
-    this.gif = new GIFEncoder(options.w || 100, options.h || 100);
-    this.gif.writeHeader();
-    this.gif.setRepeat(options.repeat || 0);
-    this.gif.highWaterMark = options.highWaterMark || 128000;
-    this.gif.setTransparent(options.transparentColor);
-    this.gif.setFrameRate(options.framerate || 30);
-    this.options = options;
+    constructor(options){
+        super();
+        this.gif = new GIFEncoder(options.w||100,options.h||100);
+        this.gif.writeHeader();
+        this.gif.setRepeat(options.repeat || 0);
+        this.gif.highWaterMark = options.highWaterMark || 1280000;
+        this.gif.setTransparent(options.transparentColor);
+        this.gif.setFrameRate(options.framerate || 30);
+        this.options = options ;
+        this.buffers = [];
 
-    this.buffers = [];
-    if (options.cache) {
-      const resFile = createWriteStream(`./${options.filename || Date.now()}.gif`);
-      this.gif.pipe(resFile);
+        if (options.cache){
+            if (!fs.existsSync(cacheDir)){
+              fs.mkdir(cacheDir,{recursive:true});
+            }
+            this.cacheFilePath = `${cacheDir}/${options.filename || Date.now()}.gif`;
+            if (fs.existsSync(this.cacheFilePath) && options.cache != "overwrite"){
+                this.cacheAbort = true;
+                this.gif.flushData();
+                this.gif = fs.createReadStream(this.cacheFilePath);
+                this.emit("done", {file: fs.readFileSync(this.cacheFilePath) , name: options.filename+".gif"} );
+            }
+        }
+        this.lastFrame = options.lastFrame
+        this.gif.on('data', data => this.buffers.push(data) );
+        this.gif.once('end', async () => {
+            let concatBuff = Buffer.concat(this.buffers);
+            this.emit("done", {file: concatBuff , name: options.filename+".gif"} );
+            if (options.cache){
+                fs.writeFileSync(this.cacheFilePath, concatBuff);
+            }
+            //this.gif = null;
+        });
     }
 
-    this.lastFrame = options.lastFrame;
-    this.gif.on("data", (data) => this.buffers.push(data));
-    this.gif.once("end", async () => {
-      this.emit("done", file(Buffer.concat(this.buffers), `${options.filename}.gif`));
-      this.gif = null;
-    });
-  }
+    generate(fun){
+        if (this.cacheAbort) return false;
+        return Promise.all( [...Array(this.lastFrame).keys()].map(async (frameNumber,i)=>{
+            console.log({frameNumber})
+            let frame = fun(frameNumber);
+            await this.gif.addFrame(frame.getImageData(0, 0, this.options.w, this.options.h).data);
+            if (i == this.lastFrame) this.gif.finish();
+        })).then(done=>{
+            this.gif.finish();
+        })
+    }
 
-  async generate(fun) {
-    let frameNumber = 0;
-    renderedFrames = [];
-    // Array(this.lastFrame).fill(null).forEach(async (n, frameNumber) => {
-    
-    Array(this.lastFrame-1).fill(" ").map((_,frame)=>{
-      const frame = fun(frameNumber);
-      renderedFrames.push(frame);
-    });
-    renderedFrames.forEach(frame=>{
-      this.gif.addFrame(frame.getImageData(0, 0, this.options.w, this.options.h).data);
-    });
-
-    this.gif.finish();
-    // });
-  }
 }
 
 module.exports = Animation;
