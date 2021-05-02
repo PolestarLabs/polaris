a6f5372065cd22204ce26631398493e6c5bb5d3b
Tsumari <lucasflicky@hotmail.com>
2021-05-02 09:15:09 -0300

    sfrestart

--- core/commands/_botOwner/selfrestart.js
+++ core/commands/_botOwner/selfrestart.js
@@ -1,5 +1,7 @@
 const init = async (m) => {
-  m.reply("ok");
+  await m.channel.send(`${_emoji('off')} Shutting down...`);
+  await m.channel.send(`• **Exec Q size:** ${PLX.execQueue.length}`);
+  await m.addReaction(_emoji('off').reaction);
   PLX.softKill();
 };
 
