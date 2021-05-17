c0135594157fbe1e1a3312ac37be1c85c80ae181
Tsumari <lucasflicky@hotmail.com>
2021-05-17 03:15:34 -0300

    new migrate flow

--- core/commands/infra/migrate.js
+++ core/commands/infra/migrate.js
@@ -1,3 +1,4 @@
+
 const YesNo = require("../../structures/YesNo.js");
 // TRANSLATE[epic=translations] ?? migrate
 
@@ -23,7 +24,11 @@ const init = async function (msg, args) {
   const embed = {};
   const yesNoOptions = { embed, clearReacts: true, time: 120e3 };
 
-  const userData = await DB.users.get(msg.author.id);
+
+  const userData_OLD = await vDB.users.get(msg.author.id);
+  const userData_NEW = await DB.users.get(msg.author.id);
+
+  // SECTION SLIDE 1 ----------------------------------------------------------
 
   embed.color = 0x2b2b3F;
   embed.title = "Transfer in progress...";
@@ -57,243 +62,239 @@ Pollux collects usage data for analytics and telemetry purposes and does not sto
   embed.color = 0x2b2b3F;
   embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛" };
 
-  await wait(1);
   await prompt.edit({ embed });
-  await wait(2);
+  await wait(3);
 
   step = await YesNo(prompt, msg, false, false, false, yesNoOptions);
   if (!step) return;
-
-  embed.description = `
-${_emoji("loading")} • Transferring marriage
-       `;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛" };
-
-  await wait(1);
-  await prompt.edit({ embed });
-  const marr_mig = require("../misc/mrgt.js");
-  const marriage_transfer_res = await new Promise(async (res, rej) => {
-    await marr_mig.init(msg, args, res)
-      .catch((err) => {
-        console.error(err);
-        rej(err);
-      });
-  }).timeout(15e4).catch((err) => {
-    embed.color = 0xFF5500;
-    embed.title = "Migration Aborted!";
-    embed.footer.text = "TIMEOUT";
-    prompt.edit({ embed });
-    return null;
-  });
-
-  if (!marriage_transfer_res) return "Something went wrong with your migration process. Please try again.";
-
-  embed.description = `
-${_emoji("yep")} • Transferring marriages
-${_emoji("loading")} • Converting inventory
-
-           `;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦⬛⬛⬛⬛⬛⬛⬛⬛⬛" };
-  await wait(1);
-  await prompt.edit({ embed });
-
-  const oldInventory = userData.modules.inventory;
-  const newInventory = [];
-
-  oldInventory.forEach((item) => {
-    if (item.id) return;
-    const currItem = newInventory.find((sub) => sub.id === item);
-
-    if (currItem) {
-      if (currItem.id.includes("lootbox") && currItem.count < 10) currItem.count++;
-    } else newInventory.push({ id: item, count: 1 });
-  });
-
-  await DB.users.set(msg.author.id, { $set: { "modules.inventory": newInventory } }).catch(console.error);
-
-  userData.modules.inventory = newInventory;
-
-  await wait(1);
-
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("loading")} • Capping Lootboxes
-
-`;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦⬛⬛⬛⬛⬛⬛⬛⬛" };
-  await wait(1);
-  await prompt.edit({ embed });
-
-  await wait(1);
-
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("yep")} • Capping Lootboxes
-${_emoji("loading")} • Recalculating Gemstones
-${_emoji("loading")} • Transferring Daily Streak
-
-`;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦⬛⬛⬛⬛⬛⬛⬛" };
-  await wait(1);
-  await prompt.edit({ embed });
-
-  const newRubines = Math.min(~~((userData.modules.rubines || 0) * 0.05) + (userData.modules.dyStreakHard || 1) * 10, 50000);
-  const oldRubines = userData.modules.rubines || 0;
-  const jades      = ~~(userData.modules.jades / 2);
-  const saph       = ~~(userData.modules.sapphires * (SAPPHIREFACTOR(userData.donator, userData.formerDonator)) / 10 + 1);
-
-  await DB.users.set(msg.author.id, {
-    $set:
-        {
-          "counters.daily.streak": userData.modules.dyStreakHard || 0,
-          "counters.daily.last": userData.modules.daily || 0,
-          "modules.RBN": newRubines,
-          "modules.rubinesOld": oldRubines,
-          "modules.JDE": jades,
-          "modules.SPH": saph,
-        },
-  });
-
-  await wait(1);
-
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("yep")} • Capping Lootboxes
-${_emoji("yep")} • Recalculating Gemstones
-${_emoji("yep")} • Transferring Daily Streak (${userData.modules.dyStreakHard})
-${_emoji("loading")} • Baking a Cake`;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦🟦⬛⬛⬛⬛⬛⬛" };
-  await wait(1);
-  await prompt.edit({ embed });
-  msg.channel.send({ embed: { description: `(${newRubines} ${_emoji("RBN")}) (${jades} ${_emoji("JDE")}) (${saph} ${_emoji("SPH")})`, title: "Gemstones Recalculated" } });
-  embed.description += `\n${_emoji("loading")} • Awarding **Touhou Classic** Deck Skin`;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦🟦🟦⬛⬛⬛⬛⬛" };
-  await wait(2);
-
-  await DB.users.set(msg.author.id, { $inc: { "modules.SPH": -1 * marriage_transfer_res.cost || 0 } });
-
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("yep")} • Capping Lootboxes
-${_emoji("yep")} • Recalculating Gemstones
-${_emoji("yep")} • Transferring Daily Streak (${userData.modules.dyStreakHard})
-${_emoji("loading")} • Baking a Cake
-${_emoji("yep")} • Awarding **Touhou Classic** Deck Skin
+  // !SECTION SLIDE 1
 
 
-`;
-
-  await DB.users.set(msg.author.id, { $addToSet: { "modules.skinInventory": "casino_touhou-classic" } });
+  // SECTION SLIDE 2 ----------------------------------------------------------
 
   embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦🟦🟦🟦⬛⬛⬛⬛" };
-  await wait(1);
-  await prompt.edit({ embed });
-  await wait(1);
-  await wait(1);
 
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("yep")} • Capping Lootboxes
-${_emoji("yep")} • Recalculating Gemstones
-${_emoji("yep")} • Transferring Daily Streak (${userData.modules.dyStreakHard})
-${_emoji("loading")} • Baking a Cake
-${_emoji("yep")} • Awarding **Touhou Classic** Deck Skin
-${_emoji("loading")} • Declaring Rubines Income Tax 
-
-`;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦⬛⬛⬛" };
-  await wait(1);
-  await prompt.edit({ embed });
-  await wait(1);
-
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("yep")} • Capping Lootboxes
-${_emoji("yep")} • Recalculating Gemstones
-${_emoji("yep")} • Transferring Daily Streak (${userData.modules.dyStreakHard})
-${_emoji("loading")} • Baking a Cake
-${_emoji("yep")} • Awarding **Touhou Classic** Deck Skin
-${_emoji("yep")} • Declaring Rubines Income Tax 
-${_emoji("loading")} • Converting Cosmetics IDs
-
-`;
-
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦⬛⬛" };
-  await wait(1);
-  await wait(1);
-  await prompt.edit({ embed });
-
-  const myBGs = userData.modules.bgInventory;
-  const myBGsFULL = await DB.cosmetics.find({ code: { $in: myBGs } }).lean();
-
-  // await DB.users.set(msg.author.id, {$set:{'modules.bgInventory': myBGsFULL.map(b=>b.id) }});
-
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("yep")} • Capping Lootboxes
-${_emoji("yep")} • Recalculating Gemstones
-${_emoji("yep")} • Transferring Daily Streak (${userData.modules.dyStreakHard})
-${_emoji("loading")} • Baking a Cake
-${_emoji("yep")} • Awarding **Touhou Classic** Deck Skin
-${_emoji("yep")} • Declaring Rubines Income Tax 
-${_emoji("nope")} • Converting Cosmetics IDs (Waiting for BGs sheet)
-
-`;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦⬛" };
-  await wait(1);
-  await wait(1);
-  await prompt.edit({ embed });
-  await wait(1);
-
-  embed.description = `
-${_emoji("yep")} • Transferring Marriages
-${_emoji("yep")} • Converting Inventory
-${_emoji("yep")} • Capping Lootboxes
-${_emoji("yep")} • Recalculating Gemstones
-${_emoji("yep")} • Transferring Daily Streak (${userData.modules.dyStreakHard})
-${_emoji("yep")} • Baking a Cake
-${_emoji("yep")} • Awarding **Touhou Classic** Deck Skin
-${_emoji("yep")} • Declaring Rubines Income Tax 
-${_emoji("nope")} • Converting Cosmetics IDs (Waiting for BGs sheet)
-
-`;
-  embed.color = 0x2b2b3F;
-  embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦🟦" };
-  await wait(1);
-  await wait(1);
-  await prompt.edit({ embed });
-  await wait(1);
-
-  msg.channel.send("All set! Your account has been migrated to the New Super Fancy Pollux Database™ successfully! Enjoy the new features~");
-  await wait(1);
-  msg.channel.send("*Ah, also. Have this cake!*");
-  await wait(1);
-  msg.channel.send({
-    embed: {
-      title: "You received **1 Polaris Cake**",
-      thumbnail: { url: "https://cdn.discordapp.com/emojis/446901835865784321.png" },
-      description: `
-***Food Items** are used to recover **Stamina**, which is consumed to perform certain actions.
-Try using \`${msg.prefix}food info\` to learn more!*`,
+  const TASKLIST = {
+    
+    complete: function complete(id){
+      this.items[id].status = 'complete';
     },
-  });
+    notcomplete: function complete(id){
+      this.items[id].status = 'nope';
+    },
+    printProgress(){
+      return this.items.map(x=>x.status==='complete'?'🟦':x.status=='nope'?'🟦':'⬛').sort().reverse().join('');
+    },
+    printList(){
+      return this.items.map(it=>{
+        if (it.status == 'complete') return `${_emoji("yep")} • ${it.name}`;
+        if (it.status == 'pending') return `${_emoji("loading")} • ${it.name}`;
+        else return `${_emoji("nope")} • ${it.name}`;        
+      }).join('\n');
+    },
+    items: [
+      {
+        name: "Transferring Marriages",
+        wait: true,
+        status: "pending",
+        action: async function(){
+          const marr_mig = require("../misc/mrgt.js");
+          const marriage_transfer_res = await new Promise(async (res, rej) => {
+            await marr_mig.init(msg, args, res)
+            .catch((err) => {
+              console.error(err);
+              rej(err);
+            });
+          }).timeout(15e4).catch((err) => {
+            embed.color = 0xFF5500;
+            embed.title = "Migration Aborted!";
+            embed.footer.text = "TIMEOUT";
+            prompt.edit({ embed });
+            return null;
+          });
+      
+          if (!marriage_transfer_res) {
+            msg.channel.send("Something went wrong with your migration process. Please try again.");
+            return false;      
+          }
+
+          console.log('postmarry', {marriage_transfer_res});
+
+          await DB.users.set(msg.author.id, { $inc: { "modules.SPH": -1 * marriage_transfer_res.cost || 0 } });
+
+          return true;
+        } 
+      },
+      {
+        name: "Converting Inventory",
+        wait: true,
+        status: "pending",
+        action: async function(){
+
+          const oldInventory = userData_OLD.modules.inventory;
+          const newInventory = [];
+          
+          oldInventory.forEach((item) => {
+            if (item.id) return;
+            const currItem = newInventory.find((sub) => sub.id === item);
+            
+            if (currItem) {
+              if (currItem.id.includes("lootbox") && currItem.count < 10) currItem.count++;
+            } else newInventory.push({ id: item, count: 1 });
+          });
+          
+          await DB.users.set(msg.author.id, { $set: { "modules.inventory": newInventory } }).catch(console.error);
+          userData_OLD.modules.inventory = newInventory;
+          
+          return true;
+        }
+      },
+      {
+        name: "Capping Lootboxes",
+        wait: true,
+        status: "pending",
+        action: async function(){
+          return false;
+        }
+      },
+      {
+        name: "Recalculating Gemstones",
+        wait: true,
+        status: "pending",
+        action: async function(){
+                  
+          const newRubines = Math.min(~~((userData_OLD.modules.rubines || 0) * 0.05) + (userData_OLD.modules.dyStreakHard || 1) * 10, 50000);
+          const oldRubines = userData_OLD.modules.rubines || 0;
+          const jades      = ~~(userData_OLD.modules.jades / 2);
+          const saph       = ~~(userData_OLD.modules.sapphires * (SAPPHIREFACTOR(userData_OLD.donator, userData_OLD.formerDonator)) / 10 + 1);
+
+          await DB.users.set(msg.author.id, {
+            $set:
+                {
+                  "counters.daily.streak": userData_OLD.modules.dyStreakHard || 0,
+                  "counters.daily.last": userData_OLD.modules.daily || 0,
+                  "modules.RBN": newRubines,
+                  "modules.rubinesOld": oldRubines,
+                  "modules.JDE": jades,
+                  "modules.SPH": saph,
+                },
+          });
+          
+          return true;
+        }
+      },
+      {
+        name: `Transferring Daily Streak (--)`,
+        wait: true,
+        status: "pending",
+        action: async function(){
+          this.name = `Transferring Daily Streak (${userData_OLD.modules.dyStreakHard})`;
+          return true;
+        }
+      },
+      {
+        name: "Baking a Cake",
+        wait: false,
+        status: "pending",
+        action: async function(){
+          await wait (10);          
+          return true;
+        }
+      },
+      {
+        name: "Awarding **Touhou Classic** Deck Skin",
+        wait: false,
+        status: "pending",
+        action: async function(){
+          await wait (10);
+          return true;
+        }
+      },
+      {
+        name: "Declaring Rubines Income Tax ",
+        wait: true,
+        status: "pending",
+        action: async function(){
+          return true;
+        }
+      },
+      {
+        name: "Converting Cosmetics IDs (Waiting for BGs sheet) ",
+        wait: true,
+        status: "pending",
+        action: async function(){
+          return true;
+
+          const myBGs = userData_OLD.modules.bgInventory;
+          const myBGsFULL = await DB.cosmetics.find({ code: { $in: myBGs } }).lean();
+
+          // await DB.users.set(msg.author.id, {$set:{'modules.bgInventory': myBGsFULL.map(b=>b.id) }});
+        }
+      },
+    ],
+  };
+
+   let actionsDone = 0;
+  let totalActions = TASKLIST.items.length;
+console.log({totalActions})
+  for (let i = 0; i < totalActions; i++) {
+
+    embed.description = TASKLIST.printList();
+    await prompt.edit({embed});
+
+
+    let result;
+    if (TASKLIST.items[i].wait){
+      result = await TASKLIST.items[i].action();
+      postTask(result,i);
+    }else{
+      TASKLIST.items[i].action().then(r=>postTask(r,i));
+    }
+
+    embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦" + TASKLIST.printProgress() };
+    embed.description = TASKLIST.printList();
+    await prompt.edit({embed});
+    await wait(2);
+  };
+
+  async function postTask(result,i){
+
+    if (result) TASKLIST.complete(i);
+    else TASKLIST.notcomplete(i);
+    
+    console.log('action result: ',result,"\n-------");
+    
+    await wait(1);
+    console.log('posttask')
+    actionsDone++;
+    console.log({actionsDone,totalActions,name: TASKLIST.items[i].name})
+
+    if (actionsDone >= totalActions){
+
+      embed.footer = { icon_url: msg.author.avatarURL, text: "Progress: 🟦🟦" + TASKLIST.printProgress() };
+      embed.description = TASKLIST.printList();
+      await prompt.edit({embed});
+      await wait(1);
+
+      msg.channel.send("All set! Your account has been migrated to the New Super Fancy Pollux Database™ successfully! Enjoy the new features~");
+      await wait(1);
+      msg.channel.send("*Ah, also. Have this cake!*");
+      await wait(1);
+      msg.channel.send({
+        embed: {
+          title: "You received **1 Polaris Cake**",
+          thumbnail: { url: "https://cdn.discordapp.com/emojis/446901835865784321.png" },
+          description: `
+          ***Food Items** are used to recover **Stamina**, which is consumed to perform certain actions.
+          Try using \`${msg.prefix}food info\` to learn more!*`,
+        },
+      });
+    };
+  }
+
 };
+
+
 
 module.exports = {
   init,
