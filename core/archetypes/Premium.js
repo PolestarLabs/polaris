61b12761f4856ce0c0befcf91b968e6169e20535
Tsumari <lucasflicky@hotmail.com>
2021-05-20 06:30:04 -0300

    [NEW] provisional prime rewards calculator

---
+++ core/archetypes/Premium.js
@@ -0,0 +1,414 @@
+// @ts-nocheck
+const RUNNING_YEAR = new Date().getUTCFullYear();
+const RUNNING_MONTH = new Date().getUTCMonth();
+const RUNNING_MONTH_SHORT = new Date().toLocaleString('en', { month: 'short' }).toLowerCase();
+const TURNING_DAY = 5; // when Prime starts
+const GRACE_WARNING_DAY = 10; // when Prime starts yelling
+const GRACE_TURNING_DAY = 15; // when Prime shuts down
+
+const PREMIUM_INFO = {
+    neutrino: {
+        $:  1000,
+        roleID: "532987435680923648",
+        daily_bonus: 1500,
+        monthly_sph: 100,
+        immediate_sph: 150,
+        box_bonus: [{n:15,t:'UR'}],
+        monthly_jde: 10000,
+        monthly_event_tkn: 2500,
+        booster_bonus_box: [{n:10,t:'SR'}],
+        booster_bonus_psm: 500,
+        sticker_prize: {
+            LAST: 2,
+            RANDOM: 5,
+            PACK: 8
+        },
+        prime_servers: 25,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    antimatter: {
+        $:  500,
+        roleID: "466737702772015144",
+        daily_bonus: 1000,
+        monthly_sph: 100,
+        immediate_sph: 150,
+        box_bonus: [{n:15,t:'UR'}],
+        monthly_jde: 10000,
+        monthly_event_tkn: 2500,
+        booster_bonus_box: [{n:10,t:'SR'}],
+        booster_bonus_psm: 500,
+        sticker_prize: {
+            LAST: 2,
+            RANDOM: 5,
+            PACK: 5
+        },
+        prime_servers: 8,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    astatine: {
+        $:  100,
+        roleID: "467032160763772948",
+        daily_bonus: 500,
+        monthly_sph: 50,
+        immediate_sph: 100,
+        box_bonus: [{n:10,t:'UR'}],
+        monthly_jde: 5000,
+        monthly_event_tkn: 1000,
+        booster_bonus_box: [{n:5,t:'R'}],
+        booster_bonus_psm: 200,
+        sticker_prize: {
+            LAST: 2,
+            RANDOM: 3,
+            PACK: 3
+        },
+        prime_servers: 8,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    uranium: {
+        $:  75,
+        roleID: "544620092478980111",
+        daily_bonus: 400,
+        monthly_sph: 50,
+        immediate_sph: 30,
+        box_bonus: [{n:8,t:'UR'}],
+        monthly_jde: 2000,
+        monthly_event_tkn: 700,
+        booster_bonus_box: [{n:1,t:'C'}],
+        booster_bonus_psm: 100,
+        sticker_prize: {
+            LAST: 2,
+            RANDOM: 2,
+            PACK: 3
+        },
+        prime_servers: 5,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    zircon: {
+        $:  50,
+        roleID: "467100376777228298",
+        daily_bonus: 300,
+        monthly_sph: 30,
+        immediate_sph: 25,
+        box_bonus: [{n:5,t:'UR'}],
+        monthly_jde: 2000,
+        monthly_event_tkn: 700,
+        booster_bonus_box: [{n:1,t:'UR'}],
+        booster_bonus_psm: 100,
+        sticker_prize: {
+            LAST: 2,
+            RANDOM: 2,
+            PACK: 2
+        },
+        prime_servers: 5,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    palladium: {
+        $:  25,
+        roleID: "532987443117424640",
+        daily_bonus: 200,
+        monthly_sph: 20,
+        immediate_sph: 15,
+        box_bonus: [{n:5,t:'SR'}],
+        monthly_jde: 1000,
+        monthly_event_tkn: 500,
+        booster_bonus_box: [{n:1,t:'SR'}],
+        booster_bonus_psm: 75,
+        sticker_prize: {
+            LAST: 1,
+            RANDOM: 2,
+            PACK: 1
+        },
+        prime_servers: 3,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    lithium: {
+        $:  15,
+        roleID: "467100151161290762",
+        daily_bonus: 150,
+        monthly_sph: 12,
+        immediate_sph: 10,
+        box_bonus: [{n:5,t:'SR'}],
+        monthly_jde: 1000,
+        monthly_event_tkn: 500,
+        booster_bonus_box: [{n:1,t:'SR'}],
+        booster_bonus_psm: 50,
+        sticker_prize: {
+            LAST: 1,
+            RANDOM: 1,
+            PACK: 1
+        },
+        prime_servers: 2,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    //DEPRECATE
+    iridium: {
+        $:  15,
+        roleID: "532987440831660072",
+        daily_bonus: 150,
+        monthly_sph: 12,
+        immediate_sph: 10,
+        box_bonus: [{n:5,t:'SR'}],
+        monthly_jde: 1000,
+        monthly_event_tkn: 500,
+        booster_bonus_box: [{n:1,t:'C'}],
+        booster_bonus_psm: 50,
+        sticker_prize: {
+            LAST: 1,
+            RANDOM: 1,
+            PACK: 1
+        },
+        prime_servers: 2,
+        prime_reallocation: true,
+        custom_background: true,
+        custom_handle: true,
+        custom_shop: false,
+
+    },
+    //=====================
+    carbon: {
+        $:  10,
+        roleID: "467100086330195998",
+        daily_bonus: 100,
+        monthly_sph: 5,
+        immediate_sph: 5,
+        box_bonus: [{n:5,t:'U'}],
+        monthly_jde: 1000,
+        monthly_event_tkn: 500,
+        booster_bonus_box: [{n:1,t:'SR'}],
+        booster_bonus_psm: 30,
+        sticker_prize: {
+            LAST: 1,
+            RANDOM: 1,
+            PACK: 1
+        },
+        prime_servers: 1,
+        prime_reallocation: true,
+        custom_background: false,
+        custom_handle: true,
+        custom_shop: false,
+    },
+    iron: {
+        $:  10,
+        roleID: "532987438700822529",
+        daily_bonus: 100,
+        monthly_sph: 2,
+        immediate_sph: 10,
+        box_bonus: [{n:5,t:'R'}],
+        monthly_jde: 1000,
+        monthly_event_tkn: 500,
+        booster_bonus_box: [{n:1,t:'SR'}],
+        booster_bonus_psm: 30,
+        sticker_prize: {
+            LAST: 1,
+            RANDOM: 1,
+            PACK: 0
+        },
+        prime_servers: 1,
+        prime_reallocation: true,
+        custom_background: false,
+        custom_handle: true,
+        custom_shop: false,
+    },
+    aluminium: {
+        $:  5,
+        roleID: "532993206040920065",
+        daily_bonus: 50,
+        monthly_sph: 3,
+        immediate_sph: 3,
+        box_bonus: [{n:5,t:'U'}],
+        monthly_jde: 250,
+        monthly_event_tkn: 150,
+        booster_bonus_box: [{n:1,t:'R'}],
+        booster_bonus_psm: 15,
+        sticker_prize: {
+            LAST: 0,
+            RANDOM: 1,
+            PACK: 0
+        },
+        prime_servers: 1,
+        prime_reallocation: true,
+        custom_background: false,
+        custom_handle: false,
+        custom_shop: false,
+
+    },
+    plastic: {
+        $:  2,
+        roleID: "544620335115534349",
+        daily_bonus: 10,
+        monthly_sph: 1,
+        immediate_sph: 1,
+        box_bonus: [],
+        monthly_jde: 100,
+        monthly_event_tkn: 50,
+        booster_bonus_box: [{n:1,t:'C'}],
+        booster_bonus_psm: 5,
+        sticker_prize: false,
+        prime_servers: 0,
+        prime_reallocation: false,
+        custom_background: false,
+        custom_handle: false,
+        custom_shop: false,
+
+    },
+}
+
+const PREMIUM_STICKERS = DB.cosmetics.find({public: true, GROUP:"plx_collection"}).noCache().lean();
+const PREMIUM_PACKS = DB.items.find({type: "boosterpack", GROUP:"plx_collection"}).noCache().lean();
+
+
+async function processRewards( userID, {mansionMember, dry_run= false} ){
+    const userData = await DB.users.findOne({id:userID}).noCache();
+
+    const tierPrizes = getTierBonus(userData.donator);
+
+    if (tierPrizes.sticker_prize){
+
+        const ownedStickers = userData.modules.stickerInventory;
+        const [stickerList,packsList] = await Promise.all([PREMIUM_STICKERS,PREMIUM_PACKS]);
+        
+        const availableStickerList = stickerList.filter(stk=> !ownedStickers.includes(stk.id) );
+        const availablePacks = packsList.filter(pkg=> !pkg.name.includes(RUNNING_YEAR) );
+
+        let awardedStickers = [];
+
+        const lasts = []
+        if (tierPrizes.sticker_prize.LAST > 0){
+            let adds = tierPrizes.sticker_prize.LAST;
+            while (adds-- > 0){
+                let toPush = availableStickerList.pop().id;
+                awardedStickers.push(toPush);
+                lasts.push(toPush);
+            }
+        }
+
+        const randoms = []
+        if (tierPrizes.sticker_prize.RANDOM > 0){
+            let adds = tierPrizes.sticker_prize.LAST;
+            let availableRandomStickers = shuffle(availableStickerList);
+            while (adds-- > 0){
+                availableRandomStickers = shuffle(availableRandomStickers);
+                let toPush = availableRandomStickers.pop().id;
+                awardedStickers.push(toPush);
+                randoms.push(toPush);
+            }
+        }
+
+        const packQueries = [];
+        if (tierPrizes.sticker_prize.PACK > 0){
+            let adds = tierPrizes.sticker_prize.PACK;
+            while (adds-- > 0){
+                console.log('hi')
+                let query = {
+                    updateOne: { 
+                        filter: { id: userID, "modules.inventory.id": shuffle(availablePacks)[0].id },
+                        update: { $inc: { "modules.inventory.$.count": 1 } }
+                    }
+                };
+                packQueries.push(query);
+            }
+        }
+        const bulkWriteQuery = [
+            {updateOne: { 
+                filter: {id: userID},
+                update: {
+                    $addToSet: {
+                        "modules.stickerInventory": {$each: awardedStickers }
+                    }
+                }
+            }},
+            ...packQueries
+        ];
+
+        if (dry_run){
+            let data = {
+                tier: userData.donator,
+                stk_prizes: tierPrizes.sticker_prize
+            }
+            return {data,bulkWriteQuery,randoms,lasts};
+        } 
+
+        //await DB.users.bulkWrite(bulkWriteQuery);
+    }
+    
+
+
+};
+
+
+const baseline = 125;
+
+const DAILY = {
+    antimatter:500,
+    astatine:  100,
+    uranium:   75,
+    zirconium: 50,
+    palladium: 25,
+    lithium:   15,
+    carbon:    10,
+    iridium:   10,
+    aluminium: 5,
+    plastic:   1,
+}
+
+const DAILY_GETS = {
+    antimatter:1000,
+    astatine:  800,
+    uranium:   500,
+    zirconium: 300,
+    palladium: 200,
+    lithium:   150,
+    carbon:    150,
+    iridium:   100,
+    aluminium: 50,
+    plastic:   10,
+}
+
+async function getTier(userID) {
+    const usr = await DB.users.get(userID);
+    const tier = usr.donator;
+    return tier?.toLowerCase() || null;
+    // return false;
+}
+
+async function getDailyBonus(user){
+    const tier = await getTier(user.id || user);
+    return tier ? DAILY_GETS[tier] : 0;
+}
+
+function getTierBonus(tier){
+    if (!tier) return null;
+    return PREMIUM_INFO[tier.toLowerCase()] || null;
+}
+
+module.exports = {
+    getDailyBonus, getTier, tierInfo: PREMIUM_INFO, processRewards
+}
