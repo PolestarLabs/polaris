4dc7686cb030bfe8830525ac9b5be386fbc4d600
Tsumari <lucasflicky@hotmail.com>
2021-05-26 04:37:02 -0300

    selfrestart passes message to softkill

--- core/commands/_botOwner/selfrestart.js
+++ core/commands/_botOwner/selfrestart.js
@@ -2,7 +2,7 @@ const init = async (m) => {
   await m.channel.send(`${_emoji('off')} Shutting down...`);
   await m.channel.send(`• **Exec Q size:** ${PLX.execQueue.length}`);
   await m.addReaction(_emoji('off').reaction);
-  PLX.softKill();
+  PLX.softKill(m);
 };
 
 module.exports = {
