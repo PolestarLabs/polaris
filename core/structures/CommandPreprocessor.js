7b916024440f3fc5ea15a5de1a2e13025c4ed8b9
Tsumari <lucasflicky@hotmail.com>
2021-05-28 07:59:29 -0300

    cleanup

--- core/structures/CommandPreprocessor.js
+++ core/structures/CommandPreprocessor.js
@@ -209,7 +209,6 @@ function QUEUED_COMMAND(commandFile) {
     if (PLX.restarting) {
       return args[0]?.reply(_emoji('TIME1') + " • Restart in progress... please wait up to a minute.");
     }
-    console.log({commandFile})
     const execCommand = commandFile.init ? commandFile.init(...args) : commandFile.gen(...args);
     PLX.execQueue.push(new Promise((res) => execCommand.then(res)));
     return execCommand;
