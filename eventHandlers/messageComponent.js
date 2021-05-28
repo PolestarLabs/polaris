28820b1017f3b4ec247c0cc887caedbf7afe8b53
Tsumari <lucasflicky@hotmail.com>
2021-05-28 07:59:52 -0300

    isolate button  events in dedicated files

--- eventHandlers/messageComponent.js
+++ eventHandlers/messageComponent.js
@@ -1,52 +1,9 @@
 const { image } = require("stackblur-canvas");
 
 module.exports = async ( interaction, data) => {
-    if (data.custom_id == "booruSave") {
-        const emb = interaction.message.embeds[0];
-        const save = {
-            url: emb.image.url,
-            saved: interaction.message.timestamp,
-            tags: emb.fields.tags ? emb.fields.tags.replaceAll(/[\[\]\`]/,"") : "",
-            nsfw: emb.color == 16731205,
-        }
-        let res = await DB.usercols.set(interaction.userID, { $addToSet: { "collections.boorusave": save } });
-        console.log({res})
-        if (res?.nModified !== 1) return;
-
-        Progression.emit("action.gallery.save",{msg:interaction.message,userID:interaction.userID,value:1});
-        interaction.reply({
-            flags: 64,
-            content: "Saved to your gallery!",
-            components: [{type:1, components:[
-                {
-                    type: 2,
-                    style:5,
-                    url: `${paths.DASH}/dash/boorusave`,
-                    label: "See Gallery"
-                }
-            ]}]
-        });
-        let oldButtonLabel = interaction.messageRaw.components?.[0]?.components?.[0]?.label||"";
-        const oldLabelNumber = Number(oldButtonLabel.match(/\(([0-9]+)\)/)?.[1] || 0);
-        if (!oldLabelNumber) oldButtonLabel+= " (0)";
-        let newLabel = oldButtonLabel.replace(oldLabelNumber,oldLabelNumber+1);
-
-        interaction.message.edit({
-            content:  interaction.message.content,
-            components: [
-                {type:1, components:[{
-                  type: 2,
-                  style: 2,
-                  label: newLabel,
-                  custom_id: "booruSave",
-                  emoji: {name:"⭐"}
-                },{
-                    type: 2,
-                    style:5,
-                    url: `${paths.DASH}/dash/boorusave`,
-                    label: "See your Gallery"
-                }]}
-              ]
-        })
+    try{
+        (require(`./interactions/${data.custom_id.split(":")[0]}.js`))(interaction, data)
+    } catch (err){
+        
     }
 }
