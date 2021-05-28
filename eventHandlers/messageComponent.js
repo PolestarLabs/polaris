171646ff6f08328f247cf4f03f047fc19209c14f
Tsumari <lucasflicky@hotmail.com>
2021-05-28 06:52:55 -0300

    proto button event handler

---
+++ eventHandlers/messageComponent.js
@@ -0,0 +1,19 @@
+module.exports = async ( interaction, data) => {
+    if (data.custom_id == "booruSave") {
+        console.log(interaction.message.embeds)
+        //DB.usercols.set(reaction.author.id, { $addToSet: { "collections.boorusave": save } });
+        //Progression.emit("action.gallery.save",{msg:ms,userID:reaction.userID,value:1});
+        interaction.reply({
+            flags: 64,
+            content: "Saved to your gallery!",
+            components: [{type:1, components:[
+                {
+                    type: 2,
+                    style:5,
+                    url: `${paths.DASH}/dash/gallery}`,
+                    label: "See Galery"
+                }
+            ]}]
+        })
+    }
+}
