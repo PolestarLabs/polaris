5a00d91a2fbe7536892052e0d2b41fb82a131d32
Tsumari <lucasflicky@hotmail.com>
2021-05-29 15:27:09 -0300

    most useless shit ever

---
+++ core/commands/_botOwner/horn.js
@@ -0,0 +1,21 @@
+
+const init = async function (msg) {
+    msg.delete();
+    msg.channel.send({content: _emoji("__")+"",
+        components:[{type:1,components:[
+            {type:2,style:4,emoji:{name:"🎺"},custom_id:"playHorn:horn"},
+            //{type:2,style:2,emoji:{id:"334654028535431168"},custom_id:"playHorn:verstappen"},
+            {type:2,style:2,emoji:{name:"⛳"},custom_id:"playHorn:bolinha"},
+           // {type:2,style:2,emoji:{id:"434743628800458793"},custom_id:"playHorn:noot"},
+        ]}]
+    })
+};
+
+module.exports = {
+  init,
+  pub: false,
+  cmd: "stuck",
+  cat: "sound",
+  botPerms: ["attachFiles", "embedLinks"],
+  aliases: ["buzina"],
+};
---
+++ eventHandlers/interactions/playHorn.js
@@ -0,0 +1,17 @@
+const BOARD = require("../../core/archetypes/Soundboard.js");
+module.exports = async (interaction, data)=>{
+    interaction.ack();
+    const fakeMsg = interaction.message;
+    fakeMsg.author = interaction.member.user;
+    fakeMsg.member = await PLX.resolveMember(fakeMsg.guild.id,interaction.userID);
+    console.log(fakeMsg)
+    
+    
+    const [,sound] = data.custom_id.split(":");
+    if (sound=="horn") BOARD.play(fakeMsg, "https://cdn.discordapp.com/attachments/488142034776096772/848254716765143071/colonydeed_partyhorn.ogg",{dontLeave:true});
+    if (sound=="verstappen") BOARD.play(fakeMsg, "https://cdn.discordapp.com/attachments/415550879823953930/755212852184481983/verstappen-after-another-race-mugello-2020-tuscan-gp.mp3",{dontLeave:true});
+    if (sound=="bolinha") BOARD.play(fakeMsg, `${paths.ASSETS}/sound/bolinadegolfe.mp3` ,{dontLeave:true});
+    if (sound=="noot") BOARD.play(fakeMsg, `${paths.ASSETS}/sound/noot.mp3` ,{dontLeave:true});
+    
+
+}
