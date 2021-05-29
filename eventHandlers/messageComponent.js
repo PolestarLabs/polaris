5778dfd2fe4c722d551c03b8858d2366b8e89524
Tsumari <lucasflicky@hotmail.com>
2021-05-29 15:33:44 -0300

    message components preliminary multiple types fork

--- eventHandlers/messageComponent.js
+++ eventHandlers/messageComponent.js
@@ -1,8 +1,8 @@
-const { image } = require("stackblur-canvas");
-
 module.exports = async ( interaction, data) => {
     try{
-        (require(`./interactions/${data.custom_id.split(":")[0]}.js`))(interaction, data)
+        return (require(`./interactions/${data.custom_id.split(":")[0]}.js`))(interaction, data);
+        //if (data.component_type === 3) return (require(`./interactions/${data.custom_id.split(":")[0]}.js`))(interaction, data);
+
     } catch (err){
         
     }
