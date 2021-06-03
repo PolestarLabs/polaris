2b4ccbbf29eb02810d57d852d80b25ee5140ed81
Tsumari <lucasflicky@hotmail.com>
2021-06-03 01:30:34 -0300

    D0-P: fix missing schema reference in crafting item

--- core/commands/cosmetics/craft.js
+++ core/commands/cosmetics/craft.js
@@ -35,7 +35,7 @@ const init = async (msg, args) => {
     if (!craftedItem) {
       const userDiscoveries = (await DB.users.get(msg.author.id))?.modules.inventory.filter((itm) => itm.crafted).map((itm) => itm.id) || [];
       const searchResults = await Crafter.searchItems(toBeCrafted);
-      const DYM = searchResults.filter((x) => x.open || userDiscoveries.includes(x.id)).map((x) => `${x.name} (\`${x.code}\`)`);
+      const DYM = searchResults.filter((x) => x._doc.open || userDiscoveries.includes(x.id)).map((x) => `${x.name} (\`${x.code}\`)`);
       const res = DYM.length === 1 ? $t("responses.crafting.didyoumeanOne", P) : $t("responses.crafting.didyoumean", P);
 
       if (DYM.length > 0) {
