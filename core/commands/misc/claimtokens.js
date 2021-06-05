6e957c0e1807e0dc74a52b9ebdf8629198173700
Tsumari <lucasflicky@hotmail.com>
2021-06-05 13:30:51 -0300

    claimtokens command

---
+++ core/commands/misc/claimtokens.js
@@ -0,0 +1,19 @@
+const init = async function (msg){
+
+    const oldUser = await vDB.findOne({id:msg.author.id});
+    if (!oldUser || oldUser.switches?.tokensMigrated)
+        return msg.addReaction(_emoji('nope').reaction);
+
+    await DB.users.set(msg.author.id, {$set:{"eventGoodie":oldUser.eventGoodie}} );
+    
+    return msg.addReaction(_emoji('yep').reaction);
+
+}
+module.exports={
+    init
+    ,pub:false
+    ,cmd:'claimtokens'
+    ,cat:'misc'
+    ,botPerms:['attachFiles','embedLinks']
+    ,aliases:["migratetoken"]
+}
