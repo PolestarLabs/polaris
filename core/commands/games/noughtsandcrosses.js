07dc51e862ab97645817465b2ed9df63611c9525
Bsian <chharry321@gmail.com>
2021-06-08 00:04:54 +0100

    Emoji fixes

--- core/commands/games/noughtsandcrosses.js
+++ core/commands/games/noughtsandcrosses.js
@@ -19,7 +19,7 @@ const init = async (msg, args) => {
     const [x, y] = d.data.custom_id.split(',');
     d.message.components[y - 1].components[x - 1].style = playerTurnIndex ? 1 : 4;
     d.message.components[y - 1].components[x - 1].label = '';
-    d.message.components[y - 1].components[x - 1].emoji = { name: playerTurnIndex ? 'heavy_multiplication_x' : 'o' };
+    d.message.components[y - 1].components[x - 1].emoji = { name: playerTurnIndex ? '✖️' : '⭕' };
     d.message.components[y - 1].components[x - 1].disabled = true;
     playerTurnIndex = playerTurnIndex ? 0 : 1;
     await PLX.requestHandler.request('POST', `/interactions/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: `It is now <@${players[playerTurnIndex]}>'s turn`, components: d.message.components }}).then(console.log)
