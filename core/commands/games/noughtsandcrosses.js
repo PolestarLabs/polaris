49413e33173233bae2fe831d9c97abb26c833d48
Bsian <chharry321@gmail.com>
2021-06-07 23:50:41 +0100

    Endpoint

--- core/commands/games/noughtsandcrosses.js
+++ core/commands/games/noughtsandcrosses.js
@@ -12,15 +12,15 @@ const init = async (msg, args) => {
 
   const listener = async (d) => {
     if (d.message.id !== msg.id) return;
-    if (!players.includes(d.member.user.id)) return PLX.requestHandler.request('POST', `/webhooks/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: 'Invalid user', flags: 64 } });
-    if (players[playerTurnIndex] !== d.member.user.id) return PLX.requestHandler.request('POST', `/webhooks/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: 'Turn is currently other user', flags: 64 } });
+    if (!players.includes(d.member.user.id)) return PLX.requestHandler.request('POST', `/interactions/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: 'Invalid user', flags: 64 } });
+    if (players[playerTurnIndex] !== d.member.user.id) return PLX.requestHandler.request('POST', `/interactions/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: 'Turn is currently other user', flags: 64 } });
     const [x, y] = d.data.custom_id.split(',');
     d.message.components[y - 1].components[x - 1].style = playerTurnIndex ? 1 : 4;
     d.message.components[y - 1].components[x - 1].label = '';
     d.message.components[y - 1].components[x - 1].emoji = { name: playerTurnIndex ? 'heavy_multiplication_x' : 'o' };
     d.message.components[y - 1].components[x - 1].disabled = true;
     playerTurnIndex = playerTurnIndex ? 0 : 1;
-    await PLX.requestHandler.request('POST', `/webhooks/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: `It is now <@${players[playerTurnIndex]}>'s turn`, components: d.message.components }})
+    await PLX.requestHandler.request('POST', `/interactions/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: `It is now <@${players[playerTurnIndex]}>'s turn`, components: d.message.components }})
   }
 
   await msg.channel.createMessage({
