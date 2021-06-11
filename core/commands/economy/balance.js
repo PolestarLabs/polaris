a9ccf0674aaf97712cb9634c2f14dc2f96d5b872
Tsumari <lucasflicky@hotmail.com>
2021-06-11 12:54:01 -0300

    restyle bal

--- core/commands/economy/balance.js
+++ core/commands/economy/balance.js
@@ -35,20 +35,21 @@ const init = async (msg) => {
 
     const ts = moment(x.timestamp).format("hh:mma | DD/MMM").padStart(16, "\u200b ");
     if (x.type === "SEND") x.type = "TRANSFER";
-    if (x.from && x.to === TARGETDATA.id && x.from !== POLid) {
-      othPart = (await PLX.getTarget(x.from, null, true)) || { tag: "Unknown#0000" };
+    if (x.to === POLid || x.to === 'DASHBOARD' ) return `🔴 \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b ╰ |   *${x.type}*`;
+    if (x.from === POLid || x.from === 'DASHBOARD' ) return `🟢 \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b ╰ |   *${x.type}*`;
+
+    if ( x.from && (x.to === TARGETDATA.id && x.from !== POLid) ) {
+      othPart = (await PLX.getTarget(x.from, null, true)) || { tag: x.from };
       if (!othPart) return ` \`${ts}\` **${x.amt}** ${x.currency}\n\u200b\u2003\u2003|   *\`${x.type}\`* from ${x.to}`;
-      return `↔ \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b\u2003\u2003|   `
-        + `*\`${x.type}\`* from [${othPart?.tag}](${paths.DASH}/p/${othPart?.id}) \`${othPart.id}\` `;
+      return `↔ \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b ╰ |   `
+        + `*\`${x.type}\`* from [${othPart?.tag}](${paths.DASH}/p/${othPart?.id})   `;
     }
-    if (x.to && x.from === TARGETDATA.id && x.to !== POLid) {
-      othPart = (await PLX.getTarget(x.to, null, true)) || { tag: "Unknown#0000" };
+    if ( x.to && (x.from === TARGETDATA.id && x.to !== POLid) ) {
+      othPart = (await PLX.getTarget(x.to, null, true)) || { tag: x.to };
       if (!othPart) return ` \`${ts}\` **${x.amt}** ${x.currency}\n\u200b\u2003\u2003|   *\`${x.type}\`* to ${x.to}`;
-      return `↔  \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b\u2003\u2003|   `
-        + `*\`${x.type}\`* to [${othPart?.tag}](${paths.DASH}/p/${othPart?.id}) \`${othPart.id}\` `;
+      return `↔  \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b ╰ |   `
+        + `*\`${x.type}\`* to [${othPart?.tag}](${paths.DASH}/p/${othPart?.id})  `;
     }
-    if (x.to === POLid) return `🔴 \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b\u2003\u2003|   *${x.type}*`;
-    if (x.from === POLid) return `🟢 \`${ts}\` ${_emoji(x.currency,x.currency)} **${x.amt}**\n\u200b\u2003\u2003|   *${x.type}*`;
 
     return "";
   }
