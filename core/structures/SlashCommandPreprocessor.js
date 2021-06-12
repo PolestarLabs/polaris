5c769d16ae07669c75807bf09d4934ebea5bbf4b
Tsumari <lucasflicky@hotmail.com>
2021-06-12 04:59:42 -0300

    global slash commands, update command

--- core/structures/SlashCommandPreprocessor.js
+++ core/structures/SlashCommandPreprocessor.js
@@ -15,18 +15,28 @@ const createCommand = (payload,guild) => {
     if (guild) return PLX.requestHandler.request("POST",`/applications/${PLX.user.id}/guilds/${guild}/commands`,true, payload);
     else  return PLX.requestHandler.request("POST",`/applications/${PLX.user.id}/commands`,true, payload);
 }
+const updateCommand = (cmdID,payload,guild) => {
+    console.log(payload,'payload')
+    if (guild) return PLX.requestHandler.request("PATCH",`/applications/${PLX.user.id}/guilds/${guild}/commands/${cmdID}`,true, payload);
+    else  return PLX.requestHandler.request("PATCH",`/applications/${PLX.user.id}/commands/${cmdID}`,true, payload);
+}
 
-exports.proc = function(cmdFile){
+exports.proc = async function(cmdFile){
     
     if (cmdFile?.slashOptions?.guilds){
-        console.log('guilds');
         cmdFile.slashOptions.guilds.forEach( async guild=>{
-            console.log({guild})
             let guildCommands = await getAllCommands(guild);
             let currentCommand = guildCommands.find(c=>c.name === cmdFile.cmd);
             if (currentCommand){
                 if (cmdFile.disabled){
                     await disableCommand( currentCommand.id , guild );
+                } else {
+                    updateCommand(currentCommand.id,{
+                        name: cmdFile.cmd,
+                        description: cmdFile.slashOptions.description || $t(`commands:help.${cmdFile.cmd}`,"No Command Description"),
+                        options:  cmdFile.slashOptions.options
+    
+                    } , guild);
                 }
             }else{
                 await createCommand( {
@@ -37,6 +47,32 @@ exports.proc = function(cmdFile){
                 } , guild);
             }
         })
+    }
+    
+    if (cmdFile?.slashOptions?.global){
+        console.log('global');
+        let globalCommands = await getAllCommands();
+        let currentCommand = globalCommands.find(c=>c.name === cmdFile.cmd);
+        if (currentCommand){
+            if (cmdFile.disabled){
+                await disableCommand( currentCommand.id);
+            }else{
+                updateCommand(currentCommand.id,{
+                    name: cmdFile.cmd,
+                    description: cmdFile.slashOptions.description || $t(`commands:help.${cmdFile.cmd}`,"No Command Description"),
+                    options:  cmdFile.slashOptions.options
+
+                });
+            }
+        }else{
+            await createCommand( {
+                name: cmdFile.cmd,
+                description: cmdFile.slashOptions.description || $t(`commands:help.${cmdFile.cmd}`,"No Command Description"),
+                options:  cmdFile.slashOptions.options
+
+            });
+        }
+
     }
     
 }
