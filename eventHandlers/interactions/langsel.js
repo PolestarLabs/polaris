c4e1d1c8a2b07ff13c1c4dfc8962f01a3ceed28c
Tsumari <lucasflicky@hotmail.com>
2021-06-12 04:35:01 -0300

    draft: dropdown langsel

---
+++ eventHandlers/interactions/langsel.js
@@ -0,0 +1,36 @@
+const Languages = require("../../core/structures/Locales.js");
+
+module.exports = async (interaction, data)=>{
+    interaction.reply({
+        content: "Select Language",
+        flags: 64,
+        components:[
+            {
+                "type": 1,
+                "components": [
+                    {
+                        "type": 3,
+                        "placeholder": "Select a language...",
+                        "custom_id": "ddown-langsel",
+                        "min_values": 1,
+                        "max_values": 1,
+                        "options": Languages.i18n.map(lang=>{
+                            console.log(lang.flag)
+                            return {
+                                "label": capitalize(lang.name),
+                                "description": capitalize(lang["name-e"]),
+                                "value": lang.iso,
+                                "emoji": lang.flag.id 
+                                    ? {id: lang.flag.id}
+                                    :  {name:"🤠"}
+                                ,"default": false
+                            }
+                        }).slice(0,25)
+                    }
+                ]
+            }
+        ]
+    });
+ 
+
+}
