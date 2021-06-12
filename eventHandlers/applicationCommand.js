61424b18bfbb89697d98130158d69b3ca9c85783
Tsumari <lucasflicky@hotmail.com>
2021-06-12 04:34:28 -0300

    slash command event handler

---
+++ eventHandlers/applicationCommand.js
@@ -0,0 +1,35 @@
+module.exports = async ( interaction, data) => {
+    
+    const {name: cmd} = data;
+    console.log(data,"DATA".green)
+    try{
+        const response = await Promise.race([
+            PLX.commands[cmd].execute(interaction.message,interaction.message.args||[]).catch(err=>{
+                console.error(err,'the err');
+                return "ERROR"
+            }),
+            wait(2.5).then(x=>"TIMEOUT")
+        ]);
+
+        if (response === "TIMEOUT"){
+            await interaction.defer();
+        }
+
+        response.embeds = [response?.embed]
+
+        if (response?.length && typeof response !== 'string') {
+            let [res,file] = response;            
+            return interaction.reply( res, file);
+        }
+
+        if (response === "TIMEOUT" || !response || (!response.embed && !response.content)) {          
+           return interaction.editOriginal({content: "\u200b"||_emoji("yep")});
+        }
+        
+        interaction.reply(response);
+
+    }catch(err){
+        console.error(err);
+    }
+
+}
