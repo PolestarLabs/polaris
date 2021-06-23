f35524b3c201b0693c2f9baa387a38bb523cd6b9
Tsumari <lucasflicky@hotmail.com>
2021-06-23 08:22:27 -0300

    properly remove original when option is present

--- core/structures/YesNo.js
+++ core/structures/YesNo.js
@@ -129,11 +129,10 @@ module.exports = async function yesNo(promptMessage, commandMessage, yesFunction
       embed.color = 16268605;
       if (deleteFields === true) embed.fields = [];
       embed.footer = { text: strings.cancel };
-
-      options?.deleteOriginal ? promptMessage.delete().catch(err=>null) : promptMessage.edit({ embed });
-      
-
+      promptMessage.edit({ embed });
     }
+    if (options?.deleteOriginal) promptMessage.delete().catch(err=>null);
+    
     if (typeof noFunction === "function") return noFunction(promptMessage);
     if (noFunction) return noFunction;
     if (!noFunction) return false;
@@ -144,8 +143,10 @@ module.exports = async function yesNo(promptMessage, commandMessage, yesFunction
       embed.color = 1234499;
       if (deleteFields === true) embed.fields = [];
       embed.footer = { text: strings.confirm };
-      options?.deleteOriginal ? promptMessage.delete().catch(err=>null) : promptMessage.edit({ embed });
+      promptMessage.edit({ embed });
     }
+    if (options?.deleteOriginal) promptMessage.delete().catch(err=>null);
+
     if (yesFunction && typeof yesFunction === "function") return yesFunction(cancellation, promptMessage);
     return true;
   }
@@ -156,16 +157,19 @@ module.exports = async function yesNo(promptMessage, commandMessage, yesFunction
       embed.color = 16499716;
       if (deleteFields === true) embed.fields = [];
       embed.footer = { text: strings.timeout };
-      options?.deleteOriginal ? promptMessage.delete().catch(err=>null) : promptMessage.edit({ embed });
+      promptMessage.edit({ embed });
     }
-    promptMessage.edit({
-      components: [{
-        type: 1, components: [
-          Object.assign({ disabled: true }, buttonSettings.yep),
-          Object.assign({ disabled: true }, buttonSettings.nope)
-        ]
-      }]
-    });
+    if (options?.deleteOriginal) promptMessage.delete().catch(err=>null);
+    else
+      promptMessage.edit({
+        components: [{
+          type: 1, components: [
+            Object.assign({ disabled: true }, buttonSettings.yep),
+            Object.assign({ disabled: true }, buttonSettings.nope)
+          ]
+        }]
+      });
+    
     if (timeoutFunction && typeof timeoutFunction === "function") return timeoutFunction(promptMessage);
     if (timeoutFunction) return timeoutFunction;
     return null;
