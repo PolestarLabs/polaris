feaaa1e11240278eba22d147b078f7d0c46e26e4
Tsumari <lucasflicky@hotmail.com>
2021-06-23 07:31:50 -0300

    improve invbuttons response speed

--- eventHandlers/interactions/invButton.js
+++ eventHandlers/interactions/invButton.js
@@ -1,4 +1,13 @@
+
 module.exports = async (interaction, data) => {
+
+    const startimer = Date.now();
+
+    _benchmark = (s) => {
+    console.log(`${s.blue + (Date.now() - startimer)}ms`);
+    };
+
+
     const [, , ownerID] = data.custom_id?.split(':') || [];
 
     if (ownerID != interaction.userID) {
@@ -18,33 +27,41 @@ module.exports = async (interaction, data) => {
         interaction.ack();
         return interaction.message.disableButtons("all");
     }
-
+    _benchmark('-u1')
     const fakeMsg = Object.assign({}, interaction.message, {
-        author: await PLX.resolveUser(interaction.userID)
+        author: await PLX.resolveUser(interaction.userID),
+        prefix: PLX.guildPrefixes[interaction.guild.id][0]
     })
+    
+    _benchmark('-u2')
     let args = [];
     args[10] = interaction.userID;
-    const payload = await destination(fakeMsg, args, interaction.userID);
 
-    await interaction.message.removeComponentRow(2);
-    await interaction.message.updateButtons([
+    const injectComponents = await interaction.message.updateButtons([
         { custom_id: data.custom_id, style: 1, disabled: true, label: data.custom_id.split(':')[1] },
         { custom_id: "BLANK", disabled: true },
         { custom_id: /CLOSE/, style: 4 },
         { custom_id: /.*/g, style: 2, disabled: false, label: "" },
-    ]);
+    ],{returnObj:true});
+
+
+    let payload = destination(fakeMsg, args, interaction.userID);
 
     let stashComponents;
+    payload = await payload;
     if (payload.components) {
         stashComponents = payload.components;
         payload.components = undefined;
     }
-
+    payload.components = injectComponents;
+    if (stashComponents) payload.components.push(stashComponents[0]);
+    else payload.components.splice(2,1);
 
     interaction.message.edit(payload).then(m => {
-        if (stashComponents) m.addButtons(stashComponents[0].components, 2);
-        //else interaction.message.removeComponentRow(2);
         interaction.ack();
+        //if (stashComponents) m.addButtons(stashComponents[0].components, 2);
+        //else interaction.message.removeComponentRow(2);
+        
     });
 
 }
