c3c29f703ce2b8e4f153c11392c9567953807bc3
Bsian <chharry321@gmail.com>
2021-06-29 13:52:31 +0100

    Cleanup

--- core/commands/games/noughtsandcrosses.js
+++ core/commands/games/noughtsandcrosses.js
@@ -1,10 +1,3 @@
-/**
- * 
- * @param {import('eris').Message<import('eris').GuildTextableChannel>} msg 
- * @param {string[]} args 
- */
-
-
 const scanlines = (grid) => { // grid = [[x,x,x],[x,x,x],[x,x,x]]
 
   const res = {
@@ -49,42 +42,20 @@ const scanlines = (grid) => { // grid = [[x,x,x],[x,x,x],[x,x,x]]
 
 }
 
-const init = async (msg, args) => {
+/**
+ * 
+ * @param {import('eris').Message<import('eris').GuildTextableChannel>} msg 
+ * @param {string[]} args 
+ */
+ const init = async (msg, args) => {
   if (!args[0]) return msg.channel.createMessage('Missing argument');
   const member = await PLX.getTarget(args[0], msg.guild, false, true);
   if (!member) return msg.channel.createMessage('Unresolved member')
+  if (member.id === msg.author.id) return msg.channel.createMessage('Cannot play yourself');
+  if (member.user.bot) return msg.channel.createMessage('Cannot play with bots');
   const players = [msg.author.id, member.id];
   let playerTurnIndex = 0;
   let PLXMessage;
-
-  // possible wins: top, middle, bottom, left, center, right, TLBR, TRBL
-  /*
-  const counters = [
-    Array(8).fill(0),
-    Array(8).fill(0),
-  ]
-
-  const addToCounter = (row, col) => {
-    counters[playerTurnIndex][row]++;
-    counters[playerTurnIndex][col]++;
-    if (row === col) {
-      counters[playerTurnIndex][6]++;
-    }
-    if (row + col === 2) { // 0,2 or 2,0 or 1,1
-      counters[playerTurnIndex][7]++; 
-    }
-  }
-  const hasWon = (counter) => {
-    const win = counter.indexOf(3);
-    if (win === -1) {
-      if (counters.map((c) => c.filter((d) => d !== 0).length).reduce((a, b) => a + b) === 9) return null;
-      return false;
-    }
-
-    return win;
-  }
-  */
-  ///
 
   const boardGrid = [
     [null, null, null],
@@ -102,7 +73,6 @@ const init = async (msg, args) => {
     if (players[playerTurnIndex] !== d.member.user.id) return PLX.requestHandler.request('POST', `/interactions/${d.id}/${d.token}/callback`, true, { type: 4, data: { content: 'Turn is currently other user', flags: 64 } });
 
     const [x, y] = d.data.custom_id.split(',');
-    //addToCounter(x, y);
 
     markToBoard(x, y);
     let winner = 0;
@@ -121,36 +91,10 @@ const init = async (msg, args) => {
     d.message.components[y - 1].components[x - 1].emoji = playerTurnIndex ? { name: '✖️' } : { id: '851610730880303125' }; // { name: playerTurnIndex ? '❌' : '⭕' };
     d.message.components[y - 1].components[x - 1].disabled = true;
 
-    //const winStatus = hasWon(counters[playerTurnIndex]);
-
-    const winStatus = winner;
-    if (winStatus) {
+    if (winner) {
       PLX.off('rawWS', listener);
 
       finalResult.coords.forEach(([x, y]) => d.message.components[x].components[y].style = 3);
-
-      /*
-      if (winStatus < 3) { // horizontal win (0, 1, 2 possible, 0, 1, 2 on y axis)
-        d.message.components[winStatus].components = d.message.components[winStatus].components.map((c) => {
-          c.style = 3;
-          return c;
-        });
-      } else if (winStatus < 6) { // vertical win (3, 4, 5 possible, 0, 1, 2 on x axis)
-        d.message.components = d.message.components.map((c) => {
-          c.components[winStatus - 3].style = 3;
-          return c;
-        });
-      } else if (winStatus === 6) { // TLBR
-        d.message.components[0].components[0].style = 3;
-        d.message.components[1].components[1].style = 3;
-        d.message.components[2].components[2].style = 3;
-      } else { // TRBL
-        d.message.components[0].components[2].style = 3;
-        d.message.components[1].components[1].style = 3;
-        d.message.components[2].components[0].style = 3;
-      }*/
-
-
       d.message.components = d.message.components.map((a) => { a.components = a.components.map((b) => { b.disabled = true; return b; }); return a; });
 
       msg.channel.createMessage({
@@ -161,7 +105,7 @@ const init = async (msg, args) => {
 
     }
 
-    if (winStatus === null) { // Draw
+    if (winner === null) { // Draw
       d.message.components = d.message.components.map((a) => { a.components = a.components.map((b) => { b.disabled = true; return b; }); return a; });
 
       PLX.off('rawWS', listener);
