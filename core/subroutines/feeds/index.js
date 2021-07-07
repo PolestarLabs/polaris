881673c8af7c82881ccf585845238b23b400d381
Tsumari <lucasflicky@hotmail.com>
2021-07-07 18:36:05 -0300

    fix feed check function  fuckery, add debug

--- core/subroutines/feeds/index.js
+++ core/subroutines/feeds/index.js
@@ -7,29 +7,41 @@
  * @property {"rss" | "twitch" | "youtube"} type
  */
 
+const { checkPrimeSync } = require("crypto");
+
 
  module.exports = {
-  check: async () => {
-    let allGuilds = PLX.guilds.map((g) => g.id);
-    console.log({allGuilds})
-    DB.feed.find({  }).lean().then(async (/** @type {Feed[]} */serverFeeds) => {
-      console.info(`${"RSS: ".blue}Starting...${serverFeeds.length}`);
-      const servers = await DB.servers.find({ id: { $in: serverFeeds.map((f) => f.server) } }, { "modules.LANGUAGE": 1, id: 1 }).lean();
-      serverFeeds.forEach((feed) => {
-        const serverLang = servers.find((/** @type {import("eris").Guild} */sv) => sv.id === feed.server)?.modules?.LANGUAGE || 'en';
-
-        if (feed.type === "rss") {
-          (require("./rss.js")).run((/** @type {import("./rss.js").RSSFeed} */ (feed)));
-        }
-
-        if (feed.type === "twitch") {
-          (require("./twitch.js")).run((/** @type {import("./twitch.js").TwitchFeed} */ (feed)), serverLang);
-        }
-
-        if (feed.type === "youtube") {
-          (require("./youtube.js")).run((/** @type {import("./youtube.js").YoutubeFeed} */ (feed)), serverLang);
-        }
-      });
-    });
-  },
+  check: CHK
 };
+
+async function CHK() {
+  let allGuilds = PLX.guilds.map((g) => g.id);
+  console.log({allGuilds})
+  DB.feed.find({ server: {$in: allGuilds } }).lean().then(async (/** @type {Feed[]} */serverFeeds) => {
+    console.info(`${"RSS: ".blue}Starting...${serverFeeds.length}`);
+    const servers = await DB.servers.find({ id: { $in: serverFeeds.map((f) => f.server) } }, { "modules.LANGUAGE": 1, id: 1 }).lean();
+    serverFeeds.forEach((feed) => {
+
+      console.log({feed})
+      const serverLang = servers.find(sv =>
+         sv.id === feed.server)?.modules?.LANGUAGE || 'en';
+      
+      if (feed.type === "rss") {
+        console.log("RSS")
+        delete require.cache[require.resolve("./rss.js")];
+        let RSS = require("./rss.js");
+        RSS.run(feed);
+      }
+
+      if (feed.type === "twitch") {
+        require("./twitch.js").run(feed, serverLang);
+      }
+
+      if (feed.type === "youtube") {
+        require("./youtube.js").run(feed, serverLang);
+      }
+    });
+  });
+}
+
+global.CHK = CHK
