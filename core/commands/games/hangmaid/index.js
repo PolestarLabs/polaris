e2baddde2c094d8d6d2ec22fc538a7f92f010217
Tsumari <lucasflicky@hotmail.com>
2021-07-12 20:11:22 -0300

    invert solo and server mode, server is default

--- core/commands/games/hangmaid/index.js
+++ core/commands/games/hangmaid/index.js
@@ -16,7 +16,7 @@ const init = async function (msg, args) {
     }, "There's already a game going on here.");
     return;
   }
-  const MODE = args[0] === "server" ? "server" : "solo";
+  const MODE = args[0] === "solo" ? "solo" : "server";
   const GAME = new Hangmaid(msg, WORDS, MODE);
   console.log(GAME.word)
 
