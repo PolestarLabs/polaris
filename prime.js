3db653b0464be88d487b4bbbdc1847267f5469fb
Tsumari <lucasflicky@hotmail.com>
2021-07-14 14:56:49 -0300

    load flavored swarm from ecosystem file

--- prime.js
+++ prime.js
@@ -2,5 +2,7 @@ process.env.PRIME = true;
 process.env.SHARDS_PER_CLUSTER = 1
 process.env.CLUSTER_ID = 1
 process.env.TOTAL_SHARDS = 1
+process.env.PRIME_FLAVORED_CLIENT = "prime"
+process.env.FLAVOR_SWARM_CONFIG = require("../../flavored_swarm.config.js"); // check template at index 
 
 require("./pollux.js");
