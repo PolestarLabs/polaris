948ced3639550d9fe60f60b4a1df4de8d3b004f3
Tsumari <lucasflicky@hotmail.com>
2021-07-23 18:26:56 -0300

    stringify prime swarm config

--- prime.js
+++ prime.js
@@ -3,6 +3,6 @@ process.env.SHARDS_PER_CLUSTER = 1
 process.env.CLUSTER_ID = 1
 process.env.TOTAL_SHARDS = 1
 process.env.PRIME_FLAVORED_CLIENT = "prime"
-process.env.FLAVOR_SWARM_CONFIG = require("../../flavored_swarm.config.js"); // check template at index 
-
+process.env.FLAVOR_SWARM_CONFIG = JSON.stringify(require("../../flavored_swarm.config.js")); // check template at index 
+console.log(process.env.FLAVOR_SWARM_CONFIG )
 require("./pollux.js");
