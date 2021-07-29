fb66a07c21f39d1bbeaac042f3e3d82acdaa7641
Tsumari <lucasflicky@hotmail.com>
2021-07-29 04:34:58 -0300

    webhook commands reject thread channels

--- core/commands/fun/ramsay.js
+++ core/commands/fun/ramsay.js
@@ -1,6 +1,9 @@
 const DspHook = require("../../structures/DisposableWebhook.js");
 
 const init = async (msg) => {
+
+  if (msg.channel.type === 10 || msg.channel.type === 11) return msg.reply(_emoji('nope') + ' This command is not available in Thread Channels.');
+
   const avatars = [
     // "https://robbreportedit.files.wordpress.com/2018/04/gordon-ramsay-1-e1523056498302.jpg?w=1008",
     "https://i.ytimg.com/vi/AVMtt2o2KiI/hqdefault.jpg",
