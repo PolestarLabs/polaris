c879bab1491f58899be592491c78fd7f94e58c14
Tsumari <lucasflicky@hotmail.com>
2021-08-05 12:30:15 -0300

    better interaction tracking

--- eventHandlers/messageComponent.js
+++ eventHandlers/messageComponent.js
@@ -1,7 +1,7 @@
 module.exports = async (interaction, data) => {
     try {
 
-      INSTR.inc("interactions.components", data )
+      INSTR.inc("interactions.components", {cusom_id: data.custom_id.split(":")[0], expand_id: data.custom_id.split(":")[1] } )
       return (require(`./interactions/${data.custom_id.split(":")[0]}.js`))(interaction, data);
         //if (data.component_type === 3) return (require(`./interactions/${data.custom_id.split(":")[0]}.js`))(interaction, data);
 
