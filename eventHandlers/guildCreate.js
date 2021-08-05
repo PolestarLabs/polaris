e6a7409171e6b1c37f18dc0c8e7d737c2464d20f
Tsumari <lucasflicky@hotmail.com>
2021-08-05 10:28:30 -0300

    instrument guild events

--- eventHandlers/guildCreate.js
+++ eventHandlers/guildCreate.js
@@ -1,4 +1,5 @@
 module.exports = async (guild) => {
     console.log(guild,"created".green);
+    INSTR.inc("guilds",{size: guild.memberCount})
     DB.servers.set(guild.id,{$addToSet: {activeClients:PLX.user.id} });
 }
