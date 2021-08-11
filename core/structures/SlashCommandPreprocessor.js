b0a02325872b424dca91d531fd5c64cb35f24871
Tsumari <lucasflicky@hotmail.com>
2021-08-10 23:56:48 -0300

    slash preproc context menus

--- core/structures/SlashCommandPreprocessor.js
+++ core/structures/SlashCommandPreprocessor.js
@@ -32,14 +32,16 @@ exports.proc = async function (cmdFile) {
             
             console.log('        -', guild?.gray);
 
-            let currentCommand = guildCommands.find(c => c.name === cmdFile.cmd);
+            let currentCommand = guildCommands.find(c => c.name === cmdFile.cmd ||c.name === cmdFile?.contextMenu?.name);
+
             if (currentCommand) {
                 if (cmdFile.disabled) {
                     console.log('        •'.red, "Disable...".gray,guild);
                     await disableCommand(currentCommand.id, guild);
                 } else {
                     console.log('        •'.yellow, "Update...".gray,guild);
-
+                    
+                    if (!cmdFile.slashOptions) return console.log("No Slash options for".red ,cmdFile.cmd);
                     updateCommand(currentCommand.id, {
                         name: cmdFile.cmd,
                         description: cmdFile.slashOptions.description || $t(`commands:help.${cmdFile.cmd}`, "No Command Description"),
@@ -47,8 +49,27 @@ exports.proc = async function (cmdFile) {
 
                     }, guild);
                 }
-            } else {
-                console.log('        •'.green, "Create...".gray,guild);
+            }
+
+            if (cmdFile.contextMenu){
+                if (currentCommand?.name === cmdFile.contextMenu.name){
+                    console.log('       ••'.green, "UPDATE Context Menu...".gray,guild, cmdFile.contextMenu.name);
+                    await updateCommand(currentCommand.id, {
+                        name: cmdFile.contextMenu.name,
+                        type: cmdFile.contextMenu.type,
+                    }, guild);
+                }else{
+                    console.log('       ••'.green, "CREATE Context Menu...".gray,guild, cmdFile.contextMenu.name);
+                    await createCommand({
+                        name: cmdFile.contextMenu.name,
+                        type: cmdFile.contextMenu.type,
+                    }, guild);
+                }
+            }
+            
+            if (!currentCommand) {
+                console.log('        •'.bgGreen, "Create...".green,guild);
+                if (!cmdFile.slashOptions) return console.log("No Slash options for".red ,cmdFile.cmd);
                 await createCommand({
                     name: cmdFile.cmd,
                     description: cmdFile.slashOptions.description || $t(`commands:help.${cmdFile.cmd}`, "No Command Description"),
@@ -64,13 +85,15 @@ exports.proc = async function (cmdFile) {
         console.log('    •'.cyan, "Global:".yellow);
 
         let globalCommands = await getAllCommands();
-        let currentCommand = globalCommands.find(c => c.name === cmdFile.cmd);
+        let currentCommand = globalCommands.find(c => c.name === cmdFile.cmd || c.name === cmdFile.contextMenu?.name);
         if (currentCommand) {
             if (cmdFile.disabled) {
-                console.log('       ••'.red, "Disable...".gray);
+                console.log('       ••'.red, "Disable...".gray, cmdFile.cmd);
                 await disableCommand(currentCommand.id);
             } else {
-                console.log('       ••'.yellow, "Update...".gray);
+                console.log('       ••'.yellow, "Update...".gray, cmdFile.cmd);
+                
+                if (!cmdFile.slashOptions) return console.log("No Slash options for".red ,cmdFile.cmd);
                 updateCommand(currentCommand.id, {
                     name: cmdFile.cmd,
                     description: cmdFile.slashOptions.description || $t(`commands:help.${cmdFile.cmd}`, "No Command Description"),
@@ -78,8 +101,25 @@ exports.proc = async function (cmdFile) {
 
                 });
             }
-        } else {
-            console.log('       ••'.green, "Create...".gray);
+        }
+        if (cmdFile.contextMenu){
+            if (currentCommand?.name === cmdFile.contextMenu.name){
+                console.log('       ••'.green, "UPDATE Context Menu...".yellow, cmdFile.contextMenu.name);
+                await updateCommand(currentCommand.id, {
+                    name: cmdFile.contextMenu.name,
+                    type: cmdFile.contextMenu.type,
+                });
+            }else{
+                console.log('       ••'.green, "Create Context Menu...".green, cmdFile.contextMenu.name);
+                await createCommand({
+                    name: cmdFile.contextMenu.name,
+                    type: cmdFile.contextMenu.type,
+                });
+            }            
+        }
+        if (!currentCommand) {
+            console.log('       ••'.green, "Create...".gray, cmd.cmdFile);
+            if (!cmdFile.slashOptions) return console.log("No Slash options for".red ,cmdFile.cmd);
             await createCommand({
                 name: cmdFile.cmd,
                 description: cmdFile.slashOptions.description || $t(`commands:help.${cmdFile.cmd}`, "No Command Description"),
