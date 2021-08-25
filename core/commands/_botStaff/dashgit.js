d2fde88bcbfb8df7fe511f5046d87b27f5269cee
Tsumari <lucasflicky@hotmail.com>
2021-08-25 11:27:55 -0300

    live dashgit

--- core/commands/_botStaff/dashgit.js
+++ core/commands/_botStaff/dashgit.js
@@ -2,7 +2,10 @@
 const init = async (msg, args) => {
   let fail = false;
 
-  const description = await exec(`git ${args.join(" ")}`, { cwd: "/home/pollux/polaris/DEV/dashboard" }).then(
+  const live = args[0] === 'live';
+  if (live) args[0] = '';
+
+  const description = await exec(`git ${args.join(" ")}`, { cwd: "/home/pollux/polaris/"+( live ?"LIVE":"DEV")+"/dashboard" }).then(
     (res) => `${_emoji("yep")} \`${args.join(" ")}\` ${res.length ? "```nginx\n" : "```OK!"}${res.slice(0, 1900)}${"```"}`,
     (rej) => ((fail = true), `${_emoji("nope")}**Oopsie Woopsie:** \`\`\`nginx\n${rej.message.slice(0, 1900)}\`\`\``),
   );
