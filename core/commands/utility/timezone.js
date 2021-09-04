c77ed25fb8ded88a0375e76e188e52cc6675d4a4
LuckShiba <luckshiba@protonmail.com>
2021-09-04 18:06:01 -0300

    Removing redundant code in timezone.js

--- core/commands/utility/timezone.js
+++ core/commands/utility/timezone.js
@@ -7,25 +7,31 @@ const capt = (phrase) => phrase
   .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
   .join(" ");
 
+const formatDate = (timezone) => m.tz(timezone).format("hh:mma");
+
 const init = async (msg, args) => {
   switch (args[0]) {
     case "me": {
       const targetData = await DB.userDB.get(msg.author.id);
+
       if (targetData.timezone === "") {
         msg.channel.send("You haven't set your timezone.");
         return;
       }
-      msg.channel.send(`It's **${m.tz(targetData.timezone).format("hh:mma")}** where you live.`);
+
+      msg.channel.send(`It's **${formatDate(targetData.timezone)}** where you live.`);
       break;
     }
 
     case "set": {
       const zoneName = args.splice(1).join(" ");
       const tz = ct.lookupViaCity(zoneName)[0]?.timezone || m.tz.zone(zoneName)?.name;
+
       if (!tz) {
         msg.channel.send("Couldn't find that place.");
         return;
       }
+
       await DB.userDB.set(msg.author.id, { $set: { timezone: tz } });
       msg.channel.send(`I've set your timezone to **${capt(zoneName)}**.`);
       break;
@@ -41,15 +47,17 @@ const init = async (msg, args) => {
           return;
         }
 
-        msg.channel.send(`It's **${m.tz(targetData.timezone).format("hh:mma")}** where **${targetData.tag}** lives.`);
+        msg.channel.send(`It's **${formatDate(targetData.timezone)}** where **${targetData.tag}** lives.`);
       } else {
         const zoneName = args.join(" ");
         const tz = ct.lookupViaCity(zoneName)[0]?.timezone || m.tz.zone(zoneName)?.name;
+
         if (!tz) {
           msg.channel.send("Couldn't find that place.");
           return;
         }
-        msg.channel.send(`It's **${m.tz(tz).format("hh:mma")}** in **${capt(args.join(" "))}**.`);
+
+        msg.channel.send(`It's **${formatDate(tz)}** in **${capt(zoneName)}**.`);
       }
     }
   }
