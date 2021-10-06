603935d1849b915ac619e29fab8525cb9f2d0dd2
Tsumari <lucasflicky@hotmail.com>
2021-10-06 09:41:24 -0300

    promocodes refactor

--- core/commands/misc/redeem.js
+++ core/commands/misc/redeem.js
@@ -1,33 +1,46 @@
 const Redeem = require("../../archetypes/Redeem");
+const YesNo = require("../../structures/YesNo");
+
 
 const init = async (msg, args) => {
+  const P = {lngs:msg.lang};
   const code = args[0];
   const prizeOperator = new Redeem(code, msg.author.id);
-  const invalid = await prizeOperator.verify();
+  await prizeOperator.hydrate();
+  
+  // LOCK when single-use (implicit or explicit)
+  if (!prizeOperator.data.maxUses || prizeOperator.data.maxUses === 1 ) await prizeOperator.lock();
 
-  if (invalid) {
-    switch (invalid) {
+  const validationError = prizeOperator.verify();
+
+  if (validationError) {
+    switch (validationError) {
       case "invalid":
-        return msg.reply(`${_emoji("NOPE").no_space} Oopsie! This code is invalid. Please double-check it and try again.`);
-      case "locked":
-        return msg.reply(`${_emoji("NOPE").no_space} This code is not redeemable at the moment. Please try again later.`);
+        return msg.reply(`${_emoji("nope")} Oopsie! This code is invalid. Please double-check it and try again.`);
       case "nonredeemable":
-        return msg.reply(`${_emoji("NOPE").no_space} This code is not redeemable anymore.`);
-      case "already_exists":
-        return msg.reply(`${_emoji("NOPE").no_space} You've already redeemed this code`);
-
+        return msg.reply(`${_emoji("nope")} This code is not redeemable anymore.`);
+      case "already_redeemed":
+        return msg.reply(`${_emoji("nope")} You've already redeemed this code`);
+      case "exhausted":
+        return msg.reply(`${_emoji("nope")} This code has been used the maximum amount of times.`);
+      case "expired":
+        return msg.reply(`${_emoji("nope")} This code redeem period is over.`);
+      case "locked":
+        return msg.reply(`${_emoji("nope")} This code is not redeemable at the moment. Please try again later.`);
     }
   }
 
   //TRANSLATE[epic=translations] redeem
 
   await msg.delete();
-  const prize = prizeOperator.prize.split(" ");
+  const prize = prizeOperator.parsePrize();
+  P.count = prize.amount;
+  
   const embed = {
     fields: [
       {
         name: "Here's what you get by redeeming this code:",
-        value: `• ${prize[0]}x ${prize[1]} ${_emoji(prize[2]).no_space}\n\nYou want to proceed?`
+        value: `• ${ $t(prize.keyword,P) } x  ${prize.amount} ${_emoji(prize.subject)}\n\n Do you wish to proceed?`
       }
     ],
     footer: {
@@ -37,18 +50,18 @@ const init = async (msg, args) => {
     color: 0x1B1B2B
   };
   const confirmation = await msg.channel.send({
-    content: "So you got a gift code to redeem? How nice!",
+    content: "So you got a gift code to redeem? How dandy!",
     embed
   });
-  const prompt = await prizeOperator.prompt(confirmation, msg);
+
+  const prompt = await YesNo(confirmation, msg);
 
   if (prompt) {
-    const prize = prizeOperator.prize.split(" ");
     const newEmbed = {
       fields: [
         {
           name: "Good! The items were added to your inventory",
-          value: `> ${prize[0]}x ${prize[1]} ${_emoji(prize[2]).no_space}`
+          value: `> ${ $t(prize.keyword,P) } x  ${prize.amount} ${_emoji(prize.subject)}`
         }
       ],
       footer: {
@@ -58,16 +71,20 @@ const init = async (msg, args) => {
       color: 0x1B1B2B
     };
 
-    await prizeOperator.redeem(msg.author.id, prizeOperator.prize);
+    const result = await prizeOperator.redeem(msg.author.id, prizeOperator.prize);
+    
+    if (result && result.type === 'background') newEmbed.image = {url: `${paths.CDN}/backdrops/${result.code||result.id}.png`};
+    if (result && result.type === 'medal') newEmbed.image = {url: `${paths.CDN}/medals/${result.icon||result.id}.png`};
+    if (result && result.type === 'sticker') newEmbed.image = {url: `${paths.CDN}/stickers/${result.id}.png`};
+    
+    
     await confirmation.edit({
       embed: newEmbed,
       content: ""
     });
 
   } else if (!prompt) { // unlock code in timeout/cancel
-    await DB.promocodes.collection.updateOne({ code }, {
-      $set: { locked: false }
-    });
+    await prizeOperator.unlock();
   }
 };
 
