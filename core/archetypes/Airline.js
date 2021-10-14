f0ff4c1f979504a040ead0120e30e1f2a2fdf736
Tsumari <lucasflicky@hotmail.com>
2021-10-14 02:07:41 -0300

    enforce scope

--- core/archetypes/Airline.js
+++ core/archetypes/Airline.js
@@ -1,16 +1,16 @@
 const ECO = require('./Economy');
 class Airline {
   constructor(id) {
-    this.airline = DB.airlines.AIRLINES.findOne({ id })
+    this.airline = DB.airlines.AIRLINES.findOne({ id });
   }
 
   async buySlot(airport, time) {
     const airportData = await DB.airlines.AIRPORT.findOne({ id: airport });
     if (!airportData) return Promise.reject("Invalid airport ID.");
 
-    await DB.airlines.SLOTS.validate((await this.airline).id, airportData.id).then(ok=>{
-      return ECO.pay((await this.airline).user, airportData.slotPrice, "airlines_buy_slot").then(async () => {
-        return DB.airlines.SLOTS.new((await this.airline).id, airportData.id, time);
+    await DB.airlines.SLOTS.validate((await (this.airline)).id, airportData.id).then(ok=>{
+      return ECO.pay( (await (this.airline)).user, airportData.slotPrice, "airlines_buy_slot").then(async () => {
+        return DB.airlines.SLOTS.new((await (this.airline)).id, airportData.id, time);
       }).catch(err=>{
         return Promise.reject("Insufficient Funds");
       });
