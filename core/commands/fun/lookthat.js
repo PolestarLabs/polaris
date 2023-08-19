18335afdfadf20f4e2b7126d5d1613efa2d925e8
Tsumari <lucasflicky@hotmail.com>
2023-08-19 22:19:40 +0200

    add command

---
+++ core/commands/fun/lookthat.js
@@ -0,0 +1,36 @@
+const Picto = require("../../utilities/Picto");
+
+const init = async (msg) => {
+  let link = (msg.args.join(" ").match(/(http[^ |^>]+)/gm) || [""])[0];
+
+  try {
+    // link ??= msg.mentions[0]?.displayAvatarURL ?? await PLX.getChannelImg(msg) ?? msg.author.displayAvatarURL;
+    if (!link) link = msg.mentions[0]?.displayAvatarURL ?? await PLX.getChannelImg(msg) ?? msg.author.displayAvatarURL;
+  } catch (e) {
+    // link ??= (msg.mentions[0] || msg.author).displayAvatarURL;
+    if (!link) link = (msg.mentions[0] || msg.author).displayAvatarURL;
+  }
+
+  const [subject, hand] = await Promise.all([
+    Picto.getCanvas(link),
+    Picto.getCanvas(`${paths.CDN}/build/assorted/aquelamerda.png`),
+  ]);
+  const canvas = Picto.new(subject.width, subject.height);
+  const ctx = canvas.getContext("2d");
+
+  ctx.drawImage(subject, 0, 0);
+  ctx.drawImage(hand, 0, 0, subject.height, subject.height);
+
+  //return msg.channel.send($t("responses.forFun.thatShit", { lngs: msg.lang } ), file(await canvas.toBuffer(), "latts.png"));
+  return msg.channel.send( "Olha só **aquela** merda!", file(await canvas.toBuffer(), "latts.png"));
+};
+
+module.exports = {
+  init,
+  pub: false,
+  cmd: "thatshit",
+  perms: 3,
+  cat: "fun",
+  botPerms: ["attachFiles", "embedLinks"],
+  aliases: ["aquelamerda", "ltts", "look at that shit"],
+};
