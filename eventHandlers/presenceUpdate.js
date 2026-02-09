a1b1fbe26ca44d2419887455d7d721fbae5809f5
Flicky <flicky@pollux.gg>
2026-02-08 23:07:53 -0300

    remove logs

--- eventHandlers/presenceUpdate.js
+++ eventHandlers/presenceUpdate.js
@@ -2,17 +2,10 @@ module.exports = async (member, oldPres) => {
   if (!PLX?.isPRIME) return;
 
   if (member.guild.id === '206252981895692289') {
-    console.log('bwp');
 
-    console.table({
-      name: member.nick || member.user.name,
-      id: member.id
-    });
 
-    console.log(member.activities[0]);
 
     if (member.id === '200044537270370313') {
-      console.log('saitore');
 
       if (member.activities[0]?.state?.toLowerCase() === "dead") {
         PLX.createMessage("942520060726108240", `
