c6103a009b15edce823d87719384947d40787af0
Flicky <flicky@pollux.gg>
2026-02-08 23:34:29 -0300

    Declare gallery URL locals

--- core/structures/Galleries.js
+++ core/structures/Galleries.js
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   randomOneIndexed: async (gallery, url = false) => {
-    Url = `${paths.DASH}/random/${gallery}`;
+    let Url = `${paths.DASH}/random/${gallery}`;
     const preRes = await axios.get(`${Url}/size`, {
       headers: { Accept: "json" },
       responseType: "json",
@@ -36,7 +36,7 @@ module.exports = {
   },
 
   indexedOne: async (gallery, index = 0) => {
-    Url = `${paths.DASH}/random/${gallery}/${index}`;
+    let Url = `${paths.DASH}/random/${gallery}/${index}`;
     const response = await axios.get(Url, {
       headers: { Accept: "*" },
       responseType: "arraybuffer",
@@ -44,7 +44,7 @@ module.exports = {
     return response.data;
   },
   filteredOne: async (gallery, filter) => {
-    Url = `${paths.DASH}/random/${gallery}/filter/${filter}?json=1`;
+    let Url = `${paths.DASH}/random/${gallery}/filter/${filter}?json=1`;
     const response = await axios.get(Url, {
       headers: { Accept: "json" },
       responseType: "json",
