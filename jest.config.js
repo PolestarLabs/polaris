03cc2a4118e405d8ed9b3b069710c10057fb5fa0
Flicky <flicky@pollux.gg>
2026-02-09 00:41:17 -0300

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  testMatch: ["**/*.test.js"],
+  setupFiles: ["<rootDir>/test/helpers/globals.js"],
+  setupFilesAfterEnv: ["<rootDir>/test/helpers/setupAfterEnv.js"],
+  collectCoverageFrom: [
+    "core/archetypes/**/*.js",
+    "core/structures/**/*.js",
+    "!**/node_modules/**",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "text-summary"],
+  verbose: true,
+  forceExit: true,
+};
