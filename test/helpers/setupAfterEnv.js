37b9cbf7fddebcb5a73bf516fff6cc77574298c3
Flicky <flicky@pollux.gg>
2026-02-09 00:11:51 -0300

    Bind globals for Jest

--- test/helpers/setupAfterEnv.js
+++ test/helpers/setupAfterEnv.js
@@ -2,4 +2,19 @@ const { ensureTestGlobals } = require("./globals");
 
 beforeEach(() => {
   ensureTestGlobals();
+  Object.defineProperty(globalThis, "DB", {
+    value: global.DB,
+    writable: true,
+    configurable: true,
+  });
+  Object.defineProperty(globalThis, "PLX", {
+    value: global.PLX,
+    writable: true,
+    configurable: true,
+  });
+  Object.defineProperty(globalThis, "Progression", {
+    value: global.Progression,
+    writable: true,
+    configurable: true,
+  });
 });
