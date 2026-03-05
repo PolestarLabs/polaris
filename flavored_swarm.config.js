9aa5054bbb7be556ec57cac7011e36c4291d82b6
Flicky <flicky@pollux.gg>
2026-03-05 17:53:00 -0300

    update gitignore, re-add flavored swarm shim

---
+++ flavored_swarm.config.js
@@ -0,0 +1,7 @@
+/** 
+ @deprecated
+    This file is no longer used, but is left in place to avoid breaking existing imports.
+*/
+const swarm = require("../../clients.secret")
+
+module.exports = swarm;
