7898b5e0dec38e9470faa72969b44def21878327
Flicky <flicky@pollux.gg>
2026-03-06 05:35:58 -0300

    update banner

--- resources/asciiPollux.js
+++ resources/asciiPollux.js
@@ -6,7 +6,7 @@ const TOTAL_SHARDS        = parseInt(process.env.TOTAL_SHARDS) || 1;
 const isPRIME             = process.env.PRIME === "true" || process.env.PRIME === true;
 const CLIENT_DATA         = JSON.parse(process.env.PRIME_FLAVORED_CLIENT);
 
-const UNIT = isPRIME ? `⭐ PRIME (${CLIENT_DATA.name})` : CLUSTER_NAMES[CLUSTER_ID] || `Cluster ${CLUSTER_ID}`;
+const UNIT = isPRIME ? `⭐ PRIME (${CLIENT_DATA.name})` : ` ${CLUSTER_NAMES[CLUSTER_ID]} `.bgGreen || `Cluster ${CLUSTER_ID}`;
 
 const ascii = function () {
   const a = isPRIME ? "yellow" : "red";
@@ -14,11 +14,11 @@ const ascii = function () {
 
   const text = `${"                                                                "[a][b]
     }\n${"                                                                "[a][b]
-    }\n${"  ,ggggggggggg,                                                 "[a][b]
-    }\n${" dP\"\"\"88\"\"\"\"\"\"Y8,      ,dPYb, ,dPYb,                            "[a][b]
-    }\n${" Yb,  88      `8b      IP'`Yb IP'`Yb                            "[a][b]
-    }\n${"  `\"  88      ,8P      I8  8I I8  8I                            "[a][b]
-    }\n${"      88aaaad8P\"       I8  8' I8  8'                            "[a][b]
+    }\n${"  ,ggggggggggg,"[a][b]
+    }\n${" dP\"\"\"88\"\"\"\"\"\"Y8,      ,dPYb, ,dPYb,"[a][b]
+    }\n${" Yb,  88      `8b      IP'`Yb IP'`Yb    "[a][b]+`${UNIT}`.magenta
+    }\n${"  `\"  88      ,8P      I8  8I I8  8I   "[a][b]
+    }\n${"      88aaaad8P\"       I8  8' I8  8'"[a][b]
     }\n${"      88\"\"\"\"\",ggggg,   I8 dP  I8 dP  gg      gg     ,gg,   ,gg  "[a][b]
     }\n${"      88    dP\"  \"Y8gggI8dP   I8dP   I8      8I    d8\"\"8b,dP\"   "[a][b]
     }\n${"      88   i8'    ,8I  I8P    I8P    I8,    ,8I   dP   ,88\"     "[a][b]
@@ -27,7 +27,7 @@ const ascii = function () {
     }\n${"                                                                "[a][b]
     }\n` + `     v${version}                                 ${"Powered by Eris".gray
     }\n${"                                                                "[a][b]
-    }\n${`> STARTING UP...  ${UNIT}                                              `.magenta
+    }\n${`> STARTING UP...                                             `.magenta
     }\n${"                                                                "[a][b]}`;
 
   return text;
